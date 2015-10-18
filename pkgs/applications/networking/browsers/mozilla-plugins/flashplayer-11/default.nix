70d4044136c120be5385b45f8e64e67b0f5f5327
Cody Opel <codyopel@gmail.com>
2015-10-18 05:19:10 -0400

    adobe-flash: 11.2.202.508 -> 11.2.202.535

--- pkgs/applications/networking/browsers/mozilla-plugins/flashplayer-11/default.nix
+++ pkgs/applications/networking/browsers/mozilla-plugins/flashplayer-11/default.nix
@@ -1,5 +1,4 @@
-{ stdenv
-, fetchurl
+{ stdenv, fetchurl
 , zlib
 , alsaLib
 , curl
@@ -36,7 +35,7 @@
 
 let
   # -> http://get.adobe.com/flashplayer/
-  version = "11.2.202.508";
+  version = "11.2.202.535";
 
   src =
     if stdenv.system == "x86_64-linux" then
@@ -47,7 +46,7 @@ let
       else rec {
         inherit version;
         url = "http://fpdownload.adobe.com/get/flashplayer/pdc/${version}/install_flash_player_11_linux.x86_64.tar.gz";
-        sha256 = "1i0301vnz94pxcwm9wk1jjyv7gwywy6p7z26ikd5cg259myxbg75";
+        sha256 = "13fy842plbnv4w081sbhga0jrpbwz8yydg49c2v96l2marmzw9zp";
       }
     else if stdenv.system == "i686-linux" then
       if debug then
@@ -60,7 +59,7 @@ let
       else rec {
         inherit version;
         url = "http://fpdownload.adobe.com/get/flashplayer/pdc/${version}/install_flash_player_11_linux.i386.tar.gz";
-        sha256 = "1483bi34ymchv1cqg57whxhlrhhvwhcw33zjgwzmy7bacxbkj9ia";
+        sha256 = "0z99nz1k0cf86dgs367ddxfnf05m32psidpmdzi5qiqaj10h6j6s";
       }
     else throw "Flash Player is not supported on this platform";
 
@@ -73,23 +72,42 @@ stdenv.mkDerivation {
 
   src = fetchurl { inherit (src) url sha256; };
 
-  inherit zlib alsaLib;
+  inherit
+    zlib
+    alsaLib;
+
+  rpath = stdenv.lib.makeLibraryPath [
+    zlib
+    alsaLib
+    curl
+    nspr
+    fontconfig
+    freetype
+    expat
+    libX11
+    libXext
+    libXrender
+    libXcursor
+    libXt
+    gtk
+    glib
+    pango
+    atk
+    cairo
+    gdk_pixbuf
+    libvdpau
+    nss
+  ];
+
+  buildPhase = ":";
 
   passthru = {
     mozillaPlugin = "/lib/mozilla/plugins";
   };
 
-  rpath = stdenv.lib.makeLibraryPath
-    [ zlib alsaLib curl nspr fontconfig freetype expat libX11
-      libXext libXrender libXcursor libXt gtk glib pango atk cairo gdk_pixbuf
-      libvdpau nss
-    ];
-
-  buildPhase = ":";
-
   meta = {
     description = "Adobe Flash Player browser plugin";
     homepage = http://www.adobe.com/products/flashplayer/;
-    license = stdenv.lib.licenses.unfree;
+    license = stdenv.lib.licenses.unfreeRedistributable;
   };
 }
