cc7417bcde37581c0a07cf2c9ec44e7e856c844a
Cody Opel <codyopel@gmail.com>
2015-10-19 10:31:50 -0400

    btrfsProgs: 4.2.1 -> 4.2.2

--- pkgs/tools/filesystems/btrfsprogs/default.nix
+++ pkgs/tools/filesystems/btrfsprogs/default.nix
@@ -1,31 +1,51 @@
-{ stdenv, fetchurl, pkgconfig, attr, acl, zlib, libuuid, e2fsprogs, lzo
-, asciidoc, xmlto, docbook_xml_dtd_45, docbook_xsl, libxslt
+{ stdenv, fetchurl
+, pkgconfig
+, attr
+, acl
+, zlib
+, libuuid
+, e2fsprogs
+, lzo
+, asciidoc
+, xmlto
+, docbook_xml_dtd_45
+, docbook_xsl
+, libxslt
 }:
-
-let version = "4.2.1"; in
 
 stdenv.mkDerivation rec {
   name = "btrfs-progs-${version}";
+  version = "4.2.2";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v${version}.tar.xz";
-    sha256 = "1nzyyfj84q98n7lf798k6695pbsd02hg2ffd1kgn8g3qcbwqhwar";
+    sha256 = "05hkj8fpgc2c9068f167hp8k8h28zadlh7krcz0qi8m3nbmy91sx";
   };
-
-  buildInputs = [
-    pkgconfig attr acl zlib libuuid e2fsprogs lzo
-    asciidoc xmlto docbook_xml_dtd_45 docbook_xsl libxslt
-  ];
 
   # gcc bug with -O1 on ARM with gcc 4.8
   # This should be fine on all platforms so apply universally
   patchPhase = "sed -i s/-O1/-O2/ configure";
 
+  buildInputs = [
+    pkgconfig
+    attr
+    acl
+    zlib
+    libuuid
+    e2fsprogs
+    lzo
+    asciidoc
+    xmlto
+    docbook_xml_dtd_45
+    docbook_xsl
+    libxslt
+  ];
+
   meta = with stdenv.lib; {
     description = "Utilities for the btrfs filesystem";
     homepage = https://btrfs.wiki.kernel.org/;
     license = licenses.gpl2;
-    maintainers = with maintainers; [ raskin wkennington ];
+    maintainers = with maintainers; [ wkennington ];
     platforms = platforms.linux;
   };
 }
