72ed7596f77b2062d7c772f130a73326a3634e3e
Cody Opel <codyopel@gmail.com>
2015-10-19 10:09:10 -0400

    spice: 0.12.5 -> 0.12.6

--- pkgs/development/libraries/spice/default.nix
+++ pkgs/development/libraries/spice/default.nix
@@ -1,29 +1,66 @@
-{ stdenv, fetchurl, pkgconfig, pixman, celt, alsaLib, openssl
-, libXrandr, libXfixes, libXext, libXrender, libXinerama, libjpeg, zlib
-, spice_protocol, python, pyparsing, glib }:
+{ stdenv, fetchurl
+, pkgconfig
+, spice_protocol
+
+, pixman
+, celt
+, alsaLib
+, openssl
+, libXrandr
+, libXfixes
+, libXext
+, libXrender
+, libXinerama
+, libjpeg
+, zlib
+, python
+, pyparsing
+, glib
+, cyrus_sasl
+, lz4
+}:
 
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  name = "spice-0.12.5";
+  name = "spice-0.12.6";
 
   src = fetchurl {
     url = "http://www.spice-space.org/download/releases/${name}.tar.bz2";
-    sha256 = "10gmqaanfg929aamf11n4si4r3d1g7z9qjdclsl9kjv7iw6s42a2";
+    sha256 = "1dk9hp78ldqb0a52kdlqq0scnk3drnhj7rsw8r7hmy2v2cqflj7i";
   };
 
-  buildInputs = [ pixman celt alsaLib openssl libjpeg zlib
-                  libXrandr libXfixes libXrender libXext libXinerama
-                  python pyparsing glib ];
-
-  nativeBuildInputs = [ pkgconfig spice_protocol ];
+  configureFlags = [
+    "--with-sasl"
+    "--disable-smartcard"
+    "--enable-client"
+    "--enable-lz4"
+  ];
 
   NIX_CFLAGS_COMPILE = "-fno-stack-protector";
 
-  configureFlags = [
-    "--with-sasl=no"
-    "--disable-smartcard"
-    "--enable-client"
+  nativeBuildInputs = [
+    pkgconfig
+    spice_protocol
+  ];
+
+  buildInputs = [
+    pixman
+    celt
+    alsaLib
+    openssl
+    libjpeg
+    zlib
+    libXrandr
+    libXfixes
+    libXrender
+    libXext
+    libXinerama
+    python
+    pyparsing
+    glib
+    cyrus_sasl
+    lz4
   ];
 
   postInstall = ''
@@ -31,18 +68,10 @@ stdenv.mkDerivation rec {
   '';
 
   meta = {
-    description = "Complete open source solution for interaction with virtualized desktop devices";
-    longDescription = ''
-      The Spice project aims to provide a complete open source solution for interaction
-      with virtualized desktop devices.The Spice project deals with both the virtualized
-      devices and the front-end. Interaction between front-end and back-end is done using
-      VD-Interfaces. The VD-Interfaces (VDI) enable both ends of the solution to be easily
-      utilized by a third-party component.
-    '';
+    description = "Solution for interaction with virtualized desktop devices";
     homepage = http://www.spice-space.org/;
     license = licenses.lgpl21;
-
-    maintainers = [ maintainers.bluescreen303 ];
+    maintainers = [ ];
     platforms = platforms.linux;
   };
 }
