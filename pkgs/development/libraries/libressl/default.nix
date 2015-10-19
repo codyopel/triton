4879f57601d77332663802455c06dc5fe40805f2
Cody Opel <codyopel@gmail.com>
2015-10-19 10:33:34 -0400

    libressl: 2.2.3 -> 2.2.4

--- pkgs/development/libraries/libressl/default.nix
+++ pkgs/development/libraries/libressl/default.nix
@@ -2,19 +2,19 @@
 
 stdenv.mkDerivation rec {
   name = "libressl-${version}";
-  version = "2.2.3";
+  version = "2.2.4";
 
   src = fetchurl {
     url    = "mirror://openbsd/LibreSSL/${name}.tar.gz";
-    sha256 = "10nq2rpyzgl7xhdip9mmn7hzb6hcjffbjcb04jchxmlivwdc5k51";
+    sha256 = "0zlsxw366n438dc14zqnim6fc5vh1574jj95hv1sym46prcrhh3b";
   };
 
   enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     description = "Free TLS/SSL implementation";
-    homepage    = "http://www.libressl.org";
-    platforms   = platforms.all;
-    maintainers = with maintainers; [ thoughtpolice wkennington ];
+    homepage = http://www.libressl.org;
+    maintainers = with maintainers; [ wkennington ];
+    platforms = platforms.all;
   };
 }
