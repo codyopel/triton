ac0356f78a17ba1a38b26ccdfe4e765f99e83308
Cody Opel <codyopel@gmail.com>
2015-10-26 13:39:06 -0400

    feh: 2.13.1 -> 2.14

--- pkgs/applications/graphics/feh/default.nix
+++ pkgs/applications/graphics/feh/default.nix
@@ -1,30 +1,52 @@
-{ stdenv, makeWrapper, fetchurl, xlibsWrapper, imlib2, libjpeg, libpng
-, libXinerama, curl, libexif }:
+{ stdenv, fetchurl
+, makeWrapper
+
+, xlibsWrapper
+, imlib2
+, libjpeg
+, libpng
+, libXinerama
+, curl
+, libexif
+}:
 
 stdenv.mkDerivation rec {
-  name = "feh-2.13.1";
+  name = "feh-2.14";
 
   src = fetchurl {
     url = "http://feh.finalrewind.org/${name}.tar.bz2";
-    sha256 = "1059mflgw8hl398lwy55fj50a98xryvdf23wkpbn4s0z9388hl46";
+    sha256 = "0j5wxpqccnd0hl74z2vwv25n7qnik1n2mcm2jn0c0z7cjn4wsa9q";
   };
 
-  buildInputs = [ makeWrapper xlibsWrapper imlib2 libjpeg libpng libXinerama curl libexif ];
+  makeFlags = [
+    "PREFIX=$(out)"
+    "exif=1"
+  ];
 
-  preBuild = ''
-    makeFlags="PREFIX=$out exif=1"
-  '';
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  buildInputs = [
+    xlibsWrapper
+    imlib2 libjpeg
+    libpng
+    libXinerama
+    curl
+    libexif
+  ];
 
   postInstall = ''
-    wrapProgram "$out/bin/feh" --prefix PATH : "${libjpeg}/bin" \
-                               --add-flags '--theme=feh'
+    wrapProgram "$out/bin/feh" \
+      --prefix PATH : "${libjpeg}/bin" \
+      --add-flags '--theme=feh'
   '';
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "A light-weight image viewer";
     homepage = https://derf.homelinux.org/projects/feh/;
-    license = stdenv.lib.licenses.mit;
-    maintainers = with stdenv.lib.maintainers; [viric];
-    platforms = with stdenv.lib.platforms; unix;
+    license = licenses.mit;
+    maintainers = with maintainers; [ ];
+    platforms = platforms.linux;
   };
 }
