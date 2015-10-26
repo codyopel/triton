08742f265817efc1400966262e5ca3fb692492ca
Cody Opel <codyopel@gmail.com>
2015-10-26 13:28:27 -0400

    viewnior: 1.4 -> 1.5

--- pkgs/applications/graphics/viewnior/default.nix
+++ pkgs/applications/graphics/viewnior/default.nix
@@ -1,39 +1,57 @@
-{ stdenv, fetchurl, pkgconfig, gtk2, libpng, exiv2, lcms
-, intltool, gettext, shared_mime_info, glib, gdk_pixbuf, perl}:
+{ stdenv, fetchurl
+, autoreconfHook
+, gettext
+, intltool
+, pkgconfig
+
+, exiv2
+, gdk_pixbuf
+, glib
+, gtk2
+, lcms
+, libpng
+, shared_mime_info
+}:
 
 stdenv.mkDerivation rec {
-  name = "viewnior-1.4";
+  name = "viewnior-${version}";
+  version = "1.5";
 
   src = fetchurl {
-    url = "https://www.dropbox.com/s/zytq0suabesv933/${name}.tar.gz";
-    sha256 = "0vv1133phgfzm92md6bbccmcvfiqb4kz28z1572c0qj971yz457a";
+    url = "https://github.com/xsisqox/Viewnior/archive/viewnior-${version}.tar.gz";
+    sha256 = "10by9zpasl79j234mciwa0x7fqvhxl6r3q31cnpk8sq96fi589bz";
   };
 
-  buildInputs =
-    [ pkgconfig gtk2 libpng exiv2 lcms intltool gettext
-      shared_mime_info glib gdk_pixbuf perl
-    ];
+  configureFlags = [
+    "--enable-wallpaper"
+  ];
+
+  nativeBuildInputs = [
+    autoreconfHook
+    gettext
+    intltool
+    pkgconfig
+  ];
+
+  buildInputs = [
+    exiv2
+    gdk_pixbuf
+    glib
+    gtk2
+    lcms
+    libpng
+    shared_mime_info
+  ];
 
   preFixup = ''
     rm $out/share/icons/*/icon-theme.cache
   '';
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Fast and simple image viewer";
-    longDescription =
-      '' Viewnior is insipred by big projects like Eye of Gnome, because of it's
-         usability and richness,and by GPicView, because of it's lightweight design and
-         minimal interface. So here comes Viewnior - small and light, with no compromise
-         with the quality of it's functions. The program is made with better integration
-         in mind (follows Gnome HIG2).
-      '';
-
-    license = stdenv.lib.licenses.gpl3;
-
-    homepage = http://xsisqox.github.com/Viewnior;
-
-    maintainers = [ stdenv.lib.maintainers.smironov ];
-
-    platforms = stdenv.lib.platforms.gnu;
+    homepage = http://siyanpanayotov.com/project/viewnior/;
+    license = licenses.gpl3;
+    maintainers = [ ];
+    platforms = platforms.linux;
   };
 }
