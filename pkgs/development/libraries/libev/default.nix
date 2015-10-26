31632f4dfd0f4856e18bc79cc559703391306af2
Cody Opel <codyopel@gmail.com>
2015-10-26 13:34:27 -0400

    libev: 4.19 -> 4.20

--- pkgs/development/libraries/libev/default.nix
+++ pkgs/development/libraries/libev/default.nix
@@ -1,23 +1,17 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "libev-${version}";
-  version="4.19";
+  name = "libev-4.20";
+
   src = fetchurl {
     url = "http://dist.schmorp.de/libev/Attic/${name}.tar.gz";
-    sha256 = "1jyw7qbl0spxqa0dccj9x1jsw7cj7szff43cq4acmklnra4mzz48";
+    sha256 = "17j47pbkr65a18mfvy2861p5k7w4pxmdgiw723ryfqd9gx636w7q";
   };
 
-  patches = [ ./noreturn.patch ];
-
-  # Version 4.19 is not valid C11 (which Clang default to)
-  # Check if this is still necessary on upgrade
-  NIX_CFLAGS_COMPILE = if stdenv.cc.isClang then "-std=c99" else null;
-
-  meta = {
-    description = "A high-performance event loop/event model with lots of features";
-    maintainers = [ stdenv.lib.maintainers.raskin ];
-    platforms = stdenv.lib.platforms.all;
-    license = stdenv.lib.licenses.bsd2; # or GPL2+
+  meta = with stdenv.lib; {
+    description = "A high-performance event loop/event model";
+    license = licenses.bsd2; # or GPL2+
+    maintainers = [ ];
+    platforms = platforms.all;
   };
 }
