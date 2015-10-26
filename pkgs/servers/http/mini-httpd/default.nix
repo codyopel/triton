e59c009817d7741a388a2286e0de56571c5141a6
Cody Opel <codyopel@gmail.com>
2015-10-26 13:07:31 -0400

    mini-httpd: 1.4 -> 1.5

--- pkgs/servers/http/mini-httpd/default.nix
+++ pkgs/servers/http/mini-httpd/default.nix
@@ -1,14 +1,18 @@
-{ stdenv, fetchurl, boost }:
+{ stdenv, fetchurl
+, boost
+}:
 
 stdenv.mkDerivation rec {
-  name = "mini-httpd-1.4";
+  name = "mini-httpd-1.5";
 
   src = fetchurl {
     url = "mirror://savannah/mini-httpd/${name}.tar.gz";
-    sha256 = "1i46klkx2ca1cgmlilajkx8gf7b7d7c2sj58llxfllh184pb6cpd";
+    sha256 = "1x4b6x40ymbaamqqq9p97lc0mnah4q7bza04fjs35c8agpm19zir";
   };
 
-  buildInputs = [ boost ];
+  buildInputs = [
+    boost
+  ];
 
   enableParallelBuilding = true;
 
@@ -16,11 +20,11 @@ stdenv.mkDerivation rec {
   # Please attempt removing when updating
   CPPFLAGS = "-DBOOST_ERROR_CODE_HEADER_ONLY -DBOOST_SYSTEM_NO_DEPRECATED";
 
-  meta = {
-    homepage = "http://mini-httpd.nongnu.org/";
-    description = "a minimalistic high-performance web server";
-    license = stdenv.lib.licenses.gpl3;
-    platforms = stdenv.lib.platforms.unix;
-    maintainers = [ stdenv.lib.maintainers.simons ];
+  meta = with stdenv.lib; {
+    description = "A minimalistic high-performance web server";
+    homepage = http://mini-httpd.nongnu.org/;
+    license = licenses.gpl3;
+    maintainers = [ ];
+    platforms = platforms.linux;
   };
 }
