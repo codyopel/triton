3234d144850f6370a03f48e8128e05ae7c2d9ec3
Cody Opel <codyopel@gmail.com>
2015-10-26 14:59:40 -0400

    tmux: 2.0 -> 2.1

--- pkgs/tools/misc/tmux/default.nix
+++ pkgs/tools/misc/tmux/default.nix
@@ -1,17 +1,27 @@
-{ stdenv, fetchurl, ncurses, libevent, pkgconfig }:
+{ stdenv, fetchurl
+, pkgconfig
+
+, libevent
+, ncurses
+}:
 
 stdenv.mkDerivation rec {
   name = "tmux-${version}";
-  version = "2.0";
+  version = "2.1";
 
   src = fetchurl {
     url = "https://github.com/tmux/tmux/releases/download/${version}/${name}.tar.gz";
-    sha256 = "0qnkda8kb747vmbldjpb23ksv9pq3s65xhh1ja5rdsmh8r24npvr";
+    sha256 = "0xk1mylsb08sf0w597mdgj9s6hxxjvjvjd6bngpjvvxwyixlwmii";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [
+    pkgconfig
+  ];
 
-  buildInputs = [ ncurses libevent ];
+  buildInputs = [
+    libevent
+    ncurses
+  ];
 
   configureFlags = [
     "--sysconfdir=/etc"
@@ -23,27 +33,11 @@ stdenv.mkDerivation rec {
     cp -v examples/bash_completion_tmux.sh $out/etc/bash_completion.d/tmux
   '';
 
-  meta = {
-    homepage = http://tmux.github.io/;
+  meta = with stdenv.lib; {
     description = "Terminal multiplexer";
-
-    longDescription =
-      '' tmux is intended to be a modern, BSD-licensed alternative to programs such as GNU screen. Major features include:
-
-          * A powerful, consistent, well-documented and easily scriptable command interface.
-          * A window may be split horizontally and vertically into panes.
-          * Panes can be freely moved and resized, or arranged into preset layouts.
-          * Support for UTF-8 and 256-colour terminals.
-          * Copy and paste with multiple buffers.
-          * Interactive menus to select windows, sessions or clients.
-          * Change the current window by searching for text in the target.
-          * Terminal locking, manually or after a timeout.
-          * A clean, easily extended, BSD-licensed codebase, under active development.
-      '';
-
-    license = stdenv.lib.licenses.bsd3;
-
-    platforms = stdenv.lib.platforms.unix;
-    maintainers = with stdenv.lib.maintainers; [ thammers ];
+    homepage = http://tmux.github.io/;
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ ];
+    platforms = platforms.unix;
   };
 }
