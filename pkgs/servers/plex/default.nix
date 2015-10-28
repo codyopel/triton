bb022679405fd5de7d0773be7788efec00a83a57
Cody Opel <codyopel@gmail.com>
2015-10-28 09:01:17 -0400

    plex: 0.9.12.11.1406 -> 0.9.12.13.1464

--- pkgs/servers/plex/default.nix
+++ pkgs/servers/plex/default.nix
@@ -1,24 +1,35 @@
-{ stdenv, fetchurl, rpmextract, glibc
-, dataDir ? "/var/lib/plex" # Plex's data directory must be baked into the package due to symlinks.
+{ stdenv, fetchurl
+, rpmextract
+, glibc
+# Plex's data directory must be baked into the package due to symlinks.
+, dataDir ? "/var/lib/plex"
 }:
 
 stdenv.mkDerivation rec {
   name = "plex-${version}";
-  version = "0.9.11.16.958";
-  vsnHash = "80f1748";
+  version = "0.9.11.13.1464";
+  vsnHash = "4ccd2ca";
 
   src = fetchurl {
     url = "https://downloads.plex.tv/plex-media-server/${version}-${vsnHash}/plexmediaserver-${version}-${vsnHash}.x86_64.rpm";
-    sha256 = "1wrl654nk10i9p01cgy9fqiqalxyl718qhp4kjnxvcwafayxkp26";
+    sha256 = "1gzq3ik3b23pl6i85d4abh3aqq710z5x258mjm7xai8rpyhvdp26";
   };
 
-  buildInputs = [ rpmextract glibc ];
-
-  phases = [ "unpackPhase" "installPhase" "fixupPhase" "distPhase" ];
+  phases = [
+    "unpackPhase"
+    "installPhase"
+    "fixupPhase"
+    "distPhase"
+  ];
 
   unpackPhase = ''
     rpmextract $src
   '';
+
+  buildInputs = [
+    rpmextract
+    glibc
+  ];
 
   installPhase = ''
     install -d $out/usr/lib
@@ -52,14 +63,10 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with stdenv.lib; {
+    description = "Media / DLNA server";
     homepage = http://plex.tv/;
     license = licenses.unfree;
+    maintainers = with maintainers; [ ];
     platforms = platforms.linux;
-    maintainers = with stdenv.lib.maintainers; [ forkk ];
-    description = "Media / DLNA server";
-    longDescription = ''
-      Plex is a media server which allows you to store your media and play it
-      back across many different devices.
-    '';
   };
 }
