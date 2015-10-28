4f7f133e592f89bafb6272e8f6475ea6df913d34
Cody Opel <codyopel@gmail.com>
2015-10-28 18:13:47 -0400

    zziplib: 0.13.58 -> 0.13.62

--- pkgs/development/libraries/zziplib/default.nix
+++ pkgs/development/libraries/zziplib/default.nix
@@ -1,11 +1,17 @@
-{ fetchurl, stdenv, perl, python, zip, xmlto, zlib }:
+{ fetchurl, stdenv
+, perl
+, python
+, xmlto
+, zip
+, zlib
+}:
 
 stdenv.mkDerivation rec {
-  name = "zziplib-0.13.58";
+  name = "zziplib-0.13.62";
 
   src = fetchurl {
     url = "mirror://sourceforge/zziplib/${name}.tar.bz2";
-    sha256 = "13j9f6i8rx0qd5m96iwrcha78h34qpfk5qzi7cv098pms6gq022m";
+    sha256 = "0nsjqxw017hiyp524p9316283jlf5piixc1091gkimhz38zh7f51";
   };
 
   patchPhase = ''
@@ -16,22 +22,12 @@ stdenv.mkDerivation rec {
 
   doCheck = true;
 
+  enableParallelBuilding = true;
+
   meta = with stdenv.lib; {
     description = "Library to extract data from files archived in a zip file";
-
-    longDescription = ''
-      The zziplib library is intentionally lightweight, it offers the ability
-      to easily extract data from files archived in a single zip
-      file.  Applications can bundle files into a single zip archive and
-      access them.  The implementation is based only on the (free) subset of
-      compression with the zlib algorithm which is actually used by the
-      zip/unzip tools.
-    '';
-
-    license = with licenses; [ lgpl2Plus mpl11 ];
-
     homepage = http://zziplib.sourceforge.net/;
-
+    license = with licenses; [ lgpl2Plus mpl11 ];
     maintainers = [ ];
     platforms = python.meta.platforms;
   };
