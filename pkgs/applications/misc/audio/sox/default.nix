cf6d849bef27c6391e3d9bc10bec0d425d06ad76
Cody Opel <codyopel@gmail.com>
2015-10-28 18:11:04 -0400

    sox: refactor

--- pkgs/applications/misc/audio/sox/default.nix
+++ pkgs/applications/misc/audio/sox/default.nix
@@ -1,17 +1,17 @@
-{ lib, stdenv, fetchurl
-, enableAlsa ? true, alsaLib ? null
-, enableLibao ? true, libao ? null
-, enableLame ? false, lame ? null
-, enableLibmad ? true, libmad ? null
-, enableLibogg ? true, libogg ? null, libvorbis ? null
-, enableFLAC ? true, flac ? null
-, enablePNG ? true, libpng ? null
-, enableLibsndfile ? true, libsndfile ? null
-# amrnb and amrwb are unfree, disabled by default
-, enableAMR ? false, amrnb ? null, amrwb ? null
+{ stdenv, fetchurl
+  # Optional
+, alsaLib
+, amrnb
+, amrwb
+, flac
+, lame
+, libao
+, libmad
+, libogg
+, libpng
+, libsndfile
+, libvorbis
 }:
-
-with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "sox-14.4.2";
@@ -21,24 +21,25 @@ stdenv.mkDerivation rec {
     sha256 = "0v2znlxkxxcd3f48hf3dx9pq7i6fdhb62kgj7wv8xggz8f35jpxl";
   };
 
-  buildInputs =
-    optional (enableAlsa && stdenv.isLinux) alsaLib ++
-    optional enableLibao libao ++
-    optional enableLame lame ++
-    optional enableLibmad libmad ++
-    optionals enableLibogg [ libogg libvorbis ] ++
-    optional enableFLAC flac ++
-    optional enablePNG libpng ++
-    optional enableLibsndfile libsndfile ++
-    optionals enableAMR [ amrnb amrwb ];
+  buildInputs = [
+    alsaLib
+    amrnb
+    amrwb
+    flac
+    lame
+    libao
+    libmad
+    libogg
+    libpng
+    libsndfile
+    libvorbis
+  ];
 
-  enableParallelBuilding = true;
-
-  meta = {
+  meta = with stdenv.lib; {
     description = "Sample Rate Converter for audio";
     homepage = http://sox.sourceforge.net/;
-    maintainers = [ lib.maintainers.marcweber ];
-    license = if enableAMR then lib.licenses.unfree else lib.licenses.gpl2Plus;
-    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+    maintainers = [ ];
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux;
   };
 }
