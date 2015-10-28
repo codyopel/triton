baeaca59159027da69a67cc3b3db9df9f9fc1310
Cody Opel <codyopel@gmail.com>
2015-10-28 10:45:00 -0400

    gparted 0.23.0 -> 0.24.0

--- pkgs/tools/misc/gparted/default.nix
+++ pkgs/tools/misc/gparted/default.nix
@@ -1,30 +1,50 @@
-{ stdenv, fetchurl, parted, gtk, glib, intltool, gettext, libuuid
-, pkgconfig, gtkmm, libxml2, hicolor_icon_theme
+{ stdenv, fetchurl
+, gettext
+, intltool
+, pkgconfig
+
+, parted
+, gtk2
+, glib
+, libuuid
+, gtkmm
+, libxml2
+, hicolor_icon_theme
 }:
 
 stdenv.mkDerivation rec {
-  name = "gparted-0.23.0";
+  name = "gparted-0.24.0";
 
   src = fetchurl {
-    sha256 = "0m57bni3nkbbqq920ydzvasy2qc5j6w6bdssyn12jk4157gxvlbz";
     url = "mirror://sourceforge/gparted/${name}.tar.bz2";
+    sha256 = "0q6d1s9f4qgdivj4vm9w87qmdfyq8s65jzkhv05rp9cl72rqlf82";
   };
 
-  configureFlags = "--disable-doc";
+  configureFlags = [
+    "--disable-doc"
+  ];
 
-  buildInputs = [ parted gtk glib libuuid gtkmm libxml2 hicolor_icon_theme ];
-  nativeBuildInputs = [ intltool gettext pkgconfig ];
+  nativeBuildInputs = [
+    gettext
+    intltool
+    pkgconfig
+  ];
+
+  buildInputs = [
+    parted
+    gtk2
+    glib
+    libuuid
+    gtkmm
+    libxml2
+    hicolor_icon_theme
+  ];
 
   meta = with stdenv.lib; {
     description = "Graphical disk partitioning tool";
-    longDescription = ''
-      GNOME Partition Editor for creating, reorganizing, and deleting disk
-      partitions. GParted enables you to change the partition organization
-      while preserving the partition contents.
-    '';
     homepage = http://gparted.org;
     license = licenses.gpl2Plus;
+    maintainers = with maintainers; [ ];
     platforms = platforms.linux;
-    maintainers = with maintainers; [ nckx ];
   };
 }
