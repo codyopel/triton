68730d2cd44eacaadfb60b4753ba843b9f67bdd0
Cody Opel <codyopel@gmail.com>
2015-10-29 18:04:25 -0400

    cairomm: 1.11.2 -> 1.12.0

--- pkgs/development/libraries/cairomm/default.nix
+++ pkgs/development/libraries/cairomm/default.nix
@@ -1,36 +1,34 @@
-{ fetchurl, stdenv, pkgconfig, cairo, xlibsWrapper, fontconfig, freetype, libsigcxx }:
+{ fetchurl, stdenv
+, pkgconfig
+, cairo
+, xlibsWrapper
+, fontconfig
+, freetype
+, libsigcxx
+}:
 
 stdenv.mkDerivation rec {
-  name = "cairomm-1.11.2";
+  name = "cairomm-1.12.0";
 
   src = fetchurl {
     url = "http://cairographics.org/releases/${name}.tar.gz";
-    sha1 = "35e190a03f760924bece5dc1204cc36b3583c806";
+    sha256 = "1k3lb3jwnk5nm4s5cfm5kk8kl4b066chis4inws6k5yxdzn5lhsh";
   };
 
-  buildInputs = [ pkgconfig ];
-
-  propagatedBuildInputs = [ cairo xlibsWrapper fontconfig freetype libsigcxx ];
+  buildInputs = [
+    pkgconfig
+    cairo
+    xlibsWrapper
+    fontconfig
+    freetype
+    libsigcxx
+  ];
 
   enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     description = "A 2D graphics library with support for multiple output devices";
-
-    longDescription = ''
-      Cairo is a 2D graphics library with support for multiple output
-      devices.  Currently supported output targets include the X
-      Window System, Quartz, Win32, image buffers, PostScript, PDF,
-      and SVG file output.  Experimental backends include OpenGL
-      (through glitz), XCB, BeOS, OS/2, and DirectFB.
-
-      Cairo is designed to produce consistent output on all output
-      media while taking advantage of display hardware acceleration
-      when available (e.g., through the X Render Extension).
-    '';
-
     homepage = http://cairographics.org/;
-
     license = with licenses; [ lgpl2Plus mpl10 ];
   };
 }
