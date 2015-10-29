25ddbf6a944100c0901132726568cb70d8bfceaa
Cody Opel <codyopel@gmail.com>
2015-10-29 19:12:06 -0400

    sassc: 3.2.4 -> 3.3.1

--- pkgs/development/tools/sassc/default.nix
+++ pkgs/development/tools/sassc/default.nix
@@ -1,27 +1,35 @@
-{ stdenv, fetchurl, autoreconfHook, libsass }:
+{ stdenv, fetchurl
+, autoreconfHook
+
+, libsass
+}:
 
 stdenv.mkDerivation rec {
   name = "sassc-${version}";
-  version = "3.2.4";
+  version = "3.3.1";
 
   src = fetchurl {
     url = "https://github.com/sass/sassc/archive/${version}.tar.gz";
-    sha256 = "0ksdfv9ff5smba4vbwr1wqf3bp908rnprkp6lfssj85h9ciqq896";
+    sha256 = "0694gdjd7983g15ccdv4znr0wccy9a5jb93x5rp965vasip8d6z9";
   };
 
   patchPhase = ''
     export SASSC_VERSION=${version}
   '';
 
-  nativeBuildInputs = [ autoreconfHook ];
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
 
-  buildInputs = [ libsass ];
+  buildInputs = [
+    libsass
+  ];
 
   meta = with stdenv.lib; {
     description = "A front-end for libsass";
     homepage = https://github.com/sass/sassc/;
     license = licenses.mit;
-    maintainers = with maintainers; [ codyopel pjones ];
+    maintainers = with maintainers; [ codyopel ];
     platforms = platforms.unix;
   };
 }
