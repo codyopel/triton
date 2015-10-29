625fbc592f85584b0c4399d93e05e883552c1410
Cody Opel <codyopel@gmail.com>
2015-10-29 18:05:39 -0400

    atkmm: 2.22.7 -> 2.24.1

--- pkgs/development/libraries/atkmm/default.nix
+++ pkgs/development/libraries/atkmm/default.nix
@@ -1,16 +1,31 @@
-{ stdenv, fetchurl, atk, glibmm, pkgconfig }:
+{ stdenv, fetchurl
+, atk
+, glibmm
+, pkgconfig
+}:
 
 stdenv.mkDerivation rec {
-  name = "atkmm-2.22.7";
+  name = "atkmm-${version}";
+  versionMajor = "2.24";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/atkmm/2.22/${name}.tar.xz";
-    sha256 = "06zrf2ymml2dzp53sss0d4ch4dk9v09jm8rglnrmwk4v81mq9gxz";
+    url = "mirror://gnome/sources/atkmm/${versionMajor}/${name}.tar.xz";
+    sha256 = "08zd6s5c1q90wm8310mdrb5f2lj8v63wxihrybwyw13xlf6ivi16";
   };
 
-  propagatedBuildInputs = [ atk glibmm ];
+  nativeBuildInputs = [
+    pkgconfig
+  ];
 
-  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [
+    atk
+  ];
+
+  propagatedBuildInputs = [
+    glibmm
+  ];
 
   enableParallelBuilding = true;
 }
