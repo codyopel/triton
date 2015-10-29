5e87c282a1fe0f5bde7c45ad9f82e53294b3af36
Cody Opel <codyopel@gmail.com>
2015-10-28 20:35:44 -0400

    go: refactor

--- pkgs/development/compilers/go/1.5.nix
+++ pkgs/development/compilers/go/1.5.nix
@@ -1,7 +1,27 @@
-{ stdenv, lib, fetchurl, tzdata, iana_etc, go_1_4, runCommand
-, perl, which, pkgconfig, patch
+{ stdenv, fetchurl
+, go_1_4
+, patch
+, perl
+, pkgconfig
+, which
+
+, tzdata
+, iana_etc
+, runCommand
 , pcre
-, Security }:
+}:
+
+with {
+  inherit (stdenv)
+    isi686
+    isx86_64
+    isArm
+    isLinux;
+  inherit (stdenv.lib)
+    optional
+    optionals
+    optionalString;
+};
 
 let
   goBootstrap = runCommand "go-bootstrap" {} ''
@@ -22,10 +42,7 @@ stdenv.mkDerivation rec {
     sha256 = "1xd1nd1li7pdl72vq8zkh6m7ms3ajgyqz9a5d61gagm0cgj8zilz";
   };
 
-  # perl is used for testing go vet
-  nativeBuildInputs = [ perl which pkgconfig patch ];
-  buildInputs = [ pcre ];
-  propagatedBuildInputs = lib.optional stdenv.isDarwin Security;
+  setupHook = ./setup-hook.sh;
 
   # I'm not sure what go wants from its 'src', but the go installation manual
   # describes an installation keeping the src.
@@ -33,6 +50,11 @@ stdenv.mkDerivation rec {
     mkdir -p $out/share
     cd $out/share
   '';
+
+  patches = [
+    ./cacert-1.5.patch
+    ./remove-tools-1.5.patch
+  ];
 
   prePatch = ''
     # Ensure that the source directory is named go
@@ -49,7 +71,8 @@ stdenv.mkDerivation rec {
     rm src/net/{listen_test.go,parse_test.go,port_test.go}
     rm src/syscall/exec_linux_test.go
     # !!! substituteInPlace does not seems to be effective.
-    # The os test wants to read files in an existing path. Just don't let it be /usr/bin.
+    # The os test wants to read files in an existing path. Just don't let
+    # it be /usr/bin.
     sed -i 's,/usr/bin,'"`pwd`", src/os/os_test.go
     sed -i 's,/bin/pwd,'"`type -P pwd`", src/os/os_test.go
     # Disable the unix socket test
@@ -63,37 +86,43 @@ stdenv.mkDerivation rec {
     # Remove the coverage test as we have removed this utility
     sed -i '/TestCoverageWithCgo/areturn' src/cmd/go/go_test.go
 
-    sed -i 's,/etc/protocols,${iana_etc}/etc/protocols,' src/net/lookup_unix.go
-  '' + lib.optionalString stdenv.isLinux ''
-    sed -i 's,/usr/share/zoneinfo/,${tzdata}/share/zoneinfo/,' src/time/zoneinfo_unix.go
-  '' + lib.optionalString stdenv.isDarwin ''
-    sed -i 's,"/etc","'"$TMPDIR"'",' src/os/os_test.go
-    sed -i 's,/_go_os_test,'"$TMPDIR"'/_go_os_test,' src/os/path_test.go
-    sed -i '/TestRead0/areturn' src/os/os_test.go
-    sed -i '/TestSystemRoots/areturn' src/crypto/x509/root_darwin_test.go
-
-    touch $TMPDIR/group $TMPDIR/hosts $TMPDIR/passwd
+    sed -i 's,/etc/protocols,${iana_etc}/etc/protocols,' \
+      src/net/lookup_unix.go
+  '' + optionalString isLinux ''
+    sed -i 's,/usr/share/zoneinfo/,${tzdata}/share/zoneinfo/,' \
+      src/time/zoneinfo_unix.go
   '';
 
-  patches = [
-    ./cacert-1.5.patch
-    ./remove-tools-1.5.patch
+  # perl is used for testing go vet
+  nativeBuildInputs = [
+    patch
+    perl
+    pkgconfig
+    which
   ];
 
-  GOOS = if stdenv.isDarwin then "darwin" else "linux";
-  GOARCH = if stdenv.isDarwin then "amd64"
-           else if stdenv.system == "i686-linux" then "386"
-           else if stdenv.system == "x86_64-linux" then "amd64"
-           else if stdenv.isArm then "arm"
-           else throw "Unsupported system";
-  GOARM = stdenv.lib.optionalString (stdenv.system == "armv5tel-linux") "5";
+  buildInputs = [
+    pcre
+  ];
+
+  GOOS = "linux";
+  GOARCH =
+    if isi686 then
+      "386"
+    else if isx86_64 then
+      "amd64"
+    else if isArm then
+      "arm"
+    else
+      throw "Unsupported system";
+  GOARM = optionalString (stdenv.system == "armv5tel-linux") "5";
   GO386 = 387; # from Arch: don't assume sse2 on i686
   CGO_ENABLED = 1;
   GOROOT_BOOTSTRAP = "${goBootstrap}/share/go";
 
-  # The go build actually checks for CC=*/clang and does something different, so we don't
-  # just want the generic `cc` here.
-  CC = if stdenv.isDarwin then "clang" else "cc";
+  # The go build actually checks for CC=*/clang and does something different,
+  # so we don't just want the generic `cc` here.
+  CC = if stdenv.cc.isClang then "clang" else "cc";
 
   installPhase = ''
     mkdir -p "$out/bin"
@@ -109,16 +138,16 @@ stdenv.mkDerivation rec {
     rm -r $out/share/go/pkg/bootstrap
   '';
 
-  setupHook = ./setup-hook.sh;
-
-  disallowedReferences = [ go_1_4 ];
+  disallowedReferences = [
+    go_1_4
+  ];
 
   meta = with stdenv.lib; {
-    branch = "1.5";
-    homepage = http://golang.org/;
     description = "The Go Programming language";
+    homepage = http://golang.org/;
     license = licenses.bsd3;
-    maintainers = with maintainers; [ cstrahan wkennington ];
-    platforms = platforms.linux ++ platforms.darwin;
+    maintainers = with maintainers; [ wkennington ];
+    platforms = platforms.linux;
+    branch = "1.5";
   };
 }
