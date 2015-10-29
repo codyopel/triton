82e086c4e579727d9b4c94d2114d20660ccb380f
Cody Opel <codyopel@gmail.com>
2015-10-28 23:29:43 -0400

    lzip: 1.16 -> 1.17

--- pkgs/tools/compression/lzip/default.nix
+++ pkgs/tools/compression/lzip/default.nix
@@ -1,25 +1,29 @@
-{ stdenv, fetchurl, texinfo }:
+{ stdenv, fetchurl
+, texinfo
+}:
 
 stdenv.mkDerivation rec {
-  name = "lzip-1.16";
-
-  buildInputs = [ texinfo ];
+  name = "lzip-1.17";
 
   src = fetchurl {
     url = "mirror://savannah/lzip/${name}.tar.gz";
-    sha256 = "0l9724rw1l3hg2ldr3n7ihqich4m9nc6y7l302bvdj4jmxdw530j";
+    sha256 = "0lh3x964jjldx3piax6c2qzlhfiir5i6rnrcn8ri44rk19g8ahwl";
   };
 
   configureFlags = "CPPFLAGS=-DNDEBUG CFLAGS=-O3 CXXFLAGS=-O3";
 
+  buildInputs = [
+    texinfo
+  ];
+
   doCheck = true;
+  enableParallelBuilding = true;
 
-  meta = {
+  meta = with stdenv.lib; {
+    description = "A lossless data compressor based on LZMA";
     homepage = "http://www.nongnu.org/lzip/lzip.html";
-    description = "a lossless data compressor based on the LZMA algorithm";
-    license = stdenv.lib.licenses.gpl3Plus;
-
-    platforms = stdenv.lib.platforms.unix;
-    maintainers = [ stdenv.lib.maintainers.simons ];
+    license = licenses.gpl3Plus;
+    maintainers = [ ];
+    platforms = platforms.unix;
   };
 }
