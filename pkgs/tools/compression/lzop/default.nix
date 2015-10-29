724714d69a2c88db63229adc5264b0c3abc66508
Cody Opel <codyopel@gmail.com>
2015-10-28 21:26:07 -0400

    lzop: refactor

--- pkgs/tools/compression/lzop/default.nix
+++ pkgs/tools/compression/lzop/default.nix
@@ -1,17 +1,20 @@
-{stdenv, fetchurl, lzo}:
+{ stdenv, fetchurl, lzo }:
 
 stdenv.mkDerivation {
   name = "lzop-1.03";
+
   src = fetchurl {
-    url = http://www.lzop.org/download/lzop-1.03.tar.gz;
+    url = "http://www.lzop.org/download/${name}.tar.gz";
     sha256 = "1jdjvc4yjndf7ihmlcsyln2rbnbaxa86q4jskmkmm7ylfy65nhn1";
   };
 
-  buildInputs = [ lzo ];
+  buildInputs = [
+    lzo
+  ];
 
   meta = with stdenv.lib; {
-    homepage = http://www.lzop.org;
     description = "Fast file compressor";
+    homepage = http://www.lzop.org;
     license = licenses.gpl2;
   };
 }
