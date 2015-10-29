827039d5178c118696a42b64184ff180d716fb2b
Cody Opel <codyopel@gmail.com>
2015-10-29 18:06:37 -0400

    atk: refactor

--- pkgs/development/libraries/atk/default.nix
+++ pkgs/development/libraries/atk/default.nix
@@ -1,46 +1,46 @@
-{ stdenv, fetchurl, pkgconfig, perl, glib, libintlOrEmpty, gobjectIntrospection }:
+{ stdenv, fetchurl
+, gettext
+, pkgconfig
+, perl
 
-let
-  ver_maj = "2.18";
-  ver_min = "0";
-in
+, glib
+, libintlOrEmpty
+, gobjectIntrospection
+}:
+
 stdenv.mkDerivation rec {
-  name = "atk-${ver_maj}.${ver_min}";
+  name = "atk-${version}";
+  versionMajor = "2.18";
+  versionMinor = "0";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/atk/${ver_maj}/${name}.tar.xz";
-    sha256 = "ce6c48d77bf951083029d5a396dd552d836fff3c1715d3a7022e917e46d0c92b";
+    url = "mirror://gnome/sources/atk/${versionMajor}/${name}.tar.xz";
+    sha256 = "0ay9s137x49f0akx658p7kznz0rdapfrd8ym54q0hlgrggblhv6f";
   };
 
-  buildInputs = libintlOrEmpty;
+  nativeBuildInputs = [
+    gettext
+    perl
+    pkgconfig
+  ];
 
-  nativeBuildInputs = [ pkgconfig perl ];
+  propagatedBuildInputs = [
+    glib
+  ];
 
-  propagatedBuildInputs = [ glib gobjectIntrospection /*ToDo: why propagate*/ ];
-
-  #doCheck = true; # no checks in there (2.10.0)
+  buildInputs = [
+    gobjectIntrospection
+    libintlOrEmpty
+  ];
 
   postInstall = "rm -rf $out/share/gtk-doc";
 
-  enableParallelBuilding = true;
-
-  meta = {
+  meta = with stdenv.lib; {
     description = "Accessibility toolkit";
-
-    longDescription = ''
-      ATK is the Accessibility Toolkit.  It provides a set of generic
-      interfaces allowing accessibility technologies such as screen
-      readers to interact with a graphical user interface.  Using the
-      ATK interfaces, accessibility tools have full access to view and
-      control running applications.
-    '';
-
     homepage = http://library.gnome.org/devel/atk/;
-
     license = stdenv.lib.licenses.lgpl2Plus;
-
-    maintainers = with stdenv.lib.maintainers; [ raskin urkud ];
-    platforms = stdenv.lib.platforms.linux ++ stdenv.lib.platforms.darwin;
+    maintainers = with maintainers; [ ];
+    platforms = platforms.linux;
   };
-
 }
