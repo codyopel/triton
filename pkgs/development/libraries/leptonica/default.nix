f130987b1255eaab31b355b73f62f265deac6d72
Cody Opel <codyopel@gmail.com>
2015-10-29 14:22:39 -0400

    leptonica: 1.69 -> 1.72

--- pkgs/development/libraries/leptonica/default.nix
+++ pkgs/development/libraries/leptonica/default.nix
@@ -1,19 +1,54 @@
-{stdenv, fetchurl, libpng, libtiff, libjpeg, zlib}:
+{ stdenv, fetchurl
+# Optional
+, giflib
+, libjpeg
+, libpng
+, libtiff
+, libwebp
+, openjpeg
+, zlib
+, programs ? true
+}:
 
-stdenv.mkDerivation {
-  name = "leptonica-1.69";
+with {
+  inherit (stdenv.lib)
+    enFlag
+    wtFlag;
+};
+
+stdenv.mkDerivation rec {
+  name = "leptonica-1.72";
 
   src = fetchurl {
-    url = http://www.leptonica.org/source/leptonica-1.69.tar.gz;
-    sha256 = "0bd7w0zpmwwfn1cnrlyzjw3jf8x59r0rhdmvk7rigr57rnfnddry";
+    url = "http://www.leptonica.org/source/${name}.tar.gz";
+    sha256 = "0mhzvqs0im04y1cpcc1yma70hgdac1frf33h73m9z3356bfymmbr";
   };
 
-  buildInputs = [ libpng libtiff libjpeg zlib ];
+  configureFlags = [
+    (enFlag "programs" programs null)
+    (wtFlag "giflib" (giflib != null) null)
+    (wtFlag "jpeg" (openjpeg != null) null)
+    (wtFlag "libopenjpeg" (openjpeg != null) null)
+    (wtFlag "libpng" (libpng != null) null)
+    (wtFlag "libtiff" (libtiff != null) null)
+    (wtFlag "libwebp" (libwebp != null) null)
+    (wtFlag "zlib" (zlib != null) null)
+  ];
 
-  meta = {
+  buildInputs = [
+    giflib
+    libjpeg
+    libpng
+    libtiff
+    libwebp
+    openjpeg
+    zlib
+  ];
+
+  meta = with stdenv.lib; {
     description = "Image processing and analysis library";
     homepage = http://www.leptonica.org/;
     # Its own license: http://www.leptonica.org/about-the-license.html
-    license = stdenv.lib.licenses.free;
+    license = licenses.free;
   };
 }
