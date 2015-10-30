c90392ca10482a0ceacd03899653d92dacf42abb
Cody Opel <codyopel@gmail.com>
2015-10-30 15:59:03 -0400

    gdk-pixbuf: 2.32.0 -> 2.32.1

--- pkgs/development/libraries/gdk-pixbuf/default.nix
+++ pkgs/development/libraries/gdk-pixbuf/default.nix
@@ -1,40 +1,57 @@
-{ stdenv, fetchurl, pkgconfig, glib, libtiff, libjpeg, libpng, libX11
-, jasper, libintlOrEmpty, gobjectIntrospection, doCheck ? false }:
+{ stdenv, fetchurl
+, pkgconfig
+, glib
+, libtiff
+, libjpeg
+, libpng
+, libX11
+, jasper
+, libintlOrEmpty
+, gobjectIntrospection
+}:
 
-let
-  ver_maj = "2.32";
-  ver_min = "0";
-in
 stdenv.mkDerivation rec {
-  name = "gdk-pixbuf-${ver_maj}.${ver_min}";
+  name = "gdk-pixbuf-${version}";
+  versionMajor = "2.32";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gdk-pixbuf/${ver_maj}/${name}.tar.xz";
-    sha256 = "0rqvj5gcs2zfyyg9llm289b0xkj4mrhzxfjpjja0wx1m6vn5axjk";
+    url = "mirror://gnome/sources/gdk-pixbuf/${versionMajor}/${name}.tar.xz";
+    sha256 = "1g7kjxv67jcdasi14n7jan4icrnnppd1m99wrdmpv32k4m7vfcj4";
   };
 
   setupHook = ./setup-hook.sh;
 
-  # !!! We might want to factor out the gdk-pixbuf-xlib subpackage.
-  buildInputs = [ libX11 libintlOrEmpty ];
+  configureFlags = [
+    "--with-libjasper"
+    "--with-x11"
+    "--enable-introspection=yes"
+  ];
 
-  nativeBuildInputs = [ pkgconfig gobjectIntrospection ];
+  nativeBuildInputs = [
+    gobjectIntrospection
+    libintlOrEmpty
+    pkgconfig
+  ];
 
-  propagatedBuildInputs = [ glib libtiff libjpeg libpng jasper ];
-
-  configureFlags = "--with-libjasper --with-x11"
-    + stdenv.lib.optionalString (gobjectIntrospection != null) " --enable-introspection=yes"
-    ;
-
-  # The tests take an excessive amount of time (> 1.5 hours) and memory (> 6 GB).
-  inherit (doCheck);
+  buildInputs = [
+    glib
+    jasper
+    libjpeg
+    libpng
+    libtiff
+    libX11
+  ];
 
   postInstall = "rm -rf $out/share/gtk-doc";
 
-  meta = {
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
     description = "A library for image loading and manipulation";
     homepage = http://library.gnome.org/devel/gdk-pixbuf/;
-    maintainers = [ stdenv.lib.maintainers.eelco ];
-    platforms = stdenv.lib.platforms.unix;
+    maintainers = [ ];
+    platforms = platforms.linux;
   };
 }
