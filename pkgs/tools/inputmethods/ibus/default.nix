c825d02f468da4b43e72f6ff6b468f4ccfad9253
Cody Opel <codyopel@gmail.com>
2015-10-30 14:41:26 -0400

    ibus: 1.5.10 -> 1.5.11

--- pkgs/tools/inputmethods/ibus/default.nix
+++ pkgs/tools/inputmethods/ibus/default.nix
@@ -1,22 +1,54 @@
-{ stdenv, fetchurl, makeWrapper, python, intltool, pkgconfig
-, gnome3, atk, pygobject3, dbus, libnotify, isocodes, gobjectIntrospection, wayland }:
+{ stdenv, fetchurl
+, makeWrapper
+, python
+, intltool
+, pkgconfig
+, gnome3
+, atk
+, pygobject
+, dbus
+, libnotify
+, isocodes
+, gobjectIntrospection
+, wayland
+, xorg
+}:
 
 stdenv.mkDerivation rec {
   name = "ibus-${version}";
-  version = "1.5.10";
+  version = "1.5.11";
 
   src = fetchurl {
     url = "https://github.com/ibus/ibus/releases/download/${version}/${name}.tar.gz";
-    sha256 = "152mdzi9hr246spnn7bkb4gy16x30082xwq460gmm1q2cs0bf08x";
+    sha256 = "1g26llizd26h9sfz4xdq8krhz19hn08pirvfbkk3g89ri8lmm6a9";
   };
 
-  configureFlags = "--disable-gconf --enable-dconf --disable-memconf --enable-ui --enable-python-library";
+  configureFlags = [
+    "--disable-gconf"
+    "--enable-dconf"
+    "--disable-memconf"
+    "--enable-ui"
+    "--enable-python-library"
+  ];
 
   buildInputs = [
-    makeWrapper python gnome3.glib wayland
-    intltool pkgconfig gnome3.gtk2
-    gnome3.gtk3 dbus gnome3.dconf gnome3.gconf
-    libnotify isocodes gobjectIntrospection
+    makeWrapper
+    python
+    gnome3.glib
+    wayland
+    intltool
+    pkgconfig
+    gnome3.gtk2
+    gnome3.gtk3
+    dbus
+    gnome3.dconf
+    gnome3.gconf
+    libnotify
+    isocodes
+    gobjectIntrospection
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXi
   ];
 
   preConfigure = ''
@@ -25,18 +57,19 @@ stdenv.mkDerivation rec {
   '';
 
   preFixup = ''
-    for f in "$out"/bin/*; do
-      wrapProgram "$f" --prefix XDG_DATA_DIRS : "$out/share:$GSETTINGS_SCHEMAS_PATH" \
-                       --prefix PYTHONPATH : "$(toPythonPath ${pygobject3})" \
-                       --prefix LD_LIBRARY_PATH : "${gnome3.gtk3}/lib:${atk}/lib:$out/lib" \
-                       --prefix GI_TYPELIB_PATH : "$GI_TYPELIB_PATH:$out/lib/girepository-1.0" \
-                       --prefix GIO_EXTRA_MODULES : "${gnome3.dconf}/lib/gio/modules"
+    for f in "$out"/bin/* ; do
+      wrapProgram "$f" \
+        --prefix XDG_DATA_DIRS : "$out/share:$GSETTINGS_SCHEMAS_PATH" \
+        --prefix PYTHONPATH : "$(toPythonPath ${pygobject})" \
+        --prefix LD_LIBRARY_PATH : "${gnome3.gtk3}/lib:${atk}/lib:$out/lib" \
+        --prefix GI_TYPELIB_PATH : "$GI_TYPELIB_PATH:$out/lib/girepository-1.0" \
+        --prefix GIO_EXTRA_MODULES : "${gnome3.dconf}/lib/gio/modules"
     done
   '';
 
   meta = {
+    description = "Intelligent Input Bus for Linux";
     homepage = https://github.com/ibus/ibus;
-    description = "Intelligent Input Bus for Linux / Unix OS";
     platforms = stdenv.lib.platforms.linux;
   };
 }
