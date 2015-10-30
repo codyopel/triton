474b3350c998d3e6a197d80b113311daf758e0de
Cody Opel <codyopel@gmail.com>
2015-10-30 15:53:23 -0400

    pulseaudio: 7.0 -> 7.1

--- pkgs/servers/pulseaudio/default.nix
+++ pkgs/servers/pulseaudio/default.nix
@@ -1,21 +1,59 @@
-{ stdenv, fetchurl, pkgconfig, intltool, automake, autoconf, libtool
-, json_c, libsndfile, gettext, check
+{ stdenv, fetchurl
+, pkgconfig
+, intltool
+, automake
+, autoconf
+, libtool
+, json_c
+, libsndfile
+, gettext
+, check
 
 # Optional Dependencies
-, xorg ? null, libcap ? null, valgrind ? null, oss ? null, coreaudio ? null
-, alsaLib ? null, esound ? null, glib ? null, gtk3 ? null, gconf ? null
-, avahi ? null, libjack2 ? null, libasyncns ? null, lirc ? null, dbus ? null
-, sbc ? null, bluez5 ? null, udev ? null, openssl ? null, fftw ? null
-, speexdsp ? null, soxr ? null, systemd ? null, webrtc-audio-processing ? null
+, alsaLib ? null
+, avahi ? null
+, bluez5 ? null
+, coreaudio ? null
+, dbus ? null
+, esound ? null
+, fftw ? null
+, gconf ? null
+, glib ? null
+, gtk3 ? null
+, libasyncns ? null
+, libcap ? null
+, libjack2 ? null
+, lirc ? null
+, openssl ? null
+, oss ? null
+, sbc ? null
+, soxr ? null
+, speexdsp ? null
+, systemd ? null
+, udev ? null
+, valgrind ? null
+, webrtc-audio-processing ? null
+, xorg ? null
 
 # Database selection
-, tdb ? null, gdbm ? null
+, tdb ? null
+, gdbm ? null
 
 # Extra options
 , prefix ? ""
 }:
 
-with stdenv;
+with {
+  inherit (stdenv)
+    shouldUsePkg;
+  inherit (stdenv.lib)
+    enFlag
+    otFlag
+    wtFlag
+    optionals
+    optionalString;
+};
+
 let
   libOnly = prefix == "lib";
 
@@ -47,7 +85,11 @@ let
   optSpeexdsp = shouldUsePkg speexdsp;
   optSoxr = if libOnly then null else shouldUsePkg soxr;
   optSystemd = shouldUsePkg systemd;
-  optWebrtc-audio-processing = if libOnly then null else shouldUsePkg webrtc-audio-processing;
+  optWebrtc-audio-processing =
+    if libOnly then
+      null
+    else
+      shouldUsePkg webrtc-audio-processing;
   hasWebrtc = if libOnly then null else optWebrtc-audio-processing != null;
 
   # Pick a database to use
@@ -59,29 +101,73 @@ let
     simple = null;
   }.${databaseName};
 in
-with stdenv.lib;
+
 stdenv.mkDerivation rec {
   name = "${prefix}pulseaudio-${version}";
-  version = "7.0";
+  version = "7.1";
 
   src = fetchurl {
-    url = "http://freedesktop.org/software/pulseaudio/releases/pulseaudio-${version}.tar.xz";
-    sha256 = "1yp8x8z4wigrzik131kjdyhn7hznazvbkbp2zz1vy9l9gqvy26na";
+    url = "http://freedesktop.org/software/pulseaudio/releases/"
+        + "pulseaudio-${version}.tar.xz";
+    sha256 = "1ndrac0j528lsg3b8wcsgvzds38ml0ja4m57xsn953rj51552rz6";
   };
 
-  patches = [ ./caps-fix.patch ];
+  patches = [
+    ./caps-fix.patch
+  ];
 
-  nativeBuildInputs = [ pkgconfig intltool automake autoconf libtool ];
-  propagatedBuildInputs = [ optLibcap ];
-  buildInputs = [
-    json_c libsndfile gettext check database
-
-    valgrind optOss optCoreaudio optAlsaLib optEsound optGlib
-    optGtk3 optGconf optAvahi optLibjack2 optLibasyncns optLirc optDbus optUdev
-    optOpenssl optFftw optSpeexdsp optSoxr optSystemd optWebrtc-audio-processing
-  ] ++ optionals hasXlibs (with xorg; [
-      libX11 libxcb libICE libSM libXtst xextproto libXi
-    ]) ++ optionals (optBluez5 != null) [ optBluez5 optSbc ];
+  configureFlags = [
+    (otFlag "localstatedir" "/var")
+    (otFlag "sysconfdir" "/etc")
+    (enFlag "atomic-arm-memory-barrier" false null) # TODO: Enable on armv8
+    (enFlag "neon-opt" false null) # TODO: Enable on armv8
+    (enFlag "x11" hasXlibs null)
+    (wtFlag "caps" hasCaps optLibcap)
+    (enFlag "tests" true null)
+    (enFlag "samplerate" false null) # Deprecated
+    (wtFlag "database" true databaseName)
+    (enFlag "oss-output" hasOss null)
+    (enFlag "oss-wrapper" true null) # Does not use OSS
+    (enFlag "coreaudio-output" (optCoreaudio != null) null)
+    (enFlag "alsa" (optAlsaLib != null) null)
+    (enFlag "esound" (optEsound != null) null)
+    (enFlag "solaris" false null)
+    (enFlag "waveout" false null) # Windows Only
+    (enFlag "glib2" (optGlib != null) null)
+    (enFlag "gtk3" (optGtk3 != null) null)
+    (enFlag "gconf" (optGconf != null) null)
+    (enFlag "avahi" (optAvahi != null) null)
+    (enFlag "jack" (optLibjack2 != null) null)
+    (enFlag "asyncns" (optLibasyncns != null) null)
+    (enFlag "tcpwrap" false null)
+    (enFlag "lirc" (optLirc != null) null)
+    (enFlag "dbus" (optDbus != null) null)
+    (enFlag "bluez4" false null)
+    (enFlag "bluez5" (optBluez5 != null) null)
+    (enFlag "bluez5-ofono-headset" (optBluez5 != null) null)
+    (enFlag "bluez5-native-headset" (optBluez5 != null) null)
+    (enFlag "udev" (optUdev != null) null)
+    (enFlag "hal-compat" false null)
+    (enFlag "ipv6" true null)
+    (enFlag "openssl" (optOpenssl != null) null)
+    (wtFlag "fftw" (optFftw != null) null)
+    (wtFlag "speex" (optSpeexdsp != null) null)
+    (wtFlag "soxr" (optSoxr != null) null)
+    (enFlag "xen" false null)
+    (enFlag "gcov" false null)
+    (enFlag "systemd-daemon" (optSystemd != null) null)
+    (enFlag "systemd-login" (optSystemd != null) null)
+    (enFlag "systemd-journal" (optSystemd != null) null)
+    (enFlag "manpages" true null)
+    (enFlag "webrtc-aec" hasWebrtc null)
+    (enFlag "adrian-aec" true null)
+    (wtFlag "system-user" true "pulse")
+    (wtFlag "system-group" true "pulse")
+    (wtFlag "access-group" true "audio")
+    (wtFlag "systemduserunitdir" true "\${out}/lib/systemd/user")
+    (wtFlag "bash-completion-dir" true
+      "\${out}/share/bash-completions/completions")
+  ];
 
   preConfigure = ''
     # Performs and autoreconf
@@ -93,73 +179,62 @@ stdenv.mkDerivation rec {
     sed -i "src/Makefile.in" \
         -e "s|udevrulesdir[[:blank:]]*=.*$|udevrulesdir = $out/lib/udev/rules.d|g"
 
-   # don't install proximity-helper as root and setuid
-   sed -i "src/Makefile.in" \
-       -e "s|chown root|true |" \
-       -e "s|chmod r+s |true |"
+    # don't install proximity-helper as root and setuid
+    sed -i "src/Makefile.in" \
+        -e "s|chown root|true |" \
+        -e "s|chmod r+s |true |"
   '';
 
-  configureFlags = [
-    (mkOther                          "localstatedir"              "/var")
-    (mkOther                          "sysconfdir"                 "/etc")
-    (mkEnable false                   "atomic-arm-memory-barrier"  null)         # TODO: Enable on armv8
-    (mkEnable false                   "neon-opt"                   null)         # TODO: Enable on armv8
-    (mkEnable hasXlibs                "x11"                        null)
-    (mkWith   hasCaps                 "caps"                       optLibcap)
-    (mkEnable true                    "tests"                      null)
-    (mkEnable false                   "samplerate"                 null)         # Deprecated
-    (mkWith   true                    "database"                   databaseName)
-    (mkEnable hasOss                  "oss-output"                 null)
-    (mkEnable true                    "oss-wrapper"                null)         # Does not use OSS
-    (mkEnable (optCoreaudio != null)  "coreaudio-output"           null)
-    (mkEnable (optAlsaLib != null)    "alsa"                       null)
-    (mkEnable (optEsound != null)     "esound"                     null)
-    (mkEnable false                   "solaris"                    null)
-    (mkEnable false                   "waveout"                    null)         # Windows Only
-    (mkEnable (optGlib != null)       "glib2"                      null)
-    (mkEnable (optGtk3 != null)       "gtk3"                       null)
-    (mkEnable (optGconf != null)      "gconf"                      null)
-    (mkEnable (optAvahi != null)      "avahi"                      null)
-    (mkEnable (optLibjack2 != null)   "jack"                       null)
-    (mkEnable (optLibasyncns != null) "asyncns"                    null)
-    (mkEnable false                   "tcpwrap"                    null)
-    (mkEnable (optLirc != null)       "lirc"                       null)
-    (mkEnable (optDbus != null)       "dbus"                       null)
-    (mkEnable false                   "bluez4"                     null)
-    (mkEnable (optBluez5 != null)     "bluez5"                     null)
-    (mkEnable (optBluez5 != null)     "bluez5-ofono-headset"       null)
-    (mkEnable (optBluez5 != null)     "bluez5-native-headset"      null)
-    (mkEnable (optUdev != null)       "udev"                       null)
-    (mkEnable false                   "hal-compat"                 null)
-    (mkEnable true                    "ipv6"                       null)
-    (mkEnable (optOpenssl != null)    "openssl"                    null)
-    (mkWith   (optFftw != null)       "fftw"                       null)
-    (mkWith   (optSpeexdsp != null)   "speex"                      null)
-    (mkWith   (optSoxr != null)       "soxr"                       null)
-    (mkEnable false                   "xen"                        null)
-    (mkEnable false                   "gcov"                       null)
-    (mkEnable (optSystemd != null)    "systemd-daemon"             null)
-    (mkEnable (optSystemd != null)    "systemd-login"              null)
-    (mkEnable (optSystemd != null)    "systemd-journal"            null)
-    (mkEnable true                    "manpages"                   null)
-    (mkEnable hasWebrtc               "webrtc-aec"                 null)
-    (mkEnable true                    "adrian-aec"                 null)
-    (mkWith   true                    "system-user"                "pulse")
-    (mkWith   true                    "system-group"               "pulse")
-    (mkWith   true                    "access-group"               "audio")
-    (mkWith   true                    "systemduserunitdir"         "\${out}/lib/systemd/user")
-    (mkWith   stdenv.isDarwin         "mac-sysroot"                "/")
-    (mkWith   true                    "bash-completion-dir"        "\${out}/share/bash-completions/completions")
+  nativeBuildInputs = [
+    pkgconfig
+    intltool
+    automake
+    autoconf
+    libtool
   ];
 
-  enableParallelBuilding = true;
+  propagatedBuildInputs = [
+    optLibcap
+  ];
 
-  # not sure what the best practices are here -- can't seem to find a way
-  # for the compiler to bring in stdlib and stdio (etc.) properly
-  # the alternative is to copy the files from /usr/include to src, but there are
-  # probably a large number of files that would need to be copied (I stopped
-  # after the seventh)
-  NIX_CFLAGS_COMPILE = optionalString stdenv.isDarwin "-I/usr/include";
+  buildInputs = [
+    json_c
+    libsndfile
+    gettext
+    check
+    database
+    valgrind
+    optOss
+    optCoreaudio
+    optAlsaLib
+    optEsound
+    optGlib
+    optGtk3
+    optGconf
+    optAvahi
+    optLibjack2
+    optLibasyncns
+    optLirc
+    optDbus
+    optUdev
+    optOpenssl
+    optFftw
+    optSpeexdsp
+    optSoxr
+    optSystemd
+    optWebrtc-audio-processing
+  ] ++ optionals hasXlibs (with xorg; [
+    libX11
+    libxcb
+    libICE
+    libSM
+    libXtst
+    xextproto
+    libXi
+  ]) ++ optionals (optBluez5 != null) [
+    optBluez5
+    optSbc
+  ];
 
   installFlags = [
     "sysconfdir=$(out)/etc"
@@ -171,23 +246,13 @@ stdenv.mkDerivation rec {
     sed 's|-lltdl|-L${libtool}/lib -lltdl|' -i $out/lib/libpulsecore-${version}.la
   '';
 
-  meta = {
-    description = "Sound server for POSIX and Win32 systems";
-    homepage    = http://www.pulseaudio.org/;
-    # Note: Practically, the server is under the GPL due to the
-    # dependency on `libsamplerate'.  See `LICENSE' for details.
-    licenses    = licenses.lgpl2Plus;
-    maintainers = with maintainers; [ lovek323 wkennington ];
-    platforms   = platforms.unix;
+  enableParallelBuilding = true;
 
-    longDescription = ''
-      PulseAudio is a sound server for POSIX and Win32 systems.  A
-      sound server is basically a proxy for your sound applications.
-      It allows you to do advanced operations on your sound data as it
-      passes between your application and your hardware.  Things like
-      transferring the audio to a different machine, changing the
-      sample format or channel count and mixing several sounds into
-      one are easily achieved using a sound server.
-    '';
+  meta = with stdenv.lib; {
+    description = "Sound server for POSIX and Win32 systems";
+    homepage = http://www.pulseaudio.org/;
+    licenses = licenses.lgpl2Plus;
+    maintainers = with maintainers; [ wkennington ];
+    platforms = platforms.unix;
   };
 }
