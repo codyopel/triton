b17be58c4c0a6dd8d0de484817ae1e424d48d6dc
Cody Opel <codyopel@gmail.com>
2015-10-30 16:30:50 -0400

    gnome-characters: 3.18.0 -> 3.18.1

--- pkgs/desktops/gnome-3/3.18/apps/gnome-characters/default.nix
+++ pkgs/desktops/gnome-3/3.18/apps/gnome-characters/default.nix
@@ -1,19 +1,44 @@
-{ stdenv, fetchurl, pkgconfig, gnome3, gtk3, wrapGAppsHook
-, intltool, gjs, gdk_pixbuf, librsvg }:
+{ stdenv, fetchurl
+, pkgconfig
+
+, gnome3
+, gtk3
+, wrapGAppsHook
+, intltool
+, gjs
+, gdk_pixbuf
+, librsvg
+, gobjectIntrospection
+}:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "gnome-characters-${version}";
+  versionMajor = "3.18";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/gnome-characters/${versionMajor}/${name}.tar.xz";
+    sha256 = "1b4v1c2jqrv86bnhmpqib5q670ibygrx9f0idj5sgz0zdjxkj60n";
+  };
 
   buildInputs = [
-    pkgconfig gtk3 wrapGAppsHook intltool gjs gdk_pixbuf
-    librsvg gnome3.defaultIconTheme
+    pkgconfig
+    gtk3
+    wrapGAppsHook
+    intltool
+    gjs
+    gdk_pixbuf
+    librsvg
+    gnome3.defaultIconTheme
+    gobjectIntrospection
   ];
 
   meta = with stdenv.lib; {
+    description = "Utility to find and insert unusual characters";
     homepage = https://wiki.gnome.org/Design/Apps/CharacterMap;
-    description = "Simple utility application to find and insert unusual characters";
-    maintainers = gnome3.maintainers;
     license = licenses.gpl2;
+    maintainers = gnome3.maintainers;
     platforms = platforms.linux;
   };
 }
