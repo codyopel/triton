4599d0e17c8504ed813c695e02e2111ebe2af841
Cody Opel <codyopel@gmail.com>
2015-10-31 18:47:11 -0400

    guitarix: refactor

--- pkgs/applications/audio/guitarix/default.nix
+++ pkgs/applications/audio/guitarix/default.nix
@@ -1,7 +1,29 @@
-{ stdenv, fetchurl, gettext, intltool, pkgconfig, python
-, avahi, bluez, boost, eigen, fftw, glib, glibmm, gtk, gtkmm, libjack2
-, ladspaH, librdf, libsndfile, lilv, lv2, serd, sord, sratom
-,  zita-convolver, zita-resampler
+{ stdenv, fetchurl
+, gettext
+, intltool
+, pkgconfig
+, python
+
+, avahi
+, bluez
+, boost
+, eigen
+, fftw
+, glib
+, glibmm
+, gtk2
+, gtkmm_2
+, libjack2
+, ladspaH
+, librdf
+, libsndfile
+, lilv
+, lv2
+, serd
+, sord
+, sratom
+, zita-convolver
+, zita-resampler
 , optimizationSupport ? false # Enable support for native CPU extensions
 }:
 
@@ -18,56 +40,69 @@ stdenv.mkDerivation rec {
     sha256 = "1w6dg2n0alfjsx1iy6s53783invygwxk11p1i65cc3nq3zlidcgx";
   };
 
-  nativeBuildInputs = [ gettext intltool pkgconfig python ];
+  NIX_CFLAGS_COMPILE = [
+    "-std=c++11"
+  ];
+
+  nativeBuildInputs = [
+    gettext
+    intltool
+    pkgconfig
+    python
+  ];
 
   buildInputs = [
-    avahi bluez boost eigen fftw glib glibmm gtk gtkmm libjack2
-    ladspaH librdf libsndfile lilv lv2 serd sord sratom
-    zita-convolver zita-resampler
+    avahi
+    #bluez
+    boost
+    eigen
+    fftw
+    glib
+    glibmm
+    gtk2
+    gtkmm_2
+    libjack2
+    ladspaH
+    librdf
+    libsndfile
+    lilv
+    lv2
+    serd
+    sord
+    sratom
+    zita-convolver
+    zita-resampler
   ];
 
   configureFlags = [
+    "--nocache"
     "--shared-lib"
+    "--lib-dev"
+    "--no-ldconfig"
     "--no-desktop-update"
     "--enable-nls"
     "--no-faust" # todo: find out why --faust doesn't work
   ] ++ optional optimizationSupport "--optimization";
 
-  configurePhase = ''python waf configure --prefix=$out $configureFlags'';
+  configurePhase = ''
+    python waf configure --prefix=$out $configureFlags
+  '';
 
-  buildPhase = ''python waf build'';
+  buildPhase = ''
+    python waf build
+  '';
 
-  installPhase = ''python waf install'';
+  installPhase = ''
+    python waf install
+  '';
 
   enableParallelBuilding = true;
 
   meta = with stdenv.lib; { 
-    description = "A virtual guitar amplifier for Linux running with JACK";
-    longDescription = ''
-        guitarix is a virtual guitar amplifier for Linux running with
-      JACK (Jack Audio Connection Kit). It is free as in speech and
-      free as in beer. Its free sourcecode allows to build it for
-      other UNIX-like systems also, namely for BSD and for MacOSX.
-
-        It takes the signal from your guitar as any real amp would do:
-      as a mono-signal from your sound card. Your tone is processed by
-      a main amp and a rack-section. Both can be routed separately and
-      deliver a processed stereo-signal via JACK. You may fill the
-      rack with effects from more than 25 built-in modules spanning
-      from a simple noise-gate to brain-slashing modulation-fx like
-      flanger, phaser or auto-wah. Your signal is processed with
-      minimum latency. On any properly set-up Linux-system you do not
-      need to wait for more than 10 milli-seconds for your playing to
-      be delivered, processed by guitarix.
-
-        guitarix offers the range of sounds you would expect from a
-      full-featured universal guitar-amp. You can get crisp
-      clean-sounds, nice overdrive, fat distortion and a diversity of
-      crazy sounds never heard before.
-    '';
+    description = "A virtual guitar amplifier";
     homepage = http://guitarix.sourceforge.net/;
     license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ astsmtl goibhniu ];
+    maintainers = with maintainers; [ ];
     platforms = platforms.linux;
   };
 }
