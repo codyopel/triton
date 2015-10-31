b753f8434ce455e2fcc8d222c050c1e7140cce5c
Cody Opel <codyopel@gmail.com>
2015-10-30 20:37:05 -0400

    gnome-desktop: 3.18.0 -> 3.18.1

--- pkgs/desktops/gnome-3/3.18/core/gnome-desktop/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/gnome-desktop/default.nix
@@ -1,21 +1,65 @@
-{ stdenv, fetchurl, pkgconfig, python, libxml2Python, libxslt, which, libX11, gnome3, gtk3, glib
-, intltool, gnome_doc_utils, libxkbfile, xkeyboard_config, isocodes, itstool, wayland
-, gobjectIntrospection }:
+{ stdenv, fetchurl
+, pkgconfig
+, python
+, libxml2Python
+, libxslt
+, which
+, libX11
+, gnome3
+, gtk3
+, glib
+, intltool
+, gnome_doc_utils
+, libxkbfile
+, xkeyboard_config
+, isocodes
+, itstool
+, wayland
+, gobjectIntrospection
+, xorg
+}:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "gnome-desktop-${majorVersion}.${minorVersion}";
+  majorVersion = gnome3.version;
+  minorVersion = "1";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/gnome-desktop/${majorVersion}/${name}.tar.xz";
+    sha256 = "0avpmyhzz5b3pyfpkp8iq5ym5r5w7zs3a396hqkdpdsiym0vrazc";
+  };
 
   # this should probably be setuphook for glib
-  NIX_CFLAGS_COMPILE = "-I${glib}/include/gio-unix-2.0";
+  NIX_CFLAGS_COMPILE = [
+    "-I${glib}/include/gio-unix-2.0"
+  ];
+
+  buildInputs = [
+    pkgconfig
+    python
+    libxml2Python
+    libxslt
+    which
+    libX11
+    xkeyboard_config
+    isocodes
+    itstool
+    wayland
+    gtk3
+    glib
+    intltool
+    gnome_doc_utils
+    libxkbfile
+    gobjectIntrospection
+    xorg.libXext
+    xorg.libXrandr
+  ];
+
+  propagatedBuildInputs = [
+    gnome3.gsettings_desktop_schemas
+  ];
 
   enableParallelBuilding = true;
-
-  buildInputs = [ pkgconfig python libxml2Python libxslt which libX11
-                  xkeyboard_config isocodes itstool wayland
-                  gtk3 glib intltool gnome_doc_utils libxkbfile
-                  gobjectIntrospection ];
-
-  propagatedBuildInputs = [ gnome3.gsettings_desktop_schemas ];
 
   meta = with stdenv.lib; {
     platforms = platforms.linux;
