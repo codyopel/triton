69a8f733954c23afaa3eb3483448e2e378638d18
Cody Opel <codyopel@gmail.com>
2015-10-31 21:00:05 -0400

    evince: 3.18.0 -> 3.18.1

--- pkgs/desktops/gnome-3/3.18/core/evince/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/evince/default.nix
@@ -1,21 +1,43 @@
-{ fetchurl, stdenv, pkgconfig, intltool, perl, perlXMLParser, libxml2
-, glib, gtk3, pango, atk, gdk_pixbuf, shared_mime_info, itstool, gnome3
-, poppler, ghostscriptX, djvulibre, libspectre, libsecret , makeWrapper
-, librsvg, recentListSize ? null # 5 is not enough, allow passing a different number
+{ fetchurl, stdenv
+, pkgconfig
+, intltool
+, perl
+, perlXMLParser
+, libxml2
+, glib
+, gtk3
+, pango
+, atk
+, gdk_pixbuf
+, shared_mime_info
+, itstool
+, gnome3
+, ghostscriptX
+, djvulibre
+, libspectre
+, libsecret
+, makeWrapper
+, librsvg
+, recentListSize ? null # 5 is not enough, allow passing a different number
 , gobjectIntrospection
+, poppler
 }:
 
-stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+with {
+  inherit (stdenv.lib)
+    optionalString;
+};
 
-  buildInputs = [
-    pkgconfig intltool perl perlXMLParser libxml2
-    glib gtk3 pango atk gdk_pixbuf gobjectIntrospection
-    itstool gnome3.adwaita-icon-theme
-    gnome3.libgnome_keyring gnome3.gsettings_desktop_schemas
-    poppler ghostscriptX djvulibre libspectre
-    makeWrapper libsecret librsvg gnome3.adwaita-icon-theme
-  ];
+stdenv.mkDerivation rec {
+  name = "evince-${version}";
+  versionMajor = "3.18";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/evince/${versionMajor}/${name}.tar.xz";
+    sha256 = "96e8351f6a6fc5823bb8f51178cde1182bd664814f6fb09bf58a18b673cafa70";
+  };
 
   configureFlags = [
     "--disable-nautilus" # Do not use nautilus
@@ -24,16 +46,43 @@ stdenv.mkDerivation rec {
 
   NIX_CFLAGS_COMPILE = "-I${gnome3.glib}/include/gio-unix-2.0";
 
-  preConfigure = with stdenv.lib;
+  preConfigure =
     optionalString doCheck ''
       for file in test/*.py; do
         echo "patching $file"
         sed '1s,/usr,${python},' -i "$file"
       done
-    '' + optionalString (recentListSize != null) ''
+    '' +
+    optionalString (recentListSize != null) ''
       sed -i 's/\(gtk_recent_chooser_set_limit .*\)5)/\1${builtins.toString recentListSize})/' shell/ev-open-recent-action.c
       sed -i 's/\(if (++n_items == \)5\(.*\)/\1${builtins.toString recentListSize}\2/' shell/ev-window.c
     '';
+
+  buildInputs = [
+    pkgconfig
+    intltool
+    perl
+    perlXMLParser
+    libxml2
+    glib
+    gtk3
+    pango
+    atk
+    gdk_pixbuf
+    gobjectIntrospection
+    itstool
+    gnome3.adwaita-icon-theme
+    gnome3.libgnome_keyring
+    gnome3.gsettings_desktop_schemas
+    poppler
+    ghostscriptX
+    djvulibre
+    libspectre
+    makeWrapper
+    libsecret
+    librsvg
+    gnome3.adwaita-icon-theme
+  ];
 
   preFixup = ''
     # Tell Glib/GIO about the MIME info directory, which is used
@@ -47,18 +96,10 @@ stdenv.mkDerivation rec {
   doCheck = false; # would need pythonPackages.dogTail, which is missing
 
   meta = with stdenv.lib; {
-    homepage = http://www.gnome.org/projects/evince/;
     description = "GNOME's document viewer";
-
-    longDescription = ''
-      Evince is a document viewer for multiple document formats.  It
-      currently supports PDF, PostScript, DjVu, TIFF and DVI.  The goal
-      of Evince is to replace the multiple document viewers that exist
-      on the GNOME Desktop with a single simple application.
-    '';
-
-    license = stdenv.lib.licenses.gpl2Plus;
+    homepage = http://www.gnome.org/projects/evince/;
+    license = licenses.gpl2Plus;
+    maintainers = [ ];
     platforms = platforms.linux;
-    maintainers = [ maintainers.vcunat ];
   };
 }
