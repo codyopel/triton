226a5a3b3feb7d5e343df97e62d84aa5f0d06997
Cody Opel <codyopel@gmail.com>
2015-10-31 22:36:48 -0400

    gnome-calculator: 3.18.0 -> 3.18.1

--- pkgs/desktops/gnome-3/3.18/core/gnome-calculator/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/gnome-calculator/default.nix
@@ -1,18 +1,55 @@
-{ stdenv, intltool, fetchurl, pkgconfig, libxml2
-, bash, gtk3, glib, makeWrapper
-, itstool, gnome3, librsvg, gdk_pixbuf, mpfr, gmp }:
+{ stdenv, fetchurl
+, intltool
+, pkgconfig
+, libxml2
+, bash
+, gtk3
+, glib
+, makeWrapper
+, itstool
+, gnome3
+, librsvg
+, gdk_pixbuf
+, mpfr
+, gmp
+}:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "gnome-calculator-${version}";
+  versionMajor = "3.18";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
 
-  NIX_CFLAGS_COMPILE = "-I${gnome3.glib}/include/gio-unix-2.0";
+  src = fetchurl {
+    url = "mirror://gnome/sources/gnome-calculator/${versionMajor}/${name}.tar.xz";
+    sha256 = "0p4v9w2w9cplc3nyv7akqvk28n6d498dsajfb3f9r8z8sk1f3yhw";
+  };
 
-  propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard ];
+  NIX_CFLAGS_COMPILE = [
+    "-I${gnome3.glib}/include/gio-unix-2.0"
+  ];
 
-  buildInputs = [ bash pkgconfig gtk3 glib intltool itstool
-                  libxml2 gnome3.gtksourceview mpfr gmp
-                  gdk_pixbuf gnome3.defaultIconTheme librsvg
-                  gnome3.gsettings_desktop_schemas makeWrapper ];
+  propagatedUserEnvPkgs = [
+    gnome3.gnome_themes_standard
+  ];
+
+  buildInputs = [
+    bash
+    pkgconfig
+    gtk3
+    glib
+    intltool
+    itstool
+    libxml2
+    gnome3.gtksourceview
+    mpfr
+    gmp
+    gdk_pixbuf
+    gnome3.defaultIconTheme
+    librsvg
+    gnome3.gsettings_desktop_schemas
+    makeWrapper
+  ];
 
   preFixup = ''
     wrapProgram "$out/bin/gnome-calculator" \
