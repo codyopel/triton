0106754c00d8f6846018244f1b340211d6707aa8
Cody Opel <codyopel@gmail.com>
2015-10-31 22:03:53 -0400

    eigen: 3.2.5 -> 3.2.6

--- pkgs/development/libraries/eigen/default.nix
+++ pkgs/development/libraries/eigen/default.nix
@@ -1,25 +1,45 @@
-{stdenv, fetchurl, cmake}:
+{ stdenv, fetchurl
+, cmake
+, pkgconfig
 
-let
-  version = "3.2.5";
-in
-stdenv.mkDerivation {
+, fftw
+}:
+
+stdenv.mkDerivation rec {
   name = "eigen-${version}";
+  version = "3.2.6";
   
   src = fetchurl {
     url = "http://bitbucket.org/eigen/eigen/get/${version}.tar.gz";
     name = "eigen-${version}.tar.gz";
-    sha256 = "1vjixip19lwfia2bjpjwm09j7l20ry75493i6mjsk9djszj61agi";
+    sha256 = "0my9gy73kjg53zy83p3cahmm9y0zjh3l4r3nhihd2ikiqwalxqdn";
   };
+
+  cmakeFlags = [
+    "-DEIGEN_BUILD_TESTS=ON"
+    "-DEIGEN_TEST_NO_FORTRAN=ON"
+    "-DEIGEN_TEST_NO_OPENGL=ON"
+  ];
+
+  NIX_CFLAGS_COMPILE = [
+    "-std=c++11"
+  ];
   
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [
+    cmake
+    pkgconfig
+  ];
+
+  buildInputs = [
+    fftw
+  ];
   
   meta = with stdenv.lib; {
     description = "C++ template library for linear algebra: vectors, matrices, and related algorithms";
     license = licenses.lgpl3Plus;
     homepage = http://eigen.tuxfamily.org ;
     platforms = platforms.unix;
-    maintainers = with stdenv.lib.maintainers; [ sander urkud raskin ];
+    maintainers = with stdenv.lib.maintainers; [ ];
     inherit version;
   };
 }
