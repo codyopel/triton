e0f10ed8cfd1500a5c5672b73c1fa1dc7f3214db
Cody Opel <codyopel@gmail.com>
2015-10-31 22:43:43 -0400

    gnome-online-accounts: 3.18.0 -> 3.18.1

--- pkgs/desktops/gnome-3/3.18/core/gnome-online-accounts/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/gnome-online-accounts/default.nix
@@ -1,24 +1,68 @@
-{ stdenv, fetchurl, pkgconfig, glib, libxslt, gtk, makeWrapper
-, webkitgtk, json_glib, rest, libsecret, dbus_glib, gnome_common
-, telepathy_glib, intltool, dbus_libs, icu, autoreconfHook
-, libsoup, docbook_xsl_ns, docbook_xsl, gnome3
+{ stdenv, fetchurl
+, pkgconfig
+, glib
+, libxslt
+, gtk
+, makeWrapper
+, webkitgtk
+, json_glib
+, rest
+, libsecret
+, dbus_glib
+, gnome_common
+, telepathy_glib
+, intltool
+, dbus_libs
+, icu
+, autoreconfHook
+, libsoup
+, docbook_xsl_ns
+, docbook_xsl
+, gnome3
 }:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "gnome-online-accounts-${version}";
+  versionMajor = "3.18";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
 
-  NIX_CFLAGS_COMPILE = "-I${dbus_glib}/include/dbus-1.0 -I${dbus_libs}/include/dbus-1.0";
+  src = fetchurl {
+    url = "mirror://gnome/sources/gnome-online-accounts/${versionMajor}/${name}.tar.xz";
+    sha256 = "fc2dac96551746576759bd14f9b322bae1d40aeedc0e755065ddf5eaaefacd34";
+  };
 
-  enableParallelBuilding = true;
+  NIX_CFLAGS_COMPILE = [
+    "-I${dbus_glib}/include/dbus-1.0"
+    "-I${dbus_libs}/include/dbus-1.0"
+  ];
 
   preAutoreconf = ''
     sed '/disable-settings/d' -i configure.ac
     sed "/if HAVE_INTROSPECTION/a INTROSPECTION_COMPILER_ARGS = --shared-library=$out/lib/libgoa-1.0.so" -i src/goa/Makefile.am
   '';
 
-  buildInputs = [ pkgconfig glib libxslt gtk webkitgtk json_glib rest gnome_common makeWrapper
-                  libsecret dbus_glib telepathy_glib intltool icu libsoup autoreconfHook
-                  docbook_xsl_ns docbook_xsl gnome3.defaultIconTheme ];
+  buildInputs = [
+    pkgconfig
+    glib
+    libxslt
+    gtk
+    webkitgtk
+    json_glib
+    rest
+    gnome_common
+    makeWrapper
+    libsecret
+    dbus_glib
+    telepathy_glib
+    intltool
+    icu
+    libsoup
+    autoreconfHook
+    docbook_xsl_ns
+    docbook_xsl
+    gnome3.defaultIconTheme
+  ];
 
   preFixup = ''
     for f in "$out/libexec/"*; do
@@ -26,8 +70,10 @@ stdenv.mkDerivation rec {
     done
   '';
 
+  enableParallelBuilding = true;
+
   meta = with stdenv.lib; {
-    platforms = platforms.linux;
     maintainers = gnome3.maintainers;
+    platforms = platforms.linux;
   };
 }
