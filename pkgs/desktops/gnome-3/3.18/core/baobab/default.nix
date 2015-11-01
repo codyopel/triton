4f44f4a01f7c3eb23ed79954094c702d263e7676
Cody Opel <codyopel@gmail.com>
2015-10-31 22:26:08 -0400

    baobab: 3.18.0 -> 3.18.1

--- pkgs/desktops/gnome-3/3.18/core/baobab/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/baobab/default.nix
@@ -1,26 +1,62 @@
-{ stdenv, intltool, fetchurl, vala, libgtop
-, pkgconfig, gtk3, glib
-, bash, makeWrapper, itstool, libxml2
-, gnome3, librsvg, gdk_pixbuf, file }:
+{ stdenv, fetchurl
+, intltool
+, vala
+, libgtop
+, pkgconfig
+, gtk3, glib
+, bash
+, makeWrapper
+, itstool
+, libxml2
+, gnome3
+, librsvg
+, gdk_pixbuf
+, file
+}:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "baobab-3.18.0";
+  versionMajor = "3.18";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
 
-  doCheck = true;
+  src = fetchurl {
+    url = "mirror://gnome/sources/baobab/${versionMajor}/${name}.tar.xz";
+    sha256 = "0nhbfs4c1inr7sn1mqmpzanmkz1zz9q05hqf5xydk50fvm9lr4km";
+  };
 
-  NIX_CFLAGS_COMPILE = "-I${gnome3.glib}/include/gio-unix-2.0";
+  NIX_CFLAGS_COMPILE = [
+    "-I${gnome3.glib}/include/gio-unix-2.0"
+  ];
 
-  propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard ];
+  propagatedUserEnvPkgs = [
+    gnome3.gnome_themes_standard
+  ];
 
-  buildInputs = [ vala pkgconfig gtk3 glib libgtop intltool itstool libxml2
-                  gnome3.gsettings_desktop_schemas makeWrapper file
-                  gdk_pixbuf gnome3.defaultIconTheme librsvg ];
+  buildInputs = [
+    vala
+    pkgconfig
+    gtk3
+    glib
+    libgtop
+    intltool
+    itstool
+    libxml2
+    gnome3.gsettings_desktop_schemas
+    makeWrapper
+    file
+    gdk_pixbuf
+    gnome3.defaultIconTheme
+    librsvg
+  ];
 
   preFixup = ''
     wrapProgram "$out/bin/baobab" \
       --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE" \
       --prefix XDG_DATA_DIRS : "${gnome3.gnome_themes_standard}/share:$out/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
   '';
+
+  doCheck = true;
 
   meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/Apps/Baobab;
