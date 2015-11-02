680a9fa181cfec5a979dd10e2f8909a216b4d60c
Cody Opel <codyopel@gmail.com>
2015-11-02 13:05:05 -0500

    gtkhtml: add xorg libs

--- pkgs/desktops/gnome-3/3.18/misc/gtkhtml/default.nix
+++ pkgs/desktops/gnome-3/3.18/misc/gtkhtml/default.nix
@@ -1,16 +1,33 @@
-{ stdenv, fetchurl, pkgconfig, gtk3, intltool
-, gnome3, enchant, isocodes }:
+{ stdenv, fetchurl
+, pkgconfig
+, gtk3
+, intltool
+, gnome3
+, enchant
+, isocodes
+, xorg
+}:
 
 stdenv.mkDerivation rec {
   inherit (import ./src.nix fetchurl) name src;
 
-  buildInputs = [ pkgconfig gtk3 intltool gnome3.adwaita-icon-theme
-                  gnome3.gsettings_desktop_schemas ];
+  propagatedBuildInputs = [
+    enchant
+    isocodes
+  ];
 
-  propagatedBuildInputs = [ enchant isocodes ];
+  buildInputs = [
+    pkgconfig
+    gtk3
+    intltool
+    gnome3.adwaita-icon-theme
+    gnome3.gsettings_desktop_schemas
+    xorg.libSM
+    xorg.libICE
+  ];
 
   meta = with stdenv.lib; {
-    platforms = platforms.linux;
     maintainers = gnome3.maintainers;
+    platforms = platforms.linux;
   };
 }
