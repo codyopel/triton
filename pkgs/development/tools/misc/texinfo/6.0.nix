6fe591bc72e533261ecbab0aa3e953c1c3238d51
Cody Opel <codyopel@gmail.com>
2015-11-03 13:21:45 -0500

    texinfo: refactor

--- pkgs/development/tools/misc/texinfo/6.0.nix
+++ pkgs/development/tools/misc/texinfo/6.0.nix
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
     installTargets="install install-tex";
   '';
 
-  doCheck = !stdenv.isDarwin;
+  doCheck = true;
 
   meta = {
     homepage = "http://www.gnu.org/software/texinfo/";
@@ -24,21 +24,7 @@ stdenv.mkDerivation rec {
     license = stdenv.lib.licenses.gpl3Plus;
     platforms = stdenv.lib.platforms.all;
 
-    longDescription = ''
-      Texinfo is the official documentation format of the GNU project.
-      It was invented by Richard Stallman and Bob Chassell many years
-      ago, loosely based on Brian Reid's Scribe and other formatting
-      languages of the time.  It is used by many non-GNU projects as
-      well.
 
-      Texinfo uses a single source file to produce output in a number
-      of formats, both online and printed (dvi, html, info, pdf, xml,
-      etc.).  This means that instead of writing different documents
-      for online information and another for a printed manual, you
-      need write only one document.  And when the work is revised, you
-      need revise only that one document.  The Texinfo system is
-      well-integrated with GNU Emacs.
-    '';
-    branch = "5.2";
+    branch = "6";
   };
 }
