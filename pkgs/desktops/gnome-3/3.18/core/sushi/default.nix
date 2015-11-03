5bbfecf4b1cbb418df734baf431a755cf69f0c8c
Cody Opel <codyopel@gmail.com>
2015-11-03 18:12:11 -0500

    sushi: refactor

--- pkgs/desktops/gnome-3/3.18/core/sushi/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/sushi/default.nix
@@ -1,19 +1,63 @@
-{ stdenv, fetchurl, pkgconfig, file, intltool, gobjectIntrospection, glib
-, clutter_gtk, clutter-gst, gnome3, gtksourceview, libmusicbrainz
-, webkitgtk, libmusicbrainz5, icu, makeWrapper, gst_all_1
-, gdk_pixbuf, librsvg, gtk3 }:
+{ stdenv, fetchurl
+, pkgconfig
+, file
+, intltool
+, gobjectIntrospection
+, glib
+, clutter_gtk
+, clutter-gst_2
+, gnome3
+, gtksourceview
+, libmusicbrainz
+, webkitgtk
+, libmusicbrainz5
+, icu
+, makeWrapper
+, gst_all_1
+, gdk_pixbuf
+, librsvg
+, gtk3
+}:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "sushi-${version}";
+  versionMajor = "3.18";
+  versionMinor = "0";
+  version = "${versionMajor}.${versionMinor}";
 
-  propagatedUserEnvPkgs = [ gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good ];
+  src = fetchurl {
+    url = "mirror://gnome/sources/sushi/${versionMajor}/${name}.tar.xz";
+    sha256 = "174fc0jh5q8712flmhggi0dd4vbn13hrr94dyapj7gshx4mzjkbz";
+  };
 
-  buildInputs = [ pkgconfig file intltool gobjectIntrospection glib gtk3
-                  clutter_gtk clutter-gst gnome3.gjs gtksourceview gdk_pixbuf
-                  librsvg gnome3.defaultIconTheme libmusicbrainz5 webkitgtk
-                  gnome3.evince icu makeWrapper ];
+  propagatedUserEnvPkgs = [
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+  ];
 
-  enableParallelBuilding = true;
+  buildInputs = [
+    pkgconfig
+    file
+    intltool
+    gobjectIntrospection
+    glib
+    gtk3
+    clutter_gtk
+    clutter-gst_2
+    gnome3.gjs
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gtksourceview
+    gdk_pixbuf
+    librsvg
+    gnome3.defaultIconTheme
+    libmusicbrainz5
+    webkitgtk
+    gnome3.evince
+    icu
+    makeWrapper
+  ];
 
   preFixup = ''
     wrapProgram $out/libexec/sushi-start \
@@ -23,9 +67,11 @@ stdenv.mkDerivation rec {
       --prefix XDG_DATA_DIRS : "$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
   '';
 
+  enableParallelBuilding = true;
+
   meta = with stdenv.lib; {
-    homepage = "http://en.wikipedia.org/wiki/Sushi_(software)";
     description = "A quick previewer for Nautilus";
+    homepage = "http://en.wikipedia.org/wiki/Sushi_(software)";
     maintainers = gnome3.maintainers;
     license = licenses.gpl2Plus;
     platforms = platforms.linux;
