55f8c3f6cf0434a4b1b716e3f5e9478280382107
Cody Opel <codyopel@gmail.com>
2015-11-05 23:47:55 -0500

    acpid: refactor

--- pkgs/os-specific/linux/acpid/default.nix
+++ pkgs/os-specific/linux/acpid/default.nix
@@ -8,14 +8,16 @@ stdenv.mkDerivation rec {
     sha256 = "0s2wg84x6pnrkf7i7lpzw2rilq4mj50vwb7p2b2n5hdyfa00lw0b";
   };
 
-  preBuild = ''
-    makeFlagsArray=(BINDIR=$out/bin SBINDIR=$out/sbin MAN8DIR=$out/share/man/man8)
-  '';
+  makeFlags = [
+    "BINDIR=$(out)/bin"
+    "SBINDIR=$(out)/sbin"
+    "MAN8DIR=$(out)/share/man/man8"
+  ];
 
-  meta = {
-    homepage = http://tedfelix.com/linux/acpid-netlink.html;
+  meta = with stdenv.lib; {
     description = "A daemon for delivering ACPI events to userspace programs";
-    license = stdenv.lib.licenses.gpl2Plus;
-    platforms = stdenv.lib.platforms.linux;
+    homepage = http://tedfelix.com/linux/acpid-netlink.html;
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux;
   };
 }
