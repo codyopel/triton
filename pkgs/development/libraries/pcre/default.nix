c2749a7ed2dd57761b47f6b99325c975e32924dd
Cody Opel <codyopel@gmail.com>
2015-11-07 19:22:44 -0500

    pcre: 8.37 -> 8.38-rc1

--- pkgs/development/libraries/pcre/default.nix
+++ pkgs/development/libraries/pcre/default.nix
@@ -1,23 +1,23 @@
-{ stdenv, fetchurl, unicodeSupport ? true, cplusplusSupport ? true
-, windows ? null
+{ stdenv, fetchurl
+, unicodeSupport ? true
+, cplusplusSupport ? true
 }:
 
-with stdenv.lib;
-
 stdenv.mkDerivation rec {
-  name = "pcre-8.37";
+  #name = "pcre-8.37";
+  name = "pcre-8.38-RC1";
 
   src = fetchurl {
-    url = "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/${name}.tar.bz2";
-    sha256 = "17bqykp604p7376wj3q2nmjdhrb6v1ny8q08zdwi7qvc02l9wrsi";
+    #url = "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/${name}.tar.bz2";
+    #sha256 = "17bqykp604p7376wj3q2nmjdhrb6v1ny8q08zdwi7qvc02l9wrsi";
+    url = "http://pub.wak.io/nixos/tarballs/${name}.tar.bz2";
+    sha256 = "60106bd136df843b9542127ffe6767e66a8d8452de345b1ed5c9e1b7f2376379";
   };
 
-  patches =
-    [ ./cve-2015-3210.patch
-      ./cve-2015-5073.patch
-    ];
-
-  outputs = [ "out" "doc" "man" ];
+  #patches = [
+  #  ./cve-2015-3210.patch
+  #  ./cve-2015-5073.patch
+  #];
 
   configureFlags = ''
     --enable-jit
@@ -25,28 +25,16 @@ stdenv.mkDerivation rec {
     ${if !cplusplusSupport then "--disable-cpp" else ""}
   '';
 
-  doCheck = with stdenv; !(isCygwin || isFreeBSD);
-    # XXX: test failure on Cygwin
-    # we are running out of stack on both freeBSDs on Hydra
+  outputs = [ "out" "doc" "man" ];
 
-  crossAttrs = optionalAttrs (stdenv.cross.libc == "msvcrt") {
-    buildInputs = [ windows.mingw_w64_pthreads.crossDrv ];
-  };
+  # we are running out of stack on both freeBSDs on Hydra
+  doCheck = with stdenv; !isFreeBSD;
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = "http://www.pcre.org/";
     description = "A library for Perl Compatible Regular Expressions";
-    license = stdenv.lib.licenses.bsd3;
-
-    longDescription = ''
-      The PCRE library is a set of functions that implement regular
-      expression pattern matching using the same syntax and semantics as
-      Perl 5. PCRE has its own native API, as well as a set of wrapper
-      functions that correspond to the POSIX regular expression API. The
-      PCRE library is free, even for building proprietary software.
-    '';
-
+    license = licenses.bsd3;
+    maintainers = [ ];
     platforms = platforms.all;
-    maintainers = [ maintainers.simons ];
   };
 }
