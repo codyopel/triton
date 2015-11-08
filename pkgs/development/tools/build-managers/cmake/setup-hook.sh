c4da198330a8652449208c77802aad83633ee87a
Cody Opel <codyopel@gmail.com>
2015-11-07 19:29:11 -0500

    cmake: refactor

--- pkgs/development/tools/build-managers/cmake/setup-hook.sh
+++ pkgs/development/tools/build-managers/cmake/setup-hook.sh
@@ -1,33 +1,38 @@
 addCMakeParams() {
-    addToSearchPath CMAKE_PREFIX_PATH $1
+
+  addToSearchPath 'CMAKE_PREFIX_PATH' "${1}"
+
 }
 
 fixCmakeFiles() {
+
     # Replace occurences of /usr and /opt by /var/empty.
-    echo "fixing cmake files..."
-    find "$1" \( -type f -name "*.cmake" -o -name "*.cmake.in" -o -name CMakeLists.txt \) -print |
-        while read fn; do
-            sed -e 's^/usr\([ /]\|$\)^/var/empty\1^g' -e 's^/opt\([ /]\|$\)^/var/empty\1^g' < "$fn" > "$fn.tmp"
-            mv "$fn.tmp" "$fn"
-        done
+  echo "fixing cmake files..."
+  find "$1" \( -type f -name "*.cmake" -o -name "*.cmake.in" -o -name CMakeLists.txt \) -print |
+    while read fn ; do
+      sed -e 's^/usr\([ /]\|$\)^/var/empty\1^g' -e 's^/opt\([ /]\|$\)^/var/empty\1^g' < "$fn" > "$fn.tmp"
+      mv "$fn.tmp" "$fn"
+    done
+
 }
 
 cmakeConfigurePhase() {
-    eval "$preConfigure"
 
-    if [ -z "$dontFixCmake" ]; then
-        fixCmakeFiles .
-    fi
+  eval "$preConfigure"
 
-    if [ -z "$dontUseCmakeBuildDir" ]; then
-        mkdir -p build
-        cd build
-        cmakeDir=..
-    fi
+  if [ -z "$dontFixCmake" ] ; then
+    fixCmakeFiles .
+  fi
 
-    if [ -z "$dontAddPrefix" ]; then
-        cmakeFlags="-DCMAKE_INSTALL_PREFIX=$prefix $cmakeFlags"
-    fi
+  if [ -z "$dontUseCmakeBuildDir" ]; then
+    mkdir -p build
+    cd build
+    cmakeDir=..
+  fi
+
+  if [ -z "$dontAddPrefix" ]; then
+    cmakeFlags="-DCMAKE_INSTALL_PREFIX=$prefix $cmakeFlags"
+  fi
 
     if [ -n "$crossConfig" ]; then
         # By now it supports linux builds only. We should set the proper
@@ -49,6 +54,9 @@ cmakeConfigurePhase() {
     # And build always Release, to ensure optimisation flags
     cmakeFlags="-DCMAKE_BUILD_TYPE=Release -DCMAKE_SKIP_BUILD_RPATH=ON $cmakeFlags"
 
+    # Generate ninja build files instead of make
+    #cmakeFlags="-GNinja $cmakeFlags"
+
     echo "cmake flags: $cmakeFlags ${cmakeFlagsArray[@]}"
 
     cmake ${cmakeDir:-.} $cmakeFlags "${cmakeFlagsArray[@]}"
@@ -56,17 +64,8 @@ cmakeConfigurePhase() {
     eval "$postConfigure"
 }
 
-if [ -z "$dontUseCmakeConfigure" -a -z "$configurePhase" ]; then
-    configurePhase=cmakeConfigurePhase
-fi
+makeCmakeFindLibs() {
 
-if [ -n "$crossConfig" ]; then
-    crossEnvHooks+=(addCMakeParams)
-else
-    envHooks+=(addCMakeParams)
-fi
-
-makeCmakeFindLibs(){
   for flag in $NIX_CFLAGS_COMPILE $NIX_LDFLAGS; do
     case $flag in
       -I*)
@@ -77,8 +76,19 @@ makeCmakeFindLibs(){
         ;;
     esac
   done
+
 }
+
+if [ -z "${dontUseCmakeConfigure}" -a -z "${configurePhase}" ]; then
+    configurePhase='cmakeConfigurePhase'
+fi
+
+if [ -n "$crossConfig" ]; then
+    crossEnvHooks+=('addCMakeParams')
+else
+    envHooks+=('addCMakeParams')
+fi
 
 # not using setupHook, because it could be a setupHook adding additional
 # include flags to NIX_CFLAGS_COMPILE
-postHooks+=(makeCmakeFindLibs)
+postHooks+=('makeCmakeFindLibs')
