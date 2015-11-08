580de16b8b7987bae1c346eb31f4f768bb3c4ddc
Cody Opel <codyopel@gmail.com>
2015-11-07 19:24:26 -0500

    fish: refactor

--- pkgs/shells/fish/default.nix
+++ pkgs/shells/fish/default.nix
@@ -1,4 +1,14 @@
-{ stdenv, fetchurl, ncurses, python, which, groff, gettext, man_db, bc, libiconv, coreutils }:
+{ stdenv, fetchurl
+, ncurses
+, python
+, which
+, groff
+, gettext
+, man_db
+, bc
+, libiconv
+, coreutils
+}:
 
 stdenv.mkDerivation rec {
   name = "fish-${version}";
@@ -9,26 +19,41 @@ stdenv.mkDerivation rec {
     sha256 = "0ympqz7llmf0hafxwglykplw6j5cz82yhlrw50lw4bnf2kykjqx7";
   };
 
-  buildInputs = [ ncurses libiconv ];
+  buildInputs = [
+    libiconv
+    ncurses
+  ];
 
   # Required binaries during execution
   # Python: Autocompletion generated from manpages and config editing
-  propagatedBuildInputs = [ python which groff gettext ]
-                          ++ stdenv.lib.optional (!stdenv.isDarwin) man_db
-                          ++ [ bc coreutils ];
+  propagatedBuildInputs = [
+    bc
+    coreutils
+    gettext
+    groff
+    man_db
+    python
+    which
+  ];
 
   postInstall = ''
     sed -e "s|bc|${bc}/bin/bc|" \
         -e "s|/usr/bin/seq|${coreutils}/bin/seq|" \
         -i "$out/share/fish/functions/seq.fish"
-    sed -i "s|which |${which}/bin/which |" "$out/share/fish/functions/type.fish"
-    sed -i "s|nroff |${groff}/bin/nroff |" "$out/share/fish/functions/__fish_print_help.fish"
+
+    sed -i "s|which |${which}/bin/which |" \
+           "$out/share/fish/functions/type.fish"
+
+    sed -i "s|nroff |${groff}/bin/nroff |" \
+           "$out/share/fish/functions/__fish_print_help.fish"
+
     sed -e "s|gettext |${gettext}/bin/gettext |" \
         -e "s|which |${which}/bin/which |" \
         -i "$out/share/fish/functions/_.fish"
-  '' + stdenv.lib.optionalString (!stdenv.isDarwin) ''
-    sed -i "s|Popen(\['manpath'|Popen(\['${man_db}/bin/manpath'|" "$out/share/fish/tools/create_manpage_completions.py"
-  '' + ''
+
+    sed -i "s|Popen(\['manpath'|Popen(\['${man_db}/bin/manpath'|" \
+           "$out/share/fish/tools/create_manpage_completions.py"
+
     sed -i "s|/sbin /usr/sbin||" \
            "$out/share/fish/functions/__fish_complete_subcommand_root.fish"
   '';
@@ -37,7 +62,7 @@ stdenv.mkDerivation rec {
     description = "Smart and user-friendly command line shell";
     homepage = "http://fishshell.com/";
     license = licenses.gpl2;
+    maintainers = with maintainers; [ codyopel ];
     platforms = platforms.unix;
-    maintainers = with maintainers; [ ocharles ];
   };
 }
