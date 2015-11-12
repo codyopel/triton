e17dc18115cec0e7e18a8568ce3026f5d5a0395f
Cody Opel <codyopel@gmail.com>
2015-11-12 11:58:49 -0500

    gnome-common: refactor

--- pkgs/desktops/gnome-3/3.18/core/gnome-common/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/gnome-common/default.nix
@@ -1,15 +1,32 @@
-{ stdenv, fetchurl, which, gnome3, autoconf, automake }:
+{ stdenv, fetchurl
+, which
+, gnome3
+, autoconf
+, automake
+}:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "gnome-common-3.18.0";
 
-  patches = [(fetchurl {
-    name = "gnome-common-patch";
-    url = "https://bug697543.bugzilla-attachments.gnome.org/attachment.cgi?id=240935";
-    sha256 = "17abp7czfzirjm7qsn2czd03hdv9kbyhk3lkjxg2xsf5fky7z7jl";
-  })];
+  src = fetchurl {
+    url = mirror://gnome/sources/gnome-common/3.18/gnome-common-3.18.0.tar.xz;
+    sha256 = "22569e370ae755e04527b76328befc4c73b62bfd4a572499fde116b8318af8cf";
+  };
 
-  propagatedBuildInputs = [ which autoconf automake ]; # autogen.sh which is using gnome_common tends to require which
+  patches = [
+    (fetchurl {
+      name = "gnome-common-patch";
+      url = "https://bug697543.bugzilla-attachments.gnome.org/attachment.cgi?id=240935";
+      sha256 = "17abp7czfzirjm7qsn2czd03hdv9kbyhk3lkjxg2xsf5fky7z7jl";
+    })
+  ];
+
+  propagatedBuildInputs = [
+    # autogen.sh which is using gnome_common tends to require which
+    which
+    autoconf
+    automake
+  ];
 
   meta = with stdenv.lib; {
     maintainers = gnome3.maintainers;
