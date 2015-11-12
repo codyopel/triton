dae478da4345fd863369a503ca02ed83d9418f8c
Cody Opel <codyopel@gmail.com>
2015-11-12 11:16:45 -0500

    libmkv: refactor

--- pkgs/development/libraries/libmkv/default.nix
+++ pkgs/development/libraries/libmkv/default.nix
@@ -1,18 +1,19 @@
-{ stdenv, fetchgit, libtool, autoconf, automake }:
+{ stdenv, fetchurl
+, autoreconfHook
+}:
 
 stdenv.mkDerivation rec {
-  name = "libmkv-0.6.5.1p2";
+  name = "libmkv-${version}";
+  version = "0.6.5.1";
   
-  src = fetchgit {
-    url = https://github.com/saintdev/libmkv.git;
-    rev = "refs/tags/0.6.5.1";
-    sha256 = "1b0441x5rl5dbwrc0hq9jih111iil7ckqws3hcdj63jx2ma3s4hi";
+  src = fetchurl {
+    url = "https://github.com/saintdev/libmkv/archive/${version}.tar.gz";
+    sha256 = "007g9i3591l01ycfn8c0s5xcd87lvk8ws7fs9y1zjm5kp848mw21";
   };
 
-  nativeBuildInputs = [ libtool autoconf automake ];
-
-  # TODO fix library version
-  preConfigure = "sh bootstrap.sh";
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
 
   # From Handbrake
   patches = [
@@ -21,9 +22,9 @@ stdenv.mkDerivation rec {
     ./P00-mingw-large-file.patch
   ];
 
-  meta = {
+  meta = with stdenv.lib; {
     homepage = https://github.com/saintdev/libmkv;
-    license = stdenv.lib.licenses.gpl2;
-    maintainers = [ stdenv.lib.maintainers.wmertens ];
+    license = licenses.gpl2;
+    maintainers = [ ];
   };
 }
