864ce10063fe484e1b8c1f6718a69ceb145ccb5a
Cody Opel <codyopel@gmail.com>
2015-11-12 11:53:43 -0500

    libmatroska: 1.4.1 -> 1.4.4

--- pkgs/development/libraries/libmatroska/default.nix
+++ pkgs/development/libraries/libmatroska/default.nix
@@ -1,23 +1,41 @@
-{ stdenv, fetchurl, libebml }:
+{ stdenv, fetchurl
+, pkgconfig
+
+, libebml
+}:
+
+with {
+  inherit (stdenv.lib)
+    optional;
+};
 
 stdenv.mkDerivation rec {
-  name = "libmatroska-1.4.1";
+  name = "libmatroska-1.4.4";
 
   src = fetchurl {
     url = "http://dl.matroska.org/downloads/libmatroska/${name}.tar.bz2";
-    sha256 = "1dzglkl0hpimld1kahkrrp857hw5pg1r7xxbpnx7jmlj7s3j2vq8";
+    sha256 = "1mvb54q3gag9dj0pkwci8w75gp6mm14gi85y0ld3ar1rdngsmvyk";
   };
 
-  configurePhase = "cd make/linux";
-  makeFlags = "prefix=$(out) LIBEBML_INCLUDE_DIR=${libebml}/include LIBEBML_LIB_DIR=${libebml}/lib"
-    + stdenv.lib.optionalString stdenv.isDarwin " CXX=clang++";
-  propagatedBuildInputs = [ libebml ];
+  makeFlags = [
+    "prefix=$(out)"
+    "LIBEBML_INCLUDE_DIR=${libebml}/include"
+    "LIBEBML_LIB_DIR=${libebml}/lib"
+  ] ++ optional stdenv.cc.isClang "CXX=clang++";
+
+  nativeBuildInputs = [
+    pkgconfig
+  ];
+
+  propagatedBuildInputs = [
+    libebml
+  ];
 
   meta = with stdenv.lib; {
     description = "A library to parse Matroska files";
     homepage = http://matroska.org/;
     license = licenses.lgpl21;
-    maintainers = [ maintainers.spwhitt ];
+    maintainers = [ ];
     platforms = platforms.unix;
   };
 }
