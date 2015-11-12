47de847e8fbfd225e0a113e9bb1864ba4441a619
Cody Opel <codyopel@gmail.com>
2015-11-12 11:15:24 -0500

    libffi: refactor

--- pkgs/development/libraries/libffi/default.nix
+++ pkgs/development/libraries/libffi/default.nix
@@ -8,40 +8,24 @@ stdenv.mkDerivation rec {
     sha256 = "0dya49bnhianl0r65m65xndz6ls2jn1xngyn72gd28ls3n7bnvnh";
   };
 
-  patches = if stdenv.isCygwin then [ ./3.2.1-cygwin.patch ] else null;
-
   configureFlags = [
     "--with-gcc-arch=generic" # no detection of -march= or -mtune=
     "--enable-pax_emutramp"
   ];
 
-  dontStrip = stdenv ? cross; # Don't run the native `strip' when cross-compiling.
-
-  # Install headers in the right place.
   postInstall = ''
-    ln -s${if (stdenv.isFreeBSD || stdenv.isOpenBSD || stdenv.isDarwin) then "" else "r"}v "$out/lib/"libffi*/include "$out/include"
+    # Install headers in the right place.
+    ln -s${if (stdenv.isFreeBSD || stdenv.isOpenBSD) then "" else "r"}v "$out/lib/"libffi*/include "$out/include"
   '';
 
-  meta = {
-    description = "A foreign function call interface library";
-    longDescription = ''
-      The libffi library provides a portable, high level programming
-      interface to various calling conventions.  This allows a
-      programmer to call any function specified by a call interface
-      description at run-time.
+  # Don't run the native `strip' when cross-compiling.
+  dontStrip = stdenv ? cross;
 
-      FFI stands for Foreign Function Interface.  A foreign function
-      interface is the popular name for the interface that allows code
-      written in one language to call code written in another
-      language.  The libffi library really only provides the lowest,
-      machine dependent layer of a fully featured foreign function
-      interface.  A layer must exist above libffi that handles type
-      conversions for values passed between the two languages.
-    '';
+  meta = with stdenv.lib; {
+    description = "A foreign function call interface library";
     homepage = http://sourceware.org/libffi/;
-    # See http://github.com/atgreen/libffi/blob/master/LICENSE .
-    license = stdenv.lib.licenses.free;
+    license = licenses.free;
     maintainers = [ ];
-    platforms = stdenv.lib.platforms.all;
+    platforms = platforms.all;
   };
 }
