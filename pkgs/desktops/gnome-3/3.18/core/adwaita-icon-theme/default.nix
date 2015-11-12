7308ca1511e26cd2d75b382f760590a340202a97
Cody Opel <codyopel@gmail.com>
2015-11-12 11:25:06 -0500

    adwaita-icon-theme: refactor

--- pkgs/desktops/gnome-3/3.18/core/adwaita-icon-theme/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/adwaita-icon-theme/default.nix
@@ -1,18 +1,54 @@
-{ stdenv, fetchurl, pkgconfig, intltool, gnome3
-, iconnamingutils, gtk, gdk_pixbuf, librsvg, hicolor_icon_theme }:
+{ stdenv, fetchurl
+, iconnamingutils
+, intltool
+, pkgconfig
+
+, gdk_pixbuf
+, gnome3
+, hicolor_icon_theme
+, librsvg
+}:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "adwaita-icon-theme-3.18.0";
 
-  # For convenience, we can specify adwaita-icon-theme only in packages
-  propagatedBuildInputs = [ hicolor_icon_theme ];
+  src = fetchurl {
+    url = mirror://gnome/sources/adwaita-icon-theme/3.18/adwaita-icon-theme-3.18.0.tar.xz;
+    sha256 = "5e9ce726001fdd8ee93c394fdc3cdb9e1603bbed5b7c62df453ccf521ec50e58";
+  };
 
-  buildInputs = [ gdk_pixbuf librsvg ];
+  configureFlags = [
+    "--enable-nls"
+    "--enable-w32-cursors"
+    "--disable-l-xl-variants"
+  ];
 
-  nativeBuildInputs = [ pkgconfig intltool iconnamingutils gtk ];
+  # Adding gtk3 as a build input will cause the build to ignore
+  # GDK_PIXBUF_MODULE_FILE, and look for loaders in the gtk lib directory
+
+  nativeBuildInputs = [
+    iconnamingutils
+    intltool
+    pkgconfig
+  ];
+
+  propagatedBuildInputs = [
+    # For convenience, we can specify adwaita-icon-theme only in packages
+    hicolor_icon_theme
+  ];
+
+  buildInputs = [
+    gdk_pixbuf
+    librsvg
+  ];
 
   # remove a tree of dirs with no files within
-  postInstall = '' rm -rf "$out/locale" '';
+  postInstall = ''
+    rm -rvf "$out/locale"
+  '';
+
+  doCheck = false;
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     platforms = platforms.linux;
