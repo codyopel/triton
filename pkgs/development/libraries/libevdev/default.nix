a95763be54dd9ed09ad618d1a5bfcd4aa198edb9
Cody Opel <codyopel@gmail.com>
2015-11-12 11:31:05 -0500

    libevdev: 1.4.4 -> 1.4.5

--- pkgs/development/libraries/libevdev/default.nix
+++ pkgs/development/libraries/libevdev/default.nix
@@ -1,20 +1,22 @@
 { stdenv, fetchurl, python }:
 
 stdenv.mkDerivation rec {
-  name = "libevdev-1.4.4";
+  name = "libevdev-1.4.5";
 
   src = fetchurl {
     url = "http://www.freedesktop.org/software/libevdev/${name}.tar.xz";
-    sha256 = "1aa5pj2ivhvpxcbvgh80ghmzpkwyahw9a2bxi7sjhvkakcv7k6gd";
+    sha256 = "07faqb47c7sjl25rc788cbslyiv5ijky0jc4g6312qz0hv55h779";
   };
 
-  buildInputs = [ python ];
+  buildInputs = [
+    python
+  ];
 
   meta = with stdenv.lib; {
     description = "Wrapper library for evdev devices";
-    homepage = http://www.freedesktop.org/software/libevdev/doc/latest/index.html;
+    homepage = http://www.freedesktop.org/wiki/Software/libevdev/;
     license = licenses.mit;
+    maintainers = [ ];
     platforms = platforms.linux;
-    maintainers = [ maintainers.amorsillo ];
   };
 }
