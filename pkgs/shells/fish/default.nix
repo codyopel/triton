de2acf6376f1af0f04a94c7ce81147fe911fe65a
Cody Opel <codyopel@gmail.com>
2015-11-12 15:06:29 -0500

    fish: fix substitute path to bc in math.fish

--- pkgs/shells/fish/default.nix
+++ pkgs/shells/fish/default.nix
@@ -39,7 +39,8 @@ stdenv.mkDerivation rec {
   postInstall = ''
     sed -e "s|bc|${bc}/bin/bc|" \
         -e "s|/usr/bin/seq|${coreutils}/bin/seq|" \
-        -i "$out/share/fish/functions/seq.fish"
+        -i "$out/share/fish/functions/seq.fish" \
+           "$out/share/fish/functions/math.fish"
 
     sed -i "s|which |${which}/bin/which |" \
            "$out/share/fish/functions/type.fish"
