e49e4b81f728ddc75b8e913f50b877fc912e5193
Cody Opel <codyopel@gmail.com>
2015-11-12 23:21:50 -0500

    gssdp: refactor

--- pkgs/development/libraries/gssdp/default.nix
+++ pkgs/development/libraries/gssdp/default.nix
@@ -1,21 +1,51 @@
-{ stdenv, fetchurl, pkgconfig, libsoup, glib }:
+{ stdenv, fetchurl
+, pkgconfig
 
-stdenv.mkDerivation {
+, libsoup
+, glib
+, gobjectIntrospection
+, gtk3
+}:
+
+stdenv.mkDerivation rec {
   name = "gssdp-0.14.11";
+  versionMajor = "0.14";
+  versionMinor = "11";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = mirror://gnome/sources/gssdp/0.14/gssdp-0.14.11.tar.xz;
+    url = "mirror://gnome/sources/gssdp/${versionMajor}/${name}.tar.xz";
     sha256 = "0njkqr2y7c6linnw4wkc4y2vq5dfkpryqcinbzn0pzhr46psxxbv";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ libsoup ];
-  propagatedBuildInputs = [ glib ];
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--enable-compile-warnings"
+    "--enable-introspection"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    "--with-gtk"
+  ];
 
-  meta = {
-    description = "GObject-based API for handling resource discovery and announcement over SSDP";
+  nativeBuildInputs = [
+    pkgconfig
+  ];
+
+  buildInputs = [
+    libsoup
+    gobjectIntrospection
+    gtk3
+  ];
+
+  propagatedBuildInputs = [
+    glib
+  ];
+
+  meta = with stdenv.lib; {
+    description = "API for handling resource discovery and announcement over SSDP";
     homepage = http://www.gupnp.org/;
-    license = stdenv.lib.licenses.lgpl2;
-    platforms = stdenv.lib.platforms.all;
+    license = licenses.lgpl2;
+    platforms = platforms.all;
   };
 }
