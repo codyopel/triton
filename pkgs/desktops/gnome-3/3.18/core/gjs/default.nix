1de0cf892c68461f11de6dd50c5778fdd34a696e
Cody Opel <codyopel@gmail.com>
2015-11-12 23:17:20 -0500

    gjs: refactor

--- pkgs/desktops/gnome-3/3.18/core/gjs/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/gjs/default.nix
@@ -1,24 +1,69 @@
-{ fetchurl, stdenv, pkgconfig, gnome3, gtk3, gobjectIntrospection
-, spidermonkey_24, pango, readline, glib, libxml2 }:
+{ fetchurl, stdenv
+, gettext
+, pkgconfig
 
-let
-  majorVersion = "1.43";
-in
+, cairo
+, glib
+, gnome3
+, gobjectIntrospection
+, gtk3
+, libffi
+, libxml2
+, pango
+, readline
+, spidermonkey_24
+, xorg
+}:
+
 stdenv.mkDerivation rec {
-  name = "gjs-${majorVersion}.3";
+  name = "gjs-${version}";
+  versionMajor = "1.44";
+  versionMinor = "0";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gjs/${majorVersion}/${name}.tar.xz";
-    sha256 = "0khwm8l6m6x71rwf3q92d6scbhmrpiw7kqmj34nn588fb7a4vdc2";
+    url = "mirror://gnome/sources/gjs/${versionMajor}/${name}.tar.xz";
+    sha256 = "106fgpr4y99sj68l72pnfa2za11ps4bn6p9z28fr79j7mpv61jc8";
   };
 
-  buildInputs = [ libxml2 gobjectIntrospection pkgconfig gtk3 glib pango readline ];
+  configureFlags = [
+    "--enable-cxx-warnings"
+    "--disable-coverage"
+    "--disable-systemtap"
+    "--disable-dtrace"
+    "--enable-Bsymbolic"
+    "--with-cairo"
+    "--with-gtk"
+  ];
 
-  propagatedBuildInputs = [ spidermonkey_24 ];
+  nativeBuildInputs = [
+    gettext
+    pkgconfig
+  ];
+
+  buildInputs = [
+    cairo
+    glib
+    gnome3.gnome_common
+    gtk3
+    libffi
+    libxml2
+    pango
+    readline
+    xorg.libICE
+    xorg.libSM
+  ];
+
+  propagatedBuildInputs = [
+    gobjectIntrospection
+    spidermonkey_24
+  ];
 
   postInstall = ''
     sed 's|-lreadline|-L${readline}/lib -lreadline|g' -i $out/lib/libgjs.la
   '';
+
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     maintainers = gnome3.maintainers;
