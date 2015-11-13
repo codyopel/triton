539ba1f440b71a68b1f73151ecddb551005d80d8
Cody Opel <codyopel@gmail.com>
2015-11-12 23:23:01 -0500

    libspectre: remove cairo to prevent circular dependencies

--- pkgs/development/libraries/libspectre/default.nix
+++ pkgs/development/libraries/libspectre/default.nix
@@ -1,4 +1,8 @@
-{ fetchurl, stdenv, pkgconfig, ghostscript, cairo }:
+{ fetchurl, stdenv
+, pkgconfig
+
+, ghostscript
+}:
 
 stdenv.mkDerivation rec {
   name = "libspectre-0.2.7";
@@ -8,23 +12,20 @@ stdenv.mkDerivation rec {
     sha256 = "1v63lqc6bhhxwkpa43qmz8phqs8ci4dhzizyy16d3vkb20m846z8";
   };
 
-  buildInputs = [
-    # Need `libgs.so'.
-    pkgconfig ghostscript cairo /*for tests*/
+  nativeBuildInputs = [
+    pkgconfig
   ];
 
-  doCheck = true;
+  buildInputs = [
+    ghostscript
+  ];
+
+  doCheck = false;
+  enableParallelBuilding = true;
 
   meta = {
-    homepage = http://libspectre.freedesktop.org/;
     description = "PostScript rendering library";
-
-    longDescription = ''
-      libspectre is a small library for rendering Postscript
-      documents.  It provides a convenient easy to use API for
-      handling and rendering Postscript documents.
-    '';
-
+    homepage = http://libspectre.freedesktop.org/;
     license = stdenv.lib.licenses.gpl2Plus;
   };
 }
