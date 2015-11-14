911e848ae86a7482e14b84f13075e2a16c243e95
Cody Opel <codyopel@gmail.com>
2015-11-14 09:05:37 -0500

    totem: 3.18.0 -> 3.18.1

--- pkgs/desktops/gnome-3/3.18/core/totem/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/totem/default.nix
@@ -1,27 +1,109 @@
-{ stdenv, intltool, fetchurl, gst_all_1
-, clutter_gtk, clutter-gst, pygobject, shared_mime_info
-, pkgconfig, gtk3, glib
-, bash, makeWrapper, itstool, libxml2, dbus_glib
-, gnome3, librsvg, gdk_pixbuf, file }:
+{ stdenv, fetchurl
+, intltool
+, itstool
+, makeWrapper
+, pkgconfig
+
+, gobjectIntrospection
+, gst_all_1
+, clutter_gtk
+, clutter-gst
+, python
+, pygobject3
+, pylint
+, shared_mime_info
+, gtk3
+, glib
+, bash
+, libxml2
+, dbus_glib
+, gnome3
+, librsvg
+, gdk_pixbuf
+, file
+# Plugins
+, lirc
+, vala
+}:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "totem-${version}";
+  versionMajor = "3.18";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
 
-  doCheck = true;
+  src = fetchurl {
+    url = "mirror://gnome/sources/totem/${versionMajor}/${name}.tar.xz";
+    sha256 = "18h784c77m4h359j3xnlwqlfvnhbw7m052ahzm26r106jsp6x0fp";
+  };
 
-  enableParallelBuilding = true;
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--enable-nls"
+    "--enable-introspection"
+    "--enable-glibtest"
+    "--enable-easy-codec-installation"
+    "--enable-python"
+    "--enable-vala"
+    "--enable-nautilus"
+    "--enable-schemas-compile"
+    "--disable-debug"
+    "--enable-compile-warnings"
+    "--enable-cxx-warnings"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    "--enable-appstream-util"
+    "--disable-run-in-source-tree"
+    "--with-plugins=autodetect"
+    #"--with-nautilusdir="
+  ];
 
-  NIX_CFLAGS_COMPILE = "-I${gnome3.glib}/include/gio-unix-2.0";
+  NIX_CFLAGS_COMPILE = [
+    "-I${gnome3.glib}/include/gio-unix-2.0"
+  ];
 
-  propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard ];
+  nativeBuildInputs = [
+    intltool
+    itstool
+    makeWrapper
+    pkgconfig
+  ];
 
-  buildInputs = [ pkgconfig gtk3 glib intltool itstool libxml2 gnome3.grilo
-                  clutter_gtk clutter-gst gnome3.totem-pl-parser gnome3.grilo-plugins
-                  gst_all_1.gstreamer gst_all_1.gst-plugins-base
-                  gst_all_1.gst-plugins-good gst_all_1.gst-plugins-bad
-                  gnome3.libpeas pygobject shared_mime_info dbus_glib
-                  gdk_pixbuf gnome3.defaultIconTheme librsvg gnome3.gnome_desktop
-                  gnome3.gsettings_desktop_schemas makeWrapper file ];
+  propagatedUserEnvPkgs = [
+    gnome3.gnome_themes_standard
+  ];
+
+  buildInputs = [
+    gobjectIntrospection
+    gtk3
+    glib
+    libxml2
+    gnome3.grilo
+    clutter_gtk
+    clutter-gst
+    gnome3.totem-pl-parser
+    gnome3.grilo-plugins
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gnome3.libpeas
+    python
+    pygobject3
+    pylint
+    shared_mime_info
+    dbus_glib
+    gdk_pixbuf
+    gnome3.defaultIconTheme
+    librsvg
+    gnome3.gnome_desktop
+    gnome3.gsettings_desktop_schemas
+    file
+    # Plugins
+    lirc
+    vala
+  ];
 
   preFixup = ''
     wrapProgram "$out/bin/totem" \
@@ -31,6 +113,9 @@ stdenv.mkDerivation rec {
       --prefix XDG_DATA_DIRS : "${gnome3.gnome_themes_standard}/share:$XDG_ICON_DIRS:$GSETTINGS_SCHEMAS_PATH"
 
   '';
+
+  doCheck = true;
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/Apps/Videos;
