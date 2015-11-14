f63cd8ef44865781a1a8fa8ca14d29b87c564273
Cody Opel <codyopel@gmail.com>
2015-11-14 04:58:38 -0500

    geocode-glib: enable introspection

--- pkgs/desktops/gnome-3/3.18/core/geocode-glib/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/geocode-glib/default.nix
@@ -1,10 +1,15 @@
-{ fetchurl, stdenv, pkgconfig, gnome3, intltool, libsoup, json_glib }:
+{ fetchurl, stdenv
+, pkgconfig, gnome3, intltool, libsoup, json_glib
+, gobjectIntrospection
+}:
 
 stdenv.mkDerivation rec {
   inherit (import ./src.nix fetchurl) name src;
 
-  buildInputs = with gnome3;
-    [ intltool pkgconfig glib libsoup json_glib ];
+  buildInputs = with gnome3; [
+    intltool pkgconfig glib libsoup json_glib
+    gobjectIntrospection
+  ];
 
   meta = with stdenv.lib; {
     platforms = platforms.linux;
