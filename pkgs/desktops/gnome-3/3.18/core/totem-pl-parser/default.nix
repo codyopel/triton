41b78486448aceb9b7823bcdd20f3419e5c81b80
Cody Opel <codyopel@gmail.com>
2015-11-14 09:04:49 -0500

    totem-pl-parser: 3.10.2 -> 3.10.5

--- pkgs/desktops/gnome-3/3.18/core/totem-pl-parser/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/totem-pl-parser/default.nix
@@ -1,14 +1,64 @@
-{ stdenv, fetchurl, pkgconfig, file, intltool, gmime, libxml2, libsoup, gnome3 }:
+{ stdenv, fetchurl
+, intltool
+, pkgconfig
+
+, file
+, glib
+, gmime
+, gobjectIntrospection
+, libsoup
+, libxml2
+
+, gnome3
+}:
 
 stdenv.mkDerivation rec {
-  name = "totem-pl-parser-3.10.2";
+  name = "totem-pl-parser-${version}";
+  versionMajor = "3.10";
+  versionMinor = "5";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/totem-pl-parser/3.10/${name}.tar.xz";
-    sha256 = "38be09bddc46ddecd2b5ed7c82144ef52aafe879a5ec3d8b192b4b64ba995469";
+    url = "mirror://gnome/sources/totem-pl-parser/${versionMajor}/${name}.tar.xz";
+    sha256 = "0dw1kiwmjwdjrighri0j9nagsnj44dllm0mamnfh4y5nc47mhim7";
   };
 
-  buildInputs = [ pkgconfig file intltool gmime libxml2 libsoup ];
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--enable-nls"
+    "--enable-gmime-i-know-what-im-doing"
+    # TODO: quvi support
+    "--disable-quvi"
+    # TODO: libarchive support
+    "--disable-libarchive"
+    # TODO: libgcrypt support
+    "--disable-libgcrypt"
+    "--disable-debug"
+    "--enable-cxx-warnings"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    "--enable-introspection"
+    "--disable-code-coverage"
+    #"--with-libgcrypt-prefix="
+  ];
+
+  nativeBuildInputs = [
+    intltool
+    pkgconfig
+  ];
+
+  propagatedBuildInputs = [
+    glib
+  ];
+
+  buildInputs = [
+    file
+    gmime
+    gobjectIntrospection
+    libsoup
+    libxml2
+  ];
 
   meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/Apps/Videos;
