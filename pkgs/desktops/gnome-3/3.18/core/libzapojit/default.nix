6936aa93f62221ddc30691ba9c27c03a39d3179d
Cody Opel <codyopel@gmail.com>
2015-11-14 05:10:37 -0500

    libzapojit: refactor & enable introspection

--- pkgs/desktops/gnome-3/3.18/core/libzapojit/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/libzapojit/default.nix
@@ -1,14 +1,51 @@
-{ stdenv, fetchurl, pkgconfig, glib, intltool, json_glib, rest, libsoup, gtk, gnome_online_accounts }:
+{ stdenv, fetchurl
+, intltool
+, pkgconfig
+
+, glib
+, gnome_online_accounts
+, gobjectIntrospection
+, gtk3
+, json_glib
+, libsoup
+, rest
+}:
 
 stdenv.mkDerivation rec {
-  name = "libzapojit-0.0.3";
+  name = "libzapojit-${version}";
+  versionMajor = "0.0";
+  versionMinor = "3";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/libzapojit/0.0/${name}.tar.xz";
+    url = "mirror://gnome/sources/libzapojit/${versionMajor}/${name}.tar.xz";
     sha256 = "0zn3s7ryjc3k1abj4k55dr2na844l451nrg9s6cvnnhh569zj99x";
   };
 
-  buildInputs = [ pkgconfig glib intltool json_glib rest libsoup gtk gnome_online_accounts ];
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--disable-debug"
+    "--enable-introspection"
+    "--enable-nls"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+  ];
+
+  nativeBuildInputs = [
+    intltool
+    pkgconfig
+  ];
+
+  buildInputs = [
+    glib
+    gnome_online_accounts
+    gobjectIntrospection
+    gtk3
+    json_glib
+    libsoup
+    rest
+  ];
 
   meta = with stdenv.lib; {
     platforms = platforms.linux;
