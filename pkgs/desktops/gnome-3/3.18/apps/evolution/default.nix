837f7a781db5eceb919318f9956ad3a3cec6e1a6
Cody Opel <codyopel@gmail.com>
2015-11-14 09:06:41 -0500

    evolution: 3.18.0 -> 3.18.2

--- pkgs/desktops/gnome-3/3.18/apps/evolution/default.nix
+++ pkgs/desktops/gnome-3/3.18/apps/evolution/default.nix
@@ -1,35 +1,110 @@
-{ stdenv, intltool, fetchurl, libxml2, webkitgtk, highlight
-, pkgconfig, gtk3, glib, libnotify, gtkspell3
-, makeWrapper, itstool, shared_mime_info, libical, db, gcr, sqlite
-, gnome3, librsvg, gdk_pixbuf, libsecret, nss, nspr, icu, libtool
-, libcanberra_gtk3, bogofilter, gst_all_1, procps, p11_kit }:
+{ stdenv, fetchurl
+, intltool
+
+, libxml2
+, webkitgtk
+, highlight
+, pkgconfig
+, gtk3
+, glib
+, libnotify
+, gtkspell3
+, makeWrapper
+, itstool
+, shared_mime_info
+, libical
+, db
+, gcr
+, sqlite
+, gnome3
+, librsvg
+, gdk_pixbuf
+, libsecret
+, nss
+, nspr
+, icu
+, libtool
+, libcanberra_gtk3
+, bogofilter
+, gst_all_1
+, procps
+, p11_kit
+}:
 
 let
   majVer = gnome3.version;
 in stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "evolution-${version}";
+  versionMajor = "3.18";
+  versionMinor = "2";
+  version = "${versionMajor}.${versionMinor}";
 
-  doCheck = true;
+  src = fetchurl {
+    url = "mirror://gnome/sources/evolution/${versionMajor}/${name}.tar.xz";
+    sha256 = "132as4z30dykl3a8avlcwy7nhd83haxd85xagrjn8sbypbx3xf4i";
+  };
 
-  propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard ];
+  configureFlags = [
+    "--disable-spamassassin"
+    "--disable-pst-import"
+    "--disable-autoar"
+    "--disable-libcryptui"
+  ];
 
-  propagatedBuildInputs = [ gnome3.gtkhtml ];
+  NIX_CFLAGS_COMPILE = [
+    "-I${nspr}/include/nspr"
+    "-I${nss}/include/nss"
+    "-I${glib}/include/gio-unix-2.0"
+  ];
 
-  buildInputs = [ pkgconfig gtk3 glib intltool itstool libxml2 libtool
-                  gdk_pixbuf gnome3.defaultIconTheme librsvg db icu
-                  gnome3.evolution_data_server libsecret libical gcr
-                  webkitgtk shared_mime_info gnome3.gnome_desktop gtkspell3
-                  libcanberra_gtk3 bogofilter gnome3.libgdata sqlite
-                  gst_all_1.gstreamer gst_all_1.gst-plugins-base p11_kit
-                  nss nspr libnotify procps highlight gnome3.libgweather
-                  gnome3.gsettings_desktop_schemas makeWrapper ];
+  propagatedUserEnvPkgs = [
+    gnome3.gnome_themes_standard
+  ];
 
-  configureFlags = [ "--disable-spamassassin" "--disable-pst-import" "--disable-autoar"
-                     "--disable-libcryptui" ];
+  nativeBuildInputs = [
+    intltool
+    itstool
+    libtool
+    pkgconfig
+  ];
 
-  NIX_CFLAGS_COMPILE = "-I${nspr}/include/nspr -I${nss}/include/nss -I${glib}/include/gio-unix-2.0";
+  propagatedBuildInputs = [
+    gnome3.gtkhtml
+  ];
 
-  enableParallelBuilding = true;
+  buildInputs = [
+    gtk3
+    glib
+    libxml2
+    gdk_pixbuf
+    gnome3.defaultIconTheme
+    librsvg
+    db
+    icu
+    gnome3.evolution_data_server
+    libsecret
+    libical
+    gcr
+    webkitgtk
+    shared_mime_info
+    gnome3.gnome_desktop
+    gtkspell3
+    libcanberra_gtk3
+    bogofilter
+    gnome3.libgdata
+    sqlite
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    p11_kit
+    nss
+    nspr
+    libnotify
+    procps
+    highlight
+    gnome3.libgweather
+    gnome3.gsettings_desktop_schemas
+    makeWrapper
+  ];
 
   preFixup = ''
     for f in $out/bin/* $out/libexec/*; do
@@ -39,11 +114,14 @@ in stdenv.mkDerivation rec {
     done
   '';
 
+  doCheck = true;
+  enableParallelBuilding = true;
+
   meta = with stdenv.lib; {
+    description = "Integrated mail, calendaring and address book";
     homepage = https://wiki.gnome.org/Apps/Evolution;
-    description = "Personal information management application that provides integrated mail, calendaring and address book functionality";
-    maintainers = gnome3.maintainers;
     license = licenses.lgpl2Plus;
+    maintainers = gnome3.maintainers;
     platforms = platforms.linux;
   };
 }
