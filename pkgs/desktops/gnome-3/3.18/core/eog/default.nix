14ab8e206abb8d1e78ad9bc3ab25391605ca03fc
Cody Opel <codyopel@gmail.com>
2015-11-15 06:12:55 -0500

    eog: 3.16.3 -> 3.18.1

--- pkgs/desktops/gnome-3/3.18/core/eog/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/eog/default.nix
@@ -2,7 +2,15 @@
 , shared_mime_info, makeWrapper, librsvg, libexif }:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "eog-${version}";
+  versionMajor = "3.18";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/eog/${versionMajor}/${name}.tar.xz";
+    sha256 = "19wkawrcwjjcvlmizkj57qycnbgizhr8ck3j5qg70605d1xb8yvv";
+  };
 
   buildInputs = with gnome3;
     [ intltool pkgconfig itstool libxml2 libjpeg gtk glib libpeas makeWrapper librsvg
