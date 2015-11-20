a588d7eb335b7395349811a25271202b3f8a5711
Cody Opel <codyopel@gmail.com>
2015-11-20 11:33:50 -0500

    audiofile: refactor

--- pkgs/development/libraries/audiofile/default.nix
+++ pkgs/development/libraries/audiofile/default.nix
@@ -1,22 +1,41 @@
-{ stdenv, fetchurl, alsaLib }:
+{ stdenv, fetchurl
+, alsaLib
+}:
+
+with {
+  inherit (stdenv)
+    isLinux;
+  inherit (stdenv.lib)
+    optional;
+};
 
 stdenv.mkDerivation rec {
   name = "audiofile-0.3.6";
 
-  nativeBuildInputs = stdenv.lib.optional stdenv.isLinux alsaLib;
+  nativeBuildInputs = optional isLinux alsaLib;
 
   src = fetchurl {
     url = "http://audiofile.68k.org/${name}.tar.gz";
     sha256 = "0rb927zknk9kmhprd8rdr4azql4gn2dp75a36iazx2xhkbqhvind";
   };
 
+  configureFlags = [
+    "--enable-largefile"
+    "--disable-werror"
+    "--disable-coverage"
+    "--disable-valgrind"
+    "--disable-docs"
+    "--disable-examples"
+    "--enable-flac"
+  ];
+
   enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     description = "Library for reading and writing audio files in various formats";
-    homepage    = http://www.68k.org/~michael/audiofile/; 
-    license     = licenses.lgpl21Plus;
-    maintainers = with maintainers; [ lovek323 ];
-    platforms   = platforms.unix;
+    homepage = http://www.68k.org/~michael/audiofile/; 
+    license = licenses.lgpl21Plus;
+    maintainers = with maintainers; [ ];
+    platforms = platforms.unix;
   };
 }
