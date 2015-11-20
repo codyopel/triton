93a0ca62c3392733c939e081b5d3d6443236fc19
Cody Opel <codyopel@gmail.com>
2015-11-20 11:32:10 -0500

    btrfs-progs: 4.3 -> 4.3.1

--- pkgs/tools/filesystems/btrfsprogs/default.nix
+++ pkgs/tools/filesystems/btrfsprogs/default.nix
@@ -1,44 +1,49 @@
 { stdenv, fetchurl
-, pkgconfig
-, attr
-, acl
-, zlib
-, libuuid
-, e2fsprogs
-, lzo
 , asciidoc
-, xmlto
 , docbook_xml_dtd_45
 , docbook_xsl
 , libxslt
+, pkgconfig
+, xmlto
+
+, acl
+, attr
+, e2fsprogs
+, libuuid
+, lzo
+, zlib
 }:
 
 stdenv.mkDerivation rec {
   name = "btrfs-progs-${version}";
-  version = "4.3";
+  version = "4.3.1";
 
   src = fetchurl {
-    url = "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v${version}.tar.xz";
-    sha256 = "1755bjysbw7m2zgzp0jyr03x2vrnxvzdh8wvzmqqgmg97ngv4rxb";
+    url = "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/" +
+          "btrfs-progs-v${version}.tar.xz";
+    sha256 = "0fpxi9pd297lrrynnsyggdwdcb4xvjvn2gvzlzsws0gdvqazzd8c";
   };
 
   # gcc bug with -O1 on ARM with gcc 4.8
   # This should be fine on all platforms so apply universally
-  patchPhase = "sed -i s/-O1/-O2/ configure";
+  postPatch = "sed -i s/-O1/-O2/ configure";
 
-  buildInputs = [
-    pkgconfig
-    attr
-    acl
-    zlib
-    libuuid
-    e2fsprogs
-    lzo
+  nativeBuildInputs = [
     asciidoc
-    xmlto
     docbook_xml_dtd_45
     docbook_xsl
     libxslt
+    pkgconfig
+    xmlto
+  ];
+
+  buildInputs = [
+    acl
+    attr
+    e2fsprogs
+    libuuid
+    lzo
+    zlib
   ];
 
   enableParallelBuilding = true;
