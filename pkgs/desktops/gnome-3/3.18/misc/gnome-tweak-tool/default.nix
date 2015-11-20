5bf815812757179da8f11b06df0ff67e42b7369f
Cody Opel <codyopel@gmail.com>
2015-11-20 02:16:54 -0500

    gnome-tweak-tool: 3.16.2 -> 3.18.1

--- pkgs/desktops/gnome-3/3.18/misc/gnome-tweak-tool/default.nix
+++ pkgs/desktops/gnome-3/3.18/misc/gnome-tweak-tool/default.nix
@@ -1,23 +1,76 @@
-{ stdenv, intltool, fetchurl, python, pygobject, atk
-, pkgconfig, gtk3, glib, libsoup
-, bash, makeWrapper, itstool, libxml2, python3Packages
-, gnome3, librsvg, gdk_pixbuf, file, libnotify }:
+{ stdenv, fetchurl
+, intltool
+, itstool
+, makeWrapper
+, pkgconfig
+
+, python
+, pygobject
+, atk
+, gtk3
+, glib
+, libsoup
+, bash
+, libxml2
+, python3Packages
+, gnome3
+, librsvg
+, gdk_pixbuf
+, file
+, libnotify
+}:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "gnome-tweak-tool-${version}";
+  versionMajor = "3.18";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
 
-  doCheck = true;
+  src = fetchurl {
+    url = "mirror://gnome/sources/gnome-tweak-tool/${versionMajor}/${name}.tar.xz";
+    sha256 = "0c4lw9fhflvpa5l5jsr70aqxcmvhdgh43ab35ly42j3n4c1i2b2w";
+  };
 
-  propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard ];
+  patches = [
+    ./find_gsettings.patch
+    #./0001-Search-for-themes-and-icons-in-system-data-dirs.patch
+    #./0002-Don-t-show-multiple-entries-for-a-single-theme.patch
+    ./0003-Create-config-dir-if-it-doesn-t-exist.patch
+  ];
 
-  makeFlags = [ "DESTDIR=/" ];
+  makeFlags = [
+    "DESTDIR=/"
+  ];
 
-  buildInputs = [ pkgconfig gtk3 glib intltool itstool libxml2
-                  gnome3.gsettings_desktop_schemas makeWrapper file
-                  gdk_pixbuf gnome3.defaultIconTheme librsvg
-                  python pygobject libnotify gnome3.gnome_shell
-                  libsoup gnome3.gnome_settings_daemon gnome3.nautilus
-                  gnome3.gnome_desktop ];
+  propagatedUserEnvPkgs = [
+    gnome3.gnome_themes_standard
+  ];
+
+  nativeBuildInputs = [
+    intltool
+    itstool
+    makeWrapper
+    pkgconfig
+  ];
+
+  buildInputs = [
+    gtk3
+    glib
+    libxml2
+    gnome3.gsettings_desktop_schemas
+    file
+    gdk_pixbuf
+    gnome3.defaultIconTheme
+    librsvg
+    python
+    pygobject
+    libnotify
+    gnome3.gnome_shell
+    libsoup
+    gnome3.gnome_settings_daemon
+    gnome3.nautilus
+    gnome3.gnome_desktop
+  ];
 
   preFixup = ''
     wrapProgram "$out/bin/gnome-tweak-tool" \
@@ -27,18 +80,14 @@ stdenv.mkDerivation rec {
       --prefix PYTHONPATH : "$PYTHONPATH:$(toPythonPath $out)"
   '';
 
-  patches = [
-    ./find_gsettings.patch
-    ./0001-Search-for-themes-and-icons-in-system-data-dirs.patch
-    ./0002-Don-t-show-multiple-entries-for-a-single-theme.patch
-    ./0003-Create-config-dir-if-it-doesn-t-exist.patch
-  ];
+  doCheck = true;
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
-    homepage = https://wiki.gnome.org/action/show/Apps/GnomeTweakTool;
     description = "A tool to customize advanced GNOME 3 options";
-    maintainers = gnome3.maintainers;
+    homepage = https://wiki.gnome.org/action/show/Apps/GnomeTweakTool;
     license = licenses.gpl3;
+    maintainers = gnome3.maintainers;
     platforms = platforms.linux;
   };
 }
