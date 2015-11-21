337b513e5c9439c5214b94d43701c40efaee1a49
Cody Opel <codyopel@gmail.com>
2015-11-21 10:35:11 -0500

    gnome-themes-standard: refactor

--- pkgs/desktops/gnome-3/3.18/core/gnome-themes-standard/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/gnome-themes-standard/default.nix
@@ -1,14 +1,45 @@
-{ stdenv, fetchurl, intltool, gtk3, gnome3, librsvg, pkgconfig, pango, atk, gtk2
-, gdk_pixbuf }:
+{ stdenv, fetchurl
+, intltool
+, pkgconfig
+
+, gtk3
+, gnome3
+, librsvg
+, pango
+, atk
+, gtk2
+, gdk_pixbuf
+}:
 
 stdenv.mkDerivation rec {
-  inherit (import ./src.nix fetchurl) name src;
+  name = "gnome-themes-standard-${version}";
+  versionMajor = "3.18";
+  versionMinor = "0";
+  version = "${versionMajor}.${versionMinor}";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/gnome-themes-standard/${versionMajor}/" +
+          "${name}.tar.xz";
+    sha256 = "1jxss8kxszhf66vic9n1sagczm5amm0mgxpzyxyjna15q82fnip6";
+  };
+
+  nativeBuildInputs = [
+    intltool
+    pkgconfig
+  ];
   
-  buildInputs = [ intltool gtk3 librsvg pkgconfig pango atk gtk2 gdk_pixbuf
-                  gnome3.defaultIconTheme ];
+  buildInputs = [
+    gtk3
+    librsvg
+    pango
+    atk
+    gtk2
+    gdk_pixbuf
+    gnome3.defaultIconTheme
+  ];
 
   meta = with stdenv.lib; {
-    platforms = platforms.linux;
     maintainers = gnome3.maintainers;
+    platforms = platforms.linux;
   };
 }
