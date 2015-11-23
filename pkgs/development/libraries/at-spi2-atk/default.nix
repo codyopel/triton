daf7617906a20a1921097e3c3f108cb62b8e750a
Cody Opel <codyopel@gmail.com>
2015-11-22 20:13:19 -0500

    at-spi2-atk: refactor

--- pkgs/development/libraries/at-spi2-atk/default.nix
+++ pkgs/development/libraries/at-spi2-atk/default.nix
@@ -1,23 +1,45 @@
-{ stdenv, fetchurl, python, pkgconfig, popt, atk, libX11, libICE, xorg, libXi
-, intltool, dbus_glib, at_spi2_core, libSM }:
+{ stdenv, fetchurl
+, python
+, pkgconfig
+, popt
+, atk
+, libX11
+, libICE
+, xorg
+, libXi
+, intltool
+, dbus_glib
+, at_spi2_core
+, libSM
+}:
 
 stdenv.mkDerivation rec {
+  name = "at-spi2-atk-${version}";
   versionMajor = "2.18";
   versionMinor = "1";
-  moduleName   = "at-spi2-atk";
-  name = "${moduleName}-${versionMajor}.${versionMinor}";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/${moduleName}/${versionMajor}/${name}.tar.xz";
+    url = "mirror://gnome/sources/at-spi2-atk/${versionMajor}/${name}.tar.xz";
     sha256 = "0bf1g5cj84rmx7p1q547vwbc0hlpcs2wrxnmv96lckfkhs9mzcf4";
   };
 
-  buildInputs = [ python pkgconfig popt atk libX11 libICE xorg.libXtst libXi
-                  intltool dbus_glib at_spi2_core libSM ];
-
-  enableParallelBuilding = true;
+  buildInputs = [
+    python
+    pkgconfig
+    popt
+    atk
+    libX11
+    libICE
+    xorg.libXtst
+    libXi
+    intltool
+    dbus_glib
+    at_spi2_core
+    libSM
+  ];
 
   meta = with stdenv.lib; {
-    platforms = platforms.unix;
+    platforms = platforms.linux;
   };
 }
