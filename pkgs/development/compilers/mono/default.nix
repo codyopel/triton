496d69e158a7a40990ac7123eb13134a985e531e
Cody Opel <codyopel@gmail.com>
2015-11-25 17:27:23 -0500

    mono: refactor

--- pkgs/development/compilers/mono/default.nix
+++ pkgs/development/compilers/mono/default.nix
@@ -1,9 +1,28 @@
-{ stdenv, fetchurl, bison, pkgconfig, glib, gettext, perl, libgdiplus, libX11, callPackage, ncurses, zlib, withLLVM ? false, cacert }:
+{ stdenv, fetchurl
+, bison
+, pkgconfig
+
+, glib
+, gettext
+, perl
+, libgdiplus
+, libX11
+, callPackage
+, ncurses
+, zlib
+, withLLVM ? false
+, cacert
+}:
 
 let
   llvm     = callPackage ./llvm.nix { };
-  llvmOpts = stdenv.lib.optionalString withLLVM "--enable-llvm --enable-llvmloaded --with-llvm=${llvm}";
+  llvmOpts = stdenv.lib.optionals withLLVM [
+    "--enable-llvm"
+    "--enable-llvmloaded"
+    "--with-llvm=${llvm}"
+  ];
 in
+
 stdenv.mkDerivation rec {
   name = "mono-${version}";
   version = "4.0.5.1";
@@ -12,39 +31,51 @@ stdenv.mkDerivation rec {
     sha256 = "0af2bfa6xi8g6p1dr831ldq115gij2cyg44z8k39ccgdvm22kx62";
   };
 
-  buildInputs =
-    [ bison pkgconfig glib gettext perl libgdiplus libX11 ncurses zlib
-    ];
-  propagatedBuildInputs = [glib];
-
-  NIX_LDFLAGS = "-lgcc_s" ;
-
-  # To overcome the bug https://bugzilla.novell.com/show_bug.cgi?id=644723
-  dontDisableStatic = true;
+  # We want pkg-config to take priority over the dlls in the Mono framework and the GAC
+  # because we control pkg-config
+  patches = [
+    ./pkgconfig-before-gac.patch
+  ];
 
   # In fact I think this line does not help at all to what I
   # wanted to achieve: have mono to find libgdiplus automatically
-  configureFlags = "--x-includes=${libX11}/include --x-libraries=${libX11}/lib --with-libgdiplus=${libgdiplus}/lib/libgdiplus.so ${llvmOpts}";
+  configureFlags = [
+    "--x-includes=${libX11}/include"
+    "--x-libraries=${libX11}/lib"
+    "--with-libgdiplus=${libgdiplus}/lib/libgdiplus.so"
+    "${llvmOpts}"
+  ];
 
-  # Attempt to fix this error when running "mcs --version":
-  # The file /nix/store/xxx-mono-2.4.2.1/lib/mscorlib.dll is an invalid CIL image
-  dontStrip = true;
+  NIX_LDFLAGS = "-lgcc_s" ;
 
-  # Parallel building doesn't work, as shows http://hydra.nixos.org/build/2983601
-  enableParallelBuilding = false;
+  propagatedBuildInputs = [
+    glib
+  ];
 
-  # We want pkg-config to take priority over the dlls in the Mono framework and the GAC
-  # because we control pkg-config
-  patches = [ ./pkgconfig-before-gac.patch ];
+  buildInputs = [
+    bison
+    pkgconfig
+    glib
+    gettext
+    perl
+    libgdiplus
+    libX11
+    ncurses
+    zlib
+  ];
 
   # Patch all the necessary scripts. Also, if we're using LLVM, we fix the default
   # LLVM path to point into the Mono LLVM build, since it's private anyway.
   preBuild = ''
-    makeFlagsArray=(INSTALL=`type -tp install`)
+    makeFlagsArray=("INSTALL=$(type -tp install)")
+
     patchShebangs ./
-    substituteInPlace mcs/class/corlib/System/Environment.cs --replace /usr/share "$out/share"
+
+    substituteInPlace mcs/class/corlib/System/Environment.cs \
+      --replace /usr/share "$out/share"
   '' + stdenv.lib.optionalString withLLVM ''
-    substituteInPlace mono/mini/aot-compiler.c --replace "llvm_path = g_strdup (\"\")" "llvm_path = g_strdup (\"${llvm}/bin/\")"
+    substituteInPlace mono/mini/aot-compiler.c \
+      --replace "llvm_path = g_strdup (\"\")" "llvm_path = g_strdup (\"${llvm}/bin/\")"
   '';
 
   # Fix mono DLLMap so it can find libX11 and gdiplus to run winforms apps
@@ -65,11 +96,22 @@ stdenv.mkDerivation rec {
     $out/bin/cert-sync ${cacert}/etc/ssl/certs/ca-bundle.crt
   '';
 
-  meta = {
-    homepage = http://mono-project.com/;
+  # To overcome the bug https://bugzilla.novell.com/show_bug.cgi?id=644723
+  dontDisableStatic = true;
+  # Attempt to fix this error when running "mcs --version":
+  # The file /nix/store/xxx-mono-2.4.2.1/lib/mscorlib.dll is an invalid CIL image
+  dontStrip = true;
+  # Parallel building doesn't work, as shows http://hydra.nixos.org/build/2983601
+  enableParallelBuilding = false;
+
+  meta = with stdenv.lib; {
     description = "Cross platform, open source .NET development framework";
-    platforms = with stdenv.lib.platforms; linux;
-    maintainers = with stdenv.lib.maintainers; [ viric thoughtpolice obadz ];
-    license = stdenv.lib.licenses.free; # Combination of LGPL/X11/GPL ?
+    homepage = http://mono-project.com/;
+    license = licenses.free;
+    maintainers = with maintainers; [ ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }
