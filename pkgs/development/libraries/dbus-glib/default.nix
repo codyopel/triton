28c264368a17c17286f4cd60f6557b6fc7dae0f8
Cody Opel <codyopel@gmail.com>
2015-11-30 16:33:19 -0500

    dbus-glib: refactor

--- pkgs/development/libraries/dbus-glib/default.nix
+++ pkgs/development/libraries/dbus-glib/default.nix
@@ -1,29 +1,58 @@
-{ stdenv, fetchurl, pkgconfig, expat, gettext, libiconv, dbus, glib }:
+{ stdenv, fetchurl
+, gettext
+, pkgconfig
+
+, dbus
+, expat
+, glib
+}:
 
 stdenv.mkDerivation rec {
   name = "dbus-glib-0.104";
 
   src = fetchurl {
-    url = "${meta.homepage}/releases/dbus-glib/${name}.tar.gz";
+    url = "http://dbus.freedesktop.org/releases/dbus-glib/${name}.tar.gz";
     sha256 = "1xi1v1msz75qs0s4lkyf1psrksdppa3hwkg0mznc6gpw5flg3hdz";
   };
 
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--disable-tests"
+    "--disable-gcov"
+    "--disable-bash-completion"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+  ];
+
+  nativeBuildInputs = [
+    pkgconfig
+    gettext
+  ];
+
+  propagatedBuildInputs = [
+    dbus.libs
+    glib
+  ];
+
+  buildInputs = [
+    expat
+  ];
+
   outputs = [ "out" "doc" ];
-
-  nativeBuildInputs = [ pkgconfig gettext ];
-
-  buildInputs = [ expat libiconv ];
-
-  propagatedBuildInputs = [ dbus.libs glib ];
 
   doCheck = true;
 
-  passthru = { inherit dbus glib; };
+  passthru = {
+    inherit
+      dbus
+      glib;
+  };
 
-  meta = {
-    homepage = http://dbus.freedesktop.org;
-    license = with stdenv.lib.licenses; [ afl21 gpl2 ];
+  meta = with stdenv.lib; {
     description = "Obsolete glib bindings for D-Bus lightweight IPC mechanism";
-    maintainers = [ stdenv.lib.maintainers.urkud ];
+    homepage = http://dbus.freedesktop.org;
+    license = with licenses; [ afl21 gpl2 ];
+    maintainers = [ ];
   };
 }
