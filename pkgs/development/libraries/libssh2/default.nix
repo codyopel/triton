0e64816549690a0edec7695b380c44a0a6812556
Cody Opel <codyopel@gmail.com>
2015-11-30 02:52:40 -0500

    libssh2: refactor

--- pkgs/development/libraries/libssh2/default.nix
+++ pkgs/development/libraries/libssh2/default.nix
@@ -4,29 +4,10 @@
 , zlib ? null
 
 # Crypto Dependencies
-, openssl ? null, libgcrypt ? null
+, openssl ? null
 }:
 
 with stdenv;
-let
-  # Prefer openssl
-  cryptoStr =
-    if shouldUsePkg openssl != null then
-      "openssl"
-    else if shouldUsePkg libgcrypt != null then
-      "libgcrypt"
-    else
-      "none";
-  crypto = {
-    openssl = openssl;
-    libgcrypt = libgcrypt;
-    none = null;
-  }.${cryptoStr};
-
-  optZlib = shouldUsePkg zlib;
-in
-
-assert crypto != null;
 
 with stdenv.lib;
 stdenv.mkDerivation rec {
@@ -37,30 +18,28 @@ stdenv.mkDerivation rec {
     sha256 = "05c2is69c50lyikkh29nk6zhghjk4i7hjx0zqfhq47aald1jj82s";
   };
 
-  buildInputs = [ crypto optZlib ];
+  buildInputs = [ openssl zlib ];
 
   configureFlags = [
-    (mkWith   (cryptoStr == "openssl")   "openssl"        null)
-    (mkWith   (cryptoStr == "libgcrypt") "libgcrypt"      null)
-    (mkWith   false                      "wincng"         null)
-    (mkWith   optZlib                    "libz"           null)
-    (mkEnable false                      "crypt-none"     null)
-    (mkEnable false                      "mac-none"       null)
-    (mkEnable true                       "gex-new"        null)
-    #(mkEnable true                       "clear-memory"   null) Use autodetection
-    (mkEnable false                      "debug"          null)
-    (mkEnable false                      "examples-build" null)
+    "--enable-largefile"
+    "--enable-rpath"
+    "--disable-crypt-none"
+    "--disable-mac-none"
+    "--enable-gex-new"
+    "--disable-examples-build"
+    "--disable-debug"
+    "--with-openssl"
+    "--without-libgcrypt"
+    "--without-wincng"
+    "--with-libz"
+    "--with-libssl-prefix=${openssl}"
+    "--with-libz-prefix=${zlib}"
   ];
 
   postInstall = ''
     sed -i \
-  '' + optionalString (optZlib != null) ''
-      -e 's,\(-lz\),-L${optZlib}/lib \1,' \
-  '' + optionalString (cryptoStr == "openssl") ''
+      -e 's,\(-lz\),-L${zlib}/lib \1,' \
       -e 's,\(-lssl\|-lcrypto\),-L${openssl}/lib \1,' \
-  '' + optionalString (cryptoStr == "libgcrypt") ''
-      -e 's,\(-lgcrypt\),-L${libgcrypt}/lib \1,' \
-  '' + ''
       $out/lib/libssh2.la
   '';
 
