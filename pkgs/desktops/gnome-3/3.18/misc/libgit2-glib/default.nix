1f630ace175e5ddcd2ac3d9a3d2ec850b036144b
Cody Opel <codyopel@gmail.com>
2015-11-30 02:54:29 -0500

    libgit2-glib: 0.0.24 -> 0.23.6

--- pkgs/desktops/gnome-3/3.18/misc/libgit2-glib/default.nix
+++ pkgs/desktops/gnome-3/3.18/misc/libgit2-glib/default.nix
@@ -1,21 +1,62 @@
-{ stdenv, fetchurl, gnome3, libtool, pkgconfig, vala
-, gtk_doc, gobjectIntrospection, libgit2, glib }:
+{ stdenv, fetchurl
+, gobjectIntrospection
+, libtool
+, pkgconfig
 
-let
-  majorVersion = "0.0";
-in
+, glib
+, gnome3
+, libgit2
+, libssh2
+, pygobject
+, python3
+, vala
+}:
+
 stdenv.mkDerivation rec {
-  name = "libgit2-glib-${majorVersion}.24";
+  name = "libgit2-glib-${version}";
+  versionMajor = "0.23";
+  versionMinor = "6";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/libgit2-glib/0.0/${name}.tar.xz";
-    sha256 = "8a0a6f65d86f2c8cb9bcb20c5e0ea6fd02271399292a71fc7e6852f13adbbdb8";
+    url = "mirror://gnome/sources/libgit2-glib/${versionMajor}/${name}.tar.xz";
+    sha256 = "0i0ig3xndnx9xl47mrqcljc8jv9v8qvyylhcrfb4pdhsp1f6p3aw";
   };
 
-  buildInputs = [ gnome3.gnome_common libtool pkgconfig vala
-                  gtk_doc gobjectIntrospection libgit2 glib ];
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--disable-debug"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    "--enable-ssh"
+    "--enable-python"
+    "--enable-vala"
+    "--enable-compile-warnings"
+  ];
+
+  nativeBuildInputs = [
+    gobjectIntrospection
+    libtool
+    pkgconfig
+  ];
+
+  buildInputs = [
+    glib
+    gnome3.gnome_common
+    libgit2
+    libssh2
+    pygobject
+    python3
+    vala
+  ];
+
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
-    platforms = platforms.linux;
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }
