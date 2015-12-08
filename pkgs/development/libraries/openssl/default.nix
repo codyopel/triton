bc17adaa3084a6074a70f311c93f78ff0e4baad4
Cody Opel <codyopel@gmail.com>
2015-12-08 07:00:18 -0500

    openssl: 1.0.2d -> 1.0.2e

--- pkgs/development/libraries/openssl/default.nix
+++ pkgs/development/libraries/openssl/default.nix
@@ -1,43 +1,45 @@
-{ stdenv, fetchurl, perl
-, withCryptodev ? false, cryptodevHeaders }:
+{ stdenv, fetchurl
+, perl
+, withCryptodev ? false
+  , cryptodevHeaders
+}:
 
-with stdenv.lib;
+with {
+  inherit (stdenv.lib)
+    optional
+    optionals;
+};
+
 let
   opensslCrossSystem = attrByPath [ "openssl" "system" ]
     (throw "openssl needs its platform name cross building" null)
     stdenv.cross;
 in
+
 stdenv.mkDerivation rec {
-  name = "openssl-1.0.2d";
+  name = "openssl-1.0.2e";
 
   src = fetchurl {
     urls = [
       "http://www.openssl.org/source/${name}.tar.gz"
       "http://openssl.linux-mirror.org/source/${name}.tar.gz"
     ];
-    sha256 = "671c36487785628a703374c652ad2cebea45fa920ae5681515df25d9f2c9a8c8";
+    sha256 = "e23ccafdb75cfcde782da0151731aa2185195ac745eea3846133f2e05c0e0bff";
   };
 
   outputs = [ "out" "man" ];
 
-  patches = optional stdenv.isCygwin ./1.0.1-cygwin64.patch
-    ++ optional (stdenv.isDarwin || (stdenv ? cross && stdenv.cross.libc == "libSystem")) ./darwin-arch.patch;
+  postPatch = ''
+    sed -e 's,`cd ./util; ./pod2mantest $(PERL)`,pod2man,g' -i Makefile.org
+  '';
 
-  nativeBuildInputs = [ perl ];
-  buildInputs = stdenv.lib.optional withCryptodev cryptodevHeaders;
-
-  # On x86_64-darwin, "./config" misdetects the system as
-  # "darwin-i386-cc".  So specify the system type explicitly.
-  configureScript =
-    if stdenv.system == "x86_64-darwin" then "./Configure darwin64-x86_64-cc"
-    else if stdenv.system == "x86_64-solaris" then "./Configure solaris64-x86_64-gcc"
-    else "./config";
+  configureScript = "./config";
 
   configureFlags = [
     "shared"
     "--libdir=lib"
     "--openssldir=etc/ssl"
-  ] ++ stdenv.lib.optionals withCryptodev [
+  ] ++ optionals withCryptodev [
     "-DHAVE_CRYPTODEV"
     "-DUSE_CRYPTODEV_DIGESTS"
   ];
@@ -46,8 +48,11 @@ stdenv.mkDerivation rec {
     "MANDIR=$(out)/share/man"
   ];
 
-  # Parallel building is broken in OpenSSL.
-  enableParallelBuilding = false;
+  nativeBuildInputs = [
+    perl
+  ];
+
+  buildInputs = optional withCryptodev cryptodevHeaders;
 
   postInstall = ''
     # If we're building dynamic libraries, then don't install static
@@ -73,9 +78,10 @@ stdenv.mkDerivation rec {
       # It's configure does not like --build or --host
       export configureFlags="${concatStringsSep " " (configureFlags ++ [ opensslCrossSystem ])}"
     '';
-
     configureScript = "./Configure";
   };
+
+  enableParallelBuilding = false;
 
   meta = {
     homepage = http://www.openssl.org/;
