c00763485a0586bd9b29206463361a29231c8540
Cody Opel <codyopel@gmail.com>
2015-12-08 07:04:34 -0500

    libev: update mirror

--- pkgs/development/libraries/libev/default.nix
+++ pkgs/development/libraries/libev/default.nix
@@ -4,9 +4,11 @@ stdenv.mkDerivation rec {
   name = "libev-4.20";
 
   src = fetchurl {
-    url = "http://dist.schmorp.de/libev/Attic/${name}.tar.gz";
+    url = "http://download.openpkg.org/components/cache/libev/${name}.tar.gz";
     sha256 = "17j47pbkr65a18mfvy2861p5k7w4pxmdgiw723ryfqd9gx636w7q";
   };
+
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
     description = "A high-performance event loop/event model";
