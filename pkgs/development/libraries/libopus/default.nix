a175690880821188ed88843b03b0c8905b157a38
Cody Opel <codyopel@gmail.com>
2015-12-08 07:48:59 -0500

    libopus: 1.1 -> 1.1.1

--- pkgs/development/libraries/libopus/default.nix
+++ pkgs/development/libraries/libopus/default.nix
@@ -1,26 +1,52 @@
-{ stdenv, fetchurl, fixedPoint ? false, withCustomModes ? true }:
+{ stdenv, fetchurl
 
-let
-  version = "1.1";
-in
+, fixedPoint ? false
+, withCustomModes ? true
+}:
+
+with {
+  inherit (stdenv)
+    isArm;
+  inherit (stdenv.lib)
+    enFlag;
+};
+
 stdenv.mkDerivation rec {
   name = "libopus-${version}";
+  version = "1.1.1";
 
   src = fetchurl {
     url = "http://downloads.xiph.org/releases/opus/opus-${version}.tar.gz";
-    sha256 = "158xprn2086arvdib3vbbygz7z6jqkw2nci7nlywzzwallap0wmr";
+    sha256 = "07iplfwim26b6k1bqjyciaqvihps9rk5gi8385axa83ppmbgz14v";
   };
 
-  configureFlags = stdenv.lib.optional fixedPoint "--enable-fixed-point"
-                ++ stdenv.lib.optional withCustomModes "--enable-custom-modes";
+  configureFlags = [
+    "--disable-maintainer-mode"
+    (enFlag "fixed-point" fixedPoint null)
+    "--disable-fixed-point-debug"
+    (enFlag "flost-api" (!fixedPoint) null)
+    (enFlag "custom-modes" withCustomModes null)
+    "--disable-flost-approx"
+    "--enable-asm"
+    "--enable-rtcd"
+    (enFlag "intrinsics" (isArm && !fixedPoint) null)
+    "--disable-assertions"
+    "--disable-fuzzing"
+    "--disable-doc"
+    "--disable-extra-programs"
+  ];
 
   doCheck = true;
+  enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
-    description = "Open, royalty-free, highly versatile audio codec";
-    license = stdenv.lib.licenses.bsd3;
+    description = "Highly versatile audio codec";
     homepage = http://www.opus-codec.org/;
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ wkennington ];
+    license = stdenv.lib.licenses.bsd3;
+    maintainers = with maintainers; [ ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }
