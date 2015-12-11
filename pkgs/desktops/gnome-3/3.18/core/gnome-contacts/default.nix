90f5eeb189590f07cf5437066da079fb000e3b95
Cody Opel <codyopel@gmail.com>
2015-12-11 02:46:08 -0500

    gnome-contacts: refactor

--- pkgs/desktops/gnome-3/3.18/core/gnome-contacts/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/gnome-contacts/default.nix
@@ -1,9 +1,33 @@
-{ stdenv, intltool, fetchurl, evolution_data_server, db
-, pkgconfig, gtk3, glib, libsecret
-, libchamplain, clutter_gtk, geocode_glib
-, bash, itstool, folks, libnotify, libxml2
-, gnome3, librsvg, gdk_pixbuf, file, telepathy_glib, nspr, nss
-, libsoup, vala, dbus_glib, automake115x, autoconf }:
+{ stdenv, fetchurl
+, autoconf
+, automake
+, intltool
+, itstool
+, pkgconfig
+
+, evolution_data_server
+, db
+, gtk3
+, glib
+, libsecret
+, libchamplain
+, clutter_gtk
+, geocode_glib
+, bash
+, folks
+, libnotify
+, libxml2
+, gnome3
+, librsvg
+, gdk_pixbuf
+, file
+, telepathy_glib
+, nspr
+, nss
+, libsoup
+, vala
+, dbus_glib
+}:
 
 stdenv.mkDerivation rec {
   name = "gnome-contacts-${version}";
@@ -16,34 +40,65 @@ stdenv.mkDerivation rec {
     sha256 = "09ng00kszzpa6al4j5ak4451i4gg34w89kjqvgs0ag77f1gd4604";
   };
 
-  doCheck = true;
-
-  propagatedUserEnvPkgs = [ gnome3.gnome_themes_standard evolution_data_server ];
-
-  # force build from vala
-  preBuild = ''
-   touch src/*.vala
-  '';
-
-  buildInputs = [ pkgconfig gtk3 glib intltool itstool evolution_data_server
-                  gnome3.gsettings_desktop_schemas file libnotify
-                  folks gnome3.gnome_desktop telepathy_glib libsecret dbus_glib
-                  libxml2 libsoup gnome3.gnome_online_accounts nspr nss
-                  gdk_pixbuf gnome3.defaultIconTheme librsvg
-                  libchamplain clutter_gtk geocode_glib
-                  vala automake115x autoconf db ];
-
-  patches = [ ./gio_unix.patch ];
+  patches = [
+    ./gio_unix.patch
+  ];
 
   patchFlags = "-p0";
 
+  nativeBuildInputs = [
+    autoconf
+    automake
+    intltool
+    itstool
+    pkgconfig
+  ];
+
+  propagatedUserEnvPkgs = [
+    gnome3.gnome_themes_standard
+    evolution_data_server
+  ];
+
+  buildInputs = [
+    gtk3
+    glib
+    evolution_data_server
+    gnome3.gsettings_desktop_schemas
+    file
+    libnotify
+    folks
+    gnome3.gnome_desktop
+    telepathy_glib
+    libsecret
+    dbus_glib
+    libxml2
+    libsoup
+    gnome3.gnome_online_accounts
+    nspr
+    nss
+    gdk_pixbuf
+    gnome3.defaultIconTheme
+    librsvg
+    libchamplain
+    clutter_gtk
+    geocode_glib
+    vala
+    db
+  ];
+
+  preBuild = ''
+    # force build from vala
+    touch src/*.vala
+  '';
+
+  doCheck = true;
   enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
-    homepage = https://wiki.gnome.org/Apps/Contacts;
     description = "Contacts is GNOME's integrated address book";
-    maintainers = gnome3.maintainers;
+    homepage = https://wiki.gnome.org/Apps/Contacts;
     license = licenses.gpl2;
+    maintainers = gnome3.maintainers;
     platforms = platforms.linux;
   };
 }
