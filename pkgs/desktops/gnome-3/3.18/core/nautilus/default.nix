e72d3657e9c554ce249d6348e8f5c732b4813ce9
Cody Opel <codyopel@gmail.com>
2015-12-15 07:33:40 -0500

    nautilus: 3.18.2 -> 3.18.3

--- pkgs/desktops/gnome-3/3.18/core/nautilus/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/nautilus/default.nix
@@ -1,29 +1,96 @@
-{ stdenv, fetchurl, pkgconfig, libxml2, dbus_glib, shared_mime_info, libexif
-, gtk, gnome3, libunique, intltool, gobjectIntrospection
-, libnotify, exempi, librsvg, tracker
+{ stdenv, fetchurl
+, gettext
+, gobjectIntrospection
+, intltool
+, pkgconfig
+
+, libxml2
+, dbus_glib
+, shared_mime_info
+, libexif
+, gtk3
+, gvfs
+, gnome3
+, glib
+, libunique
+, libnotify
+, exempi
+, librsvg
+, tracker
+, pango
 , xorg
 }:
 
 stdenv.mkDerivation rec {
   name = "nautilus-${version}";
   versionMajor = "3.18";
-  versionMinor = "2";
+  versionMinor = "3";
   version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/nautilus/${versionMajor}/${name}.tar.xz";
-    sha256 = "0jj23n8vmmyc4gp5xhiz7slsxwksydp26blxi5m154yaw9lgdp38";
+    sha256 = "17d96qxwzibclj2w7xfjh67d3bqp2h7h5jcah6gss8wifwi25024";
   };
 
-  buildInputs = [
-    pkgconfig libxml2 dbus_glib shared_mime_info libexif gtk libunique intltool
-    exempi librsvg
-    gnome3.gnome_desktop gnome3.adwaita-icon-theme
-    gnome3.gsettings_desktop_schemas libnotify tracker
-    xorg.libICE xorg.libSM
+  patches = [
+    ./extension_dir.patch
   ];
 
-  patches = [ ./extension_dir.patch ];
+  postPatch = ''
+    sed -e 's/DISABLE_DEPRECATED_CFLAGS=.*/DISABLE_DEPRECATED_CFLAGS=/' \
+        -i configure
+  '';
+
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--enable-schemas-compile"
+    "--enable-nls"
+    "--disable-gtk-doc"
+    "--disable-gtk-doc-html"
+    "--disable-gtk-doc-pdf"
+    "--disable-profiling"
+    "--enable-nst-extension"
+    "--enable-exif"
+    "--enable-xmp"
+    # Flag is not a proper boolean
+    #"--disable-empty-view"
+    "--enable-packagekit"
+    "--enable-more-warnings"
+    "--enable-tracker"
+    "--enable-introspection"
+    "--disable-update-mimedb"
+  ];
+
+  nativeBuildInputs = [
+    gettext
+    gobjectIntrospection
+    intltool
+    pkgconfig
+  ];
+
+  buildInputs = [
+    dbus_glib
+    exempi
+    glib
+    gnome3.adwaita-icon-theme
+    gnome3.dconf
+    gnome3.gnome_desktop
+    gnome3.gsettings_desktop_schemas
+    gtk3
+    gvfs
+    libexif
+    libnotify
+    librsvg
+    libunique
+    libxml2
+    pango
+    shared_mime_info
+    tracker
+  ];
+
+  preFixup = ''
+    gtk3AppsWrapperArgs+=("--prefix PATH ':' '$GSETTINGS_SCHEMAS_PATH:${gvfs}/bin'")
+  '';
 
   enableParallelBuilding = true;
 
