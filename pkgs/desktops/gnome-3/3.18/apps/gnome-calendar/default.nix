dcb8ac1fdf9153b4730ac45ea2aedabef0b3b0b7
Cody Opel <codyopel@gmail.com>
2015-12-15 08:07:28 -0500

    gnome-calendar: 3.18.0 -> 3.18.1

--- pkgs/desktops/gnome-3/3.18/apps/gnome-calendar/default.nix
+++ pkgs/desktops/gnome-3/3.18/apps/gnome-calendar/default.nix
@@ -1,5 +1,6 @@
 { stdenv, fetchurl
 , pkgconfig
+
 , gnome3
 , gtk3
 , intltool
@@ -9,30 +10,55 @@
 , libsoup
 , glib
 , gnome_online_accounts
+, librsvg
 }:
 
 stdenv.mkDerivation rec {
-  name = "gnome-calendar-3.18.0";
+  name = "gnome-calendar-${version}";
+  versionMajor = "3.18";
+  versionMinor = "1";
+  version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
-    url = mirror://gnome/sources/gnome-calendar/3.18/gnome-calendar-3.18.0.tar.xz;
-    sha256 = "f7d50fe8d5d3dcc574f0034dba6a64cbb9b3f842faab5978c9d02b38548f355b";
+    url = "mirror://gnome/sources/gnome-calendar/${versionMajor}/${name}.tar.xz";
+    sha256 = "1fgjmpk233jyxqajhariwbfq90snbh45dcn2vyzmbyjjk73hgwwn";
   };
+
+  configureFlags = [
+    "--disable-maintainer-mode"
+    "--enable-nls"
+    "--enable-schemas-compile"
+    "--enable-compile-warnings"
+    "--disable-iso-c"
+    "--enable-appstream-util"
+  ];
 
   NIX_CFLAGS_COMPILE = "-I${gnome3.glib}/include/gio-unix-2.0";
 
+  nativeBuildInputs = [
+    intltool
+    pkgconfig
+  ];
+
   buildInputs = [
-    pkgconfig gtk3 intltool evolution_data_server
-    sqlite libxml2 libsoup glib gnome3.defaultIconTheme gnome_online_accounts
+    evolution_data_server
+    gtk3
+    sqlite
+    libxml2
+    libsoup
+    glib
+    gnome3.defaultIconTheme
+    gnome_online_accounts
+    librsvg
   ];
 
   enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
+    description = "Calendar application for GNOME";
     homepage = https://wiki.gnome.org/Apps/Calendar;
-    description = "Simple and beautiful calendar application for GNOME";
-    maintainers = gnome3.maintainers;
     license = licenses.gpl3;
+    maintainers = gnome3.maintainers;
     platforms = platforms.linux;
   };
 }
