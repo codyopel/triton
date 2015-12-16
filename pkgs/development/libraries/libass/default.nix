e99693a3ae207d9a5e5599d0d0d84b1d9ef066b4
Cody Opel <codyopel@gmail.com>
2015-12-16 17:31:43 -0500

    libass: 0.12.2 -> 0.13.1

--- pkgs/development/libraries/libass/default.nix
+++ pkgs/development/libraries/libass/default.nix
@@ -1,47 +1,69 @@
-{ stdenv, fetchurl, pkgconfig, yasm
-, freetype, fribidi
-, encaSupport ? true, enca ? null # enca support
-, fontconfigSupport ? true, fontconfig ? null # fontconfig support
-, harfbuzzSupport ? true, harfbuzz ? null # harfbuzz support
-, rasterizerSupport ? false # Internal rasterizer
-, largeTilesSupport ? false # Use larger tiles in the rasterizer
+{ stdenv, fetchurl
+, pkgconfig
+, yasm
+
+, enca
+, fontconfig
+, freetype
+, fribidi
+, harfbuzz
+
+# Internal rasterizer
+, rasterizerSupport ? false
+# Use larger tiles in the rasterizer
+, largeTilesSupport ? false
 }:
 
-assert encaSupport -> enca != null;
-assert fontconfigSupport -> fontconfig != null;
-assert harfbuzzSupport -> harfbuzz != null;
+with {
+  inherit (stdenv.lib)
+    enFlag;
+};
 
-with stdenv.lib;
 stdenv.mkDerivation rec {
   name = "libass-${version}";
-  version = "0.12.2";
+  version = "0.13.1";
 
   src = fetchurl {
-    url = "https://github.com/libass/libass/releases/download/${version}/${name}.tar.xz";
-    sha256 = "1qzibgqmnnqk2r116lpk1br764g0v74f2zp12y5id0p1plaing37";
+    url = "https://github.com/libass/libass/releases/download/${version}/" +
+          "${name}.tar.xz";
+    sha256 = "1rrz6is2blx8jqyydcz71y2f5f948blgx14jzi3an756fqc6p8sa";
   };
 
   configureFlags = [
-    (mkEnable encaSupport       "enca"        null)
-    (mkEnable fontconfigSupport "fontconfig"  null)
-    (mkEnable harfbuzzSupport   "harfbuzz"    null)
-    (mkEnable rasterizerSupport "rasterizer"  null)
-    (mkEnable largeTilesSupport "large-tiles" null)
+    "--disable-test"
+    "--disable-profile"
+    "--enable-fontconfig"
+    "--disable-directwrite"
+    "--disable-coretext"
+    "--enable-require-system-font-provider"
+    "--enable-harfbuzz"
+    "--enable-asm"
+    (enFlag "rasterizer" rasterizerSupport null)
+    (enFlag "large-tiles" largeTilesSupport null)
   ];
 
-  nativeBuildInputs = [ pkgconfig yasm ];
+  nativeBuildInputs = [
+    pkgconfig
+    yasm
+  ];
 
-  buildInputs = [ freetype fribidi ]
-    ++ optional encaSupport enca
-    ++ optional fontconfigSupport fontconfig
-    ++ optional harfbuzzSupport harfbuzz;
+  buildInputs = [
+    fontconfig
+    freetype
+    fribidi
+    harfbuzz
+  ];
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Portable ASS/SSA subtitle renderer";
-    homepage    = https://github.com/libass/libass;
-    license     = licenses.isc;
-    platforms   = platforms.unix;
-    maintainers = with maintainers; [ codyopel urkud ];
-    repositories.git = git://github.com/libass/libass.git;
+    homepage = https://github.com/libass/libass;
+    license = licenses.isc;
+    maintainers = with maintainers; [
+      codyopel
+    ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }
