faf7533aca623a3b4f61ee8d41bde8d28059ef94
Cody Opel <codyopel@gmail.com>
2015-12-16 16:39:31 -0500

    libmtp: 1.1.9 -> 1.1.10

--- pkgs/development/libraries/libmtp/default.nix
+++ pkgs/development/libraries/libmtp/default.nix
@@ -1,28 +1,36 @@
-{ stdenv, fetchurl, pkgconfig, libusb1 }:
+{ stdenv, fetchurl
+, pkgconfig
+
+, libusb1
+}:
 
 stdenv.mkDerivation rec {
-  name = "libmtp-1.1.9";
-
-  propagatedBuildInputs = [ libusb1 ];
-  buildInputs = [ pkgconfig ];
-
-  # tried to install files to /lib/udev, hopefully OK
-  configureFlags = [ "--with-udev=$$out/lib/udev" ];
+  name = "libmtp-1.1.10";
 
   src = fetchurl {
     url = "mirror://sourceforge/libmtp/${name}.tar.gz";
-    sha256 = "12dinqic0ljnhrwx3rc61jc7q24ybr0mckc2ya5kh1s1np0d7w93";
+    sha256 = "0183r5is1z6qmdbj28ryz8k8dqijll4drzh8lic9xqig0m68vvhy";
   };
 
-  meta = {
-    homepage = http://libmtp.sourceforge.net;
+  configureFlags = [
+    "--with-udev=$$out/lib/udev"
+  ];
+
+  nativeBuildInputs = [
+    pkgconfig
+  ];
+
+  propagatedBuildInputs = [
+    libusb1
+  ];
+
+  meta = with stdenv.lib; {
     description = "An implementation of Microsoft's Media Transfer Protocol";
-    longDescription = ''
-      libmtp is an implementation of Microsoft's Media Transfer Protocol (MTP)
-      in the form of a library suitable primarily for POSIX compliant operating
-      systems. We implement MTP Basic, the stuff proposed for standardization.
-      '';
-    platforms = stdenv.lib.platforms.all;
-    maintainers = [ stdenv.lib.maintainers.urkud ];
+    homepage = http://libmtp.sourceforge.net;
+    maintainers = [ ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
 }
