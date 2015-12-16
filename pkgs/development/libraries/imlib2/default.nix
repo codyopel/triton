74b80658d5bee498572df61db1f6beaf5bacc00b
Cody Opel <codyopel@gmail.com>
2015-12-16 18:28:01 -0500

    imlib2: 1.4.6 -> 1.4.7

--- pkgs/development/libraries/imlib2/default.nix
+++ pkgs/development/libraries/imlib2/default.nix
@@ -1,41 +1,24 @@
-{ stdenv, fetchurl, xlibsWrapper, libjpeg, libtiff, giflib, libpng, bzip2, pkgconfig }:
+{ stdenv, fetchurl, xlibsWrapper, libjpeg, libtiff, giflib, libpng, bzip2, pkgconfig, libid3tag }:
 
 stdenv.mkDerivation rec {
-  name = "imlib2-1.4.6";
+  name = "imlib2-1.4.7";
 
   src = fetchurl {
     url = "mirror://sourceforge/enlightenment/${name}.tar.bz2";
-    sha256 = "0x1j0ylpclkp8cfpwfpkjywqz124bqskyxbw8pvwzkv2gmrbwldg";
+    sha256 = "00a7jbwj10x3jcvxa5rplnkvhv35gv9rb400zy636zdd4g737mrm";
   };
 
-  buildInputs = [ xlibsWrapper libjpeg libtiff giflib libpng bzip2 ];
+  buildInputs = [ xlibsWrapper libjpeg libtiff giflib libpng bzip2 libid3tag ];
 
   nativeBuildInputs = [ pkgconfig ];
-
-  # From
-  # https://github.com/PhantomX/slackbuilds/blob/master/imlib2/patches/imlib2-giflib51.patch
-  patches = [ ./giflib51.patch ];
 
   preConfigure = ''
     substituteInPlace imlib2-config.in \
       --replace "@my_libs@" ""
   '';
 
-  # Do not build amd64 assembly code on Darwin, because it fails to compile
-  # with unknow directive errors
-  configureFlags = if stdenv.isDarwin then [ "--enable-amd64=no" ] else null;
-
   meta = {
     description = "Image manipulation library";
-
-    longDescription = ''
-      This is the Imlib 2 library - a library that does image file loading and
-      saving as well as rendering, manipulation, arbitrary polygon support, etc.
-      It does ALL of these operations FAST. Imlib2 also tries to be highly
-      intelligent about doing them, so writing naive programs can be done
-      easily, without sacrificing speed.
-    '';
-
     license = stdenv.lib.licenses.free;
     platforms = stdenv.lib.platforms.unix;
     maintainers = with stdenv.lib.maintainers; [ spwhitt ];
