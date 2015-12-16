78b5d3997d438bdf58fa547ee1e7015f6f552d55
Cody Opel <codyopel@gmail.com>
2015-12-16 16:49:54 -0500

    libdvbpsi: 0.2.2 -> 1.3.0

--- pkgs/development/libraries/libdvbpsi/default.nix
+++ pkgs/development/libraries/libdvbpsi/default.nix
@@ -1,19 +1,22 @@
-{stdenv, fetchurl}:
+{ stdenv, fetchurl }:
 
-stdenv.mkDerivation {
-
-  name = "libdvbpsi-0.2.2";
+stdenv.mkDerivation rec {
+  name = "libdvbpsi-${version}";
+  version = "1.3.0";
 
   src = fetchurl {
-    url = http://download.videolan.org/pub/libdvbpsi/0.2.2/libdvbpsi-0.2.2.tar.bz2;
-    sha256 = "1lry2swxqm8mhq0a4rjnc819ngsf2pxnfjajb57lml7yr12j79ls";
+    url = "http://download.videolan.org/pub/libdvbpsi/${version}/" +
+          "${name}.tar.bz2";
+    sha256 = "1zm1n1np0nmx209m66ky4bvf06978vi2j7xxkf8jyrl0378x3zm2";
   };
 
-  meta = {
-    description = "A simple library designed for decoding and generation of MPEG TS and DVB PSI tables according to standards ISO/IEC 13818 and ITU-T H.222.0";
+  meta = with stdenv.lib; {
+    description = "Library for decoding & generation of MPEG TS and DVB PSI tables";
     homepage = http://www.videolan.org/developers/libdvbpsi.html ;
-    platforms = stdenv.lib.platforms.unix;
-    license = stdenv.lib.licenses.lgpl21;
+    license = licenses.lgpl21;
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+    ];
   };
-
 }
