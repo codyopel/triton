e7fa69dd1366b1d872a497bcd65f83d2fe9111bc
Cody Opel <codyopel@gmail.com>
2015-12-16 14:45:10 -0500

    speedtest-cli: 0.3.1 -> 0.3.4

--- pkgs/tools/networking/speedtest-cli/default.nix
+++ pkgs/tools/networking/speedtest-cli/default.nix
@@ -1,20 +1,20 @@
 { stdenv, fetchurl, pythonPackages }:
 
 pythonPackages.buildPythonPackage rec {
-  name = "speedtest-cli-${version}";
-  version = "0.3.1";
+  name = "speedtest-cli-0.3.4";
   namePrefix = "";
   
   src = fetchurl {
-    url = "https://pypi.python.org/packages/source/s/speedtest-cli/speedtest-cli-${version}.tar.gz";
-    sha256 = "0ln2grbskh39ph79lhcim2axm7hp4xhzbrag8xfqbfihq7jdm6ya";
+    url = "https://pypi.python.org/packages/source/s/speedtest-cli/" +
+          "${name}.tar.gz";
+    sha256 = "19i671cd815fcv0x7h2m0a493slzwkzn7r926g8myx1srkss0q6d";
   };
 
   meta = with stdenv.lib; {
+    description = "CLI for testing internet bandwidth using speedtest.net";
     homepage = https://github.com/sivel/speedtest-cli;
-    description = "Command line interface for testing internet bandwidth using speedtest.net";
-    platforms = platforms.all;
     license = licenses.asl20;
-    maintainers = [ maintainers.iElectric ];
+    maintainers = [ ];
+    platforms = platforms.all;
   };
 }
