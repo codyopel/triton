b2e9e965a7302b76d525cd18cc9787931178cab7
Cody Opel <codyopel@gmail.com>
2015-12-16 15:46:59 -0500

    alsa-lib: 1.0.29 -> 1.1.0

--- pkgs/os-specific/linux/alsa-lib/default.nix
+++ pkgs/os-specific/linux/alsa-lib/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "alsa-lib-1.0.29";
+  name = "alsa-lib-1.1.0";
 
   src = fetchurl {
     urls = [
      "ftp://ftp.alsa-project.org/pub/lib/${name}.tar.bz2"
      "http://alsa.cybermirror.org/lib/${name}.tar.bz2"
     ];
-    sha256 = "1l5xzhq7xjy8xap087zbbyi14gr1bhil18pn987vwdlnxcskq13k";
+    sha256 = "058zmqyxvab85izcbls58f2phz7vq4l65av2wn18zdl23v8nbpnz";
   };
 
   patches = [
@@ -19,8 +19,6 @@ stdenv.mkDerivation rec {
   # 2: see http://stackoverflow.com/questions/3103400/how-to-overcome-u-int8-t-vs-uint8-t-issue-efficiently
   postPatch = ''
     sed -i -e 's|//int snd_pcm_mixer_element(snd_pcm_t \*pcm, snd_mixer_t \*mixer, snd_mixer_elem_t \*\*elem);|/\*int snd_pcm_mixer_element(snd_pcm_t \*pcm, snd_mixer_t \*mixer, snd_mixer_elem_t \*\*elem);\*/|' include/pcm.h
-
-
     sed -i -e '1i#include <stdint.h>' include/pcm.h
     sed -i -e 's/u_int\([0-9]*\)_t/uint\1_t/g' include/pcm.h
   '';
@@ -32,14 +30,8 @@ stdenv.mkDerivation rec {
   };
 
   meta = with stdenv.lib; {
-    homepage = http://www.alsa-project.org/;
     description = "ALSA, the Advanced Linux Sound Architecture libraries";
-
-    longDescription = ''
-      The Advanced Linux Sound Architecture (ALSA) provides audio and
-      MIDI functionality to the Linux-based operating system.
-    '';
-
+    homepage = http://www.alsa-project.org/;
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
   };
