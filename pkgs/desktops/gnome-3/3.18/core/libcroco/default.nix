d1c4c3121314a046206b9adc902af5e59c5d83d0
Cody Opel <codyopel@gmail.com>
2015-12-16 18:30:38 -0500

    libcroco: 0.6.9 -> 0.6.10

--- pkgs/desktops/gnome-3/3.18/core/libcroco/default.nix
+++ pkgs/desktops/gnome-3/3.18/core/libcroco/default.nix
@@ -3,12 +3,12 @@
 stdenv.mkDerivation rec {
   name = "libcroco-${version}";
   versionMajor = "0.6";
-  versionMinor = "9";
+  versionMinor = "10";
   version = "${versionMajor}.${versionMinor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/libcroco/${versionMajor}/${name}.tar.xz";
-    sha256 = "1bp01c6974p65wkyj0j6k0glvba5z0d8v9h7rarmagl1s6padf9q";
+    sha256 = "0v14ajhjdhsnlwp2q65629r53hgc0rrzr31653xw9xbpvw8nc1kj";
   };
 
   outputs = [ "out" "doc" ];
