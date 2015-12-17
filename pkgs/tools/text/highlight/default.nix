c738ad39918386336142578eb0aae2035cf71fc2
Cody Opel <codyopel@gmail.com>
2015-12-17 13:49:57 -0500

    highlight: 3.18 -> 3.24

--- pkgs/tools/text/highlight/default.nix
+++ pkgs/tools/text/highlight/default.nix
@@ -1,16 +1,31 @@
-{ stdenv, fetchurl, getopt, lua, boost, pkgconfig }:
+{ stdenv, fetchurl
+, getopt
+, lua
+, boost
+, pkgconfig
+}:
 
 stdenv.mkDerivation rec {
-  name = "highlight-3.18";
+  name = "highlight-3.24";
 
   src = fetchurl {
     url = "http://www.andre-simon.de/zip/${name}.tar.bz2";
-    sha256 = "0jsq78qb75sawwggbpx5pdqxk00wgjr1a0la0w8wihmamsjzgijm";
+    sha256 = "1kbraqs2vr2hxj9abpq0zi9w2c98bcjfcrxn9fskzfcxvsbid4jg";
   };
 
-  buildInputs = [ getopt lua boost pkgconfig ];
+  nativeBuildInputs = [
+    pkgconfig
+  ];
 
-  preConfigure = ''makeFlags="PREFIX=$out conf_dir=$out/etc/highlight/"'';
+  buildInputs = [
+    getopt
+    lua
+    boost
+  ];
+
+  preConfigure = ''
+    makeFlags="PREFIX=$out conf_dir=$out/etc/highlight/"
+  '';
 
   meta = {
     description = "Source code highlighting tool";
